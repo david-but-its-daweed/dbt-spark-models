1e4e75a8f4f74f45aea7462a87450674e6b061c2
EKutynina <68954157+EKutynina@users.noreply.github.com>
2022-06-24 14:08:17 +0300

    Analytics/b2b star active device (#11045)
    * b2b_mart.star_active_device
    
    * fix fact_user_request.sql
    
    * add  b2b_mart.dim_device.sql and b2b_mart.star_active_device.sql
    
    * Remove bigquery_fail_on_missing_partitions
    
    Co-authored-by: Artem Plotnikov <aplotnikov@joom.com>
    Co-authored-by: “EKutynina” <ekutynina@gmail.com>
    Co-authored-by: Artem Plotnikov <34647929+profunctor@users.noreply.github.com>

--- analytics/dbt/models/spark/b2b_mart/star_active_device.sql
+++ analytics/dbt/models/spark/b2b_mart/star_active_device.sql
@@ -11,6 +11,8 @@
     }
 ) }}
 
+WITH stg_devices as
+(
     SELECT
         DATE(event_ts_msk) AS partition_date_msk,
         device_id,
@@ -25,12 +27,32 @@
         FIRST(payload.parsedUtm.utm_campaign) OVER (PARTITION BY device_id, user.userId, DATE(event_ts_msk) ORDER BY CASE WHEN payload.parsedUtm.utm_campaign IS NOT NULL THEN 1 ELSE 2 END, event_ts_msk)  AS utm_campaign,
         FIRST(payload.parsedUtm.utm_source) OVER (PARTITION BY device_id, user.userId, DATE(event_ts_msk) ORDER BY CASE WHEN payload.parsedUtm.utm_source IS NOT NULL THEN 1 ELSE 2 END, event_ts_msk)  AS utm_source,
         FIRST(payload.parsedUtm.utm_medium) OVER (PARTITION BY device_id, user.userId, DATE(event_ts_msk) ORDER BY CASE WHEN payload.parsedUtm.utm_medium IS NOT NULL THEN 1 ELSE 2 END, event_ts_msk)  AS utm_medium,
-        FIRST(payload.parsedUtm.utm_term) OVER (PARTITION BY device_id, user.userId, DATE(event_ts_msk) ORDER BY CASE WHEN payload.parsedUtm.utm_term IS NOT NULL THEN 1 ELSE 2 END, event_ts_msk)  AS utm_term
+        FIRST(payload.parsedUtm.utm_term) OVER (PARTITION BY device_id, user.userId, DATE(event_ts_msk) ORDER BY CASE WHEN payload.parsedUtm.utm_term IS NOT NULL THEN 1 ELSE 2 END, event_ts_msk)  AS utm_term,
+        FIRST(device.osVersion) OVER (PARTITION BY device_id, DATE(event_ts_msk) ORDER BY CASE WHEN device.osversion IS NOT NULL THEN 1 ELSE 2 END, event_ts_msk)  AS os_version,
+        FIRST(device.browserName) OVER (PARTITION BY device_id, DATE(event_ts_msk) ORDER BY CASE WHEN device.browserName IS NOT NULL THEN 1 ELSE 2 END, event_ts_msk)  AS browser
     FROM {{ source('b2b_mart', 'device_events') }}
     WHERE `type` in ("sessionStart","externalLink","bounceCheck","popupRequestCreate")
 {% if is_incremental() %}
   and DATE(event_ts_msk) >= date'{{ var("start_date_ymd") }}'
   and DATE(event_ts_msk) < date'{{ var("end_date_ymd") }}'
 {% else %}
-  and partition_date >= date'2022-05-01'
+  and DATE(event_ts_msk)   >= date'2022-05-01'
 {% endif %}
+)
+    SELECT DISTINCT partition_date_msk,
+        device_id,
+        user_id,
+--       join_ts,
+        event_ts_min_msk,
+        event_ts_max_msk,
+        session_start_count,
+        ip_country,
+        os_type,
+        link,
+        utm_campaign,
+        utm_source,
+        utm_medium,
+        utm_term,
+        os_version,
+	    browser
+    FROM stg_devices
