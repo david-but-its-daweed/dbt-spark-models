87e45c7c252be0985ce267a27562a8ba593bac14
Denis Goihburg <gburg@joom.com>
2022-06-28 14:01:20 +0300

    Fix dbt select

--- analytics/dbt/snapshots/spark/b2b_mart/scd2_mongo_order.sql
+++ analytics/dbt/snapshots/spark/b2b_mart/scd2_mongo_order.sql
@@ -11,18 +11,19 @@
     )
 }}
 
-SELECT
-        _id AS order_id,
-        millis_to_ts_msk(ctms) AS created_ts_msk,
-        millis_to_ts_msk(utms) AS update_ts_msk,
-        currencies.clientCcy AS ccy,
-        deliveryTimeDays AS delivery_time_days,
-        friendlyId AS friendly_id,
-        popupReqId AS request_id,
-        linehaulChannelID AS linehaul_channel_id,
-        csmr.deviceId as device_id,
-        csmr.Id as user_id,
-        state.rejectReason as reject_reason,
-        state.statusHistory.status[array_position(state.statusHistory.updatedTimeMs,array_max(state.statusHistory.updatedTimeMs))-1] as last_order_status
+SELECT _id AS                              order_id,
+       millis_to_ts_msk(ctms)           AS created_ts_msk,
+       millis_to_ts_msk(utms)           AS update_ts_msk,
+       currencies.clientCcy             AS ccy,
+       deliveryTimeDays                 AS delivery_time_days,
+       friendlyId                       AS friendly_id,
+       popupReqId                       AS request_id,
+       linehaulChannelID                AS linehaul_channel_id,
+       csmr.deviceId                    as device_id,
+       csmr.Id                          as user_id,
+       state.rejectreason               as reject_reason,
+       element_at(state.statusHistory.status,
+                  cast((array_position(state.statusHistory.updatedTimeMs,
+                                       array_max(state.statusHistory.updatedTimeMs))) as INTEGER)) as last_order_status
 FROM {{ source('mongo', 'b2b_core_orders_v2_daily_snapshot') }}
 {% endsnapshot %}
