07305aca77da6a567097263d70e19ab5a5eefa45
EKutynina <68954157+EKutynina@users.noreply.github.com>
2022-07-04 13:55:38 +0300

    add admin_users (#11198)
    Co-authored-by: “EKutynina” <ekutynina@gmail.com>

---
+++ analytics/dbt/models/spark/b2b_mart/dim_user_admin.sql
@@ -0,0 +1,16 @@
+{{ config(
+    schema='b2b_mart',
+    materialized='view',
+    meta = {
+      'team': 'general_analytics',
+      'bigquery_load': 'true'
+    }
+) }}
+
+select _id as admin_id,
+    email,
+    case when email like "%support.com" then "support"
+         when email like "%joom.com" then "employee"
+         else "unknown" end as role,
+    millis_to_ts_msk(ctms) AS created_ts_msk
+from mongo.b2b_core_admin_users_daily_snapshot
