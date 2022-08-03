c2e644aba4b8688ad237d3a3714e14ea4afcb67b
Artem Plotnikov <34647929+profunctor@users.noreply.github.com>
2022-08-03 17:58:43 +0300

    DBT support for metric prediction (#11797)

---
+++ analytics/dbt/models/spark/example/cube_gmv_coutry_os.sql
@@ -0,0 +1,28 @@
+{{ config(
+    schema='example',
+    materialized='view',
+    meta = {
+      'predictor_enabled': 'true'
+    }
+) }}
+WITH active_devices AS (
+SELECT
+    DISTINCT device_id,
+    date_msk AS t,
+    UPPER(country) AS country,
+    LOWER(os_type) AS platform
+FROM mart.star_active_device
+WHERE
+    ephemeral = FALSE
+)
+SELECT
+    t,
+    country,
+    platform,
+    COUNT(device_id) AS metric_value
+FROM active_devices
+WHERE
+    country IS NOT NULL
+    AND platform IS NOT NULL
+GROUP BY
+    t, country, platform
