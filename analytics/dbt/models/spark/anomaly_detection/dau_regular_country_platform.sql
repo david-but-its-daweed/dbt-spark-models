294b60ce1844ce7ed6660a77d854a4bde86b5634
Artem Plotnikov <aplotnikov@joom.com>
2022-08-30 14:53:31 +0300

    Copy metrics from simple-detector to DBT detector

---
+++ analytics/dbt/models/spark/anomaly_detection/dau_regular_country_platform.sql
@@ -0,0 +1,55 @@
+{{ config(
+    schema='anomaly_detection',
+    materialized='table',
+    file_format='delta',
+    meta = {
+      'predictor_enabled': 'true',
+      'anomalies_channel': '#aplotnikov-notifications',
+      'anomalies_metric_name': 'DAU regular',
+    }
+) }}
+WITH active_devices AS (
+    SELECT DISTINCT device_id,
+        date_msk AS date,
+        DATE(join_ts_msk) AS join_date,
+        UPPER(country) AS country,
+        LOWER(os_type) AS platform
+    FROM {{ source('mart', 'star_active_device') }}
+    ),
+
+     active_devices_2 AS (
+    SELECT device_id,
+        date,
+        LAG(date) OVER (PARTITION BY device_id ORDER BY date) AS prev_date,
+        join_date,
+        country,
+        platform
+    FROM active_devices), 
+
+     active_devices_3 AS (
+    SELECT device_id,
+        date, 
+        COALESCE(DATEDIFF(date, prev_date), 0) as active_lag,
+        join_date,
+        country,
+        platform
+    FROM active_devices_2),
+
+     active_devices_4 AS (
+    SELECT device_id,
+        date,
+        country,
+        platform,
+        IF(date = join_date, 1, 0) AS is_new,
+        IF(date != join_date AND active_lag < 28, 1, 0) AS is_regular,
+        IF(date != join_date AND active_lag > 28, 1, 0) AS is_reactivated
+    FROM active_devices_3)
+
+SELECT date as t,
+    country,
+    platform,
+    SUM(is_regular) AS y
+FROM active_devices_4
+WHERE WHERE country IN ('RU', 'DE', 'FR', 'GB', 'IT', 'ES', 'CH', 'SE', 'IL', 'UA', 'MD', 'BY')
+    AND platform in ('android', 'ios')
+GROUP BY t, country, platform
--- analytics/dbt/models/spark/anomaly_detection/ecgp_initial_country_platform.sql
+++ analytics/dbt/models/spark/anomaly_detection/ecgp_initial_country_platform.sql
@@ -5,29 +5,28 @@
     meta = {
       'predictor_enabled': 'true',
       'anomalies_channel': '#aplotnikov-notifications',
-      'anomalies_metric_name': 'Average Item Value',
+      'anomalies_metric_name': 'eCGP Initial',
     }
 ) }}
 WITH orders AS (
     SELECT device_id,
         shipping_country AS country,
         os_type AS platform,
-        gmv_initial,
-        product_quantity,
+        ecgp_initial,
         partition_date AS date
     FROM {{ source('mart', 'star_order_2020') }}
     ), 
 
-        metric_agg AS (
+     metric_agg AS (
     SELECT date as t,
         country,
         platform,
-        SUM(COALESCE(gmv_initial, 0)) / SUM(COALESCE(product_quantity, 0)) AS y
+        SUM(COALESCE(ecgp_initial, 0)) AS y
     FROM orders
     GROUP BY date, country, platform
     )
 
 SELECT * FROM metric_agg
-WHERE country IN ('RU', 'DE', 'FR', 'GB', 'IT', 'ES', 'CH', 'SE', 'IL', 'UA', 'MD', 'BY')
+WHERE WHERE country IN ('RU', 'DE', 'FR', 'GB', 'IT', 'ES', 'CH', 'SE', 'IL', 'UA', 'MD', 'BY')
     AND platform in ('android', 'ios')
 ORDER BY t, country, platform
