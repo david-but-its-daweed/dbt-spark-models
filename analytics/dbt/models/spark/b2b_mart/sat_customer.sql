978aa26b01f14f3ffab1b6699cb54e41bb31219c
EKutynina <68954157+EKutynina@users.noreply.github.com>
2022-09-16 16:34:05 +0300

    fix of customer's info (#12717)
    * fix of customer's info
    
    * fix of customer's info
    
    Co-authored-by: “EKutynina” <ekutynina@gmail.com>

--- analytics/dbt/models/spark/b2b_mart/sat_customer.sql
+++ analytics/dbt/models/spark/b2b_mart/sat_customer.sql
@@ -10,8 +10,16 @@
 SELECT customer_id,
        company_name,
        year_of_establishment,
-       estimated_purchase_volume_min,
-       estimated_purchase_volume_max,
+       CASE WHEN purchaseVolumePerMonth = 10 THEN "PurchaseVolume_1_150"
+       CASE WHEN purchaseVolumePerMonth = 20 THEN "PurchaseVolume_150_300"
+       CASE WHEN purchaseVolumePerMonth = 30 THEN "PurchaseVolume_300_500"
+       CASE WHEN purchaseVolumePerMonth = 40 THEN "PurchaseVolume_500_700"
+       CASE WHEN purchaseVolumePerMonth = 50 THEN "PurchaseVolume_700_1000"
+       CASE WHEN purchaseVolumePerMonth = 60 THEN "PurchaseVolume_1000_1500"
+       CASE WHEN purchaseVolumePerMonth = 70 THEN "PurchaseVolume_1500_5000"
+       CASE WHEN purchaseVolumePerMonth = 80 THEN "PurchaseVolume_5000_15000"
+       CASE WHEN purchaseVolumePerMonth = 90 THEN "PurchaseVolume_15000"
+       END AS purchase_volume_per_month,
        legal_entity,
        monthly_turnover_from,
        monthly_turnover_to,
