eae1cf2ef94fa127e02822e879b36f23ebbb5fed
Artem Plotnikov <aplotnikov@joom.com>
2022-11-06 12:35:11 +0400

    Add date to max_rpm_by_path

--- analytics/dbt/models/spark/anomaly_detection/max_rpm_by_path.sql
+++ analytics/dbt/models/spark/anomaly_detection/max_rpm_by_path.sql
@@ -15,17 +15,18 @@
 ) }}
 SELECT
     request_root_path,
-    to_timestamp(from_unixtime(int(unix_timestamp(window) / 600) * 600)) t,
+    to_date(from_unixtime(int(window / 86400) * 86400)) d,
+    to_timestamp(from_unixtime(int(window / 600) * 600)) t,
     max(requests_count) max_requests_count
 FROM (
     SELECT
         request_root_path,
-        from_unixtime(int(unix_timestamp(published_at) / 60) * 60) window,
+        int(unix_timestamp(published_at) / 60) * 60 as window,
         ip,
         count(1) as requests_count
     FROM threat.bot_slo_marketplace
     WHERE partition_date >= date('{{ var("start_date_ymd") }}') - INTERVAL 14 days AND NOT is_bot
     GROUP BY 1,2,3
 )
-GROUP BY 1,2
-ORDER BY 1,2
+GROUP BY 1,2,3
+ORDER BY 1,2,3
