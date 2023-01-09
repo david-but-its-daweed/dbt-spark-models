940504b9388e451e4b2d4b20a4cb67fe34adf233
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-01-09 19:50:02 +0300

    Update rfq_metrics.sql

--- analytics/dbt/models/spark/b2b_mart/rfq_metrics.sql
+++ analytics/dbt/models/spark/b2b_mart/rfq_metrics.sql
@@ -1,11 +1,12 @@
 {{ config(
     schema='b2b_mart',
-    materialized='view',
+    materialized='table',
+    file_format='parquet',
     meta = {
-      'team': 'general_analytics',
       'bigquery_load': 'true'
     }
 ) }}
+
 
 WITH requests AS (
     SELECT
