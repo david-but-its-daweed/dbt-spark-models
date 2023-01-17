375f1571a4e26b30b6e7a514613b90b80a72cd0e
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-01-17 11:56:43 +0300

    Update rfq_metrics.sql

--- analytics/dbt/models/spark/b2b_mart/rfq_metrics.sql
+++ analytics/dbt/models/spark/b2b_mart/rfq_metrics.sql
@@ -304,8 +304,8 @@ SELECT DISTINCT
     merchant_id,
     (unix_timestamp(substring(signing_and_payment_ts_msk, 0, 19) ,"yyyy-MM-dd HH:mm:ss")-unix_timestamp(substring(signing_and_payment_ts_msk, 0, 19),"yyyy-MM-dd HH:mm:ss"))/(3600) as time_final_pricing,
     (unix_timestamp(substring(rfq_response_ts_msk, 0, 19) ,"yyyy-MM-dd HH:mm:ss")-unix_timestamp(substring(rfq_sent_ts_msk, 0, 19) ,"yyyy-MM-dd HH:mm:ss"))/(3600) as time_rfq_response,
-    ROW_NUMBER() OVER (PARTITION BY order_id ORDER by order_rfq_response_id, rfq_response_ts_msk) as order_rn,
-    ROW_NUMBER() OVER (PARTITION BY order_id, rfq_request_id ORDER BY order_rfq_response_id, rfq_response_ts_msk) as rfq_rn,
+    ROW_NUMBER() OVER (PARTITION BY order_id ORDER by rfq_response_ts_msk, order_rfq_response_id) as order_rn,
+    ROW_NUMBER() OVER (PARTITION BY order_id, rfq_request_id ORDER BY rfq_response_ts_msk, order_rfq_response_id) as rfq_rn,
     CASE WHEN cancelled_ts_msk = '' THEN ''
         WHEN manufacturing_ts_msk != '' THEN 'manufacturing'
         WHEN signing_and_payment_ts_msk != '' THEN 'signing_and_payment'
