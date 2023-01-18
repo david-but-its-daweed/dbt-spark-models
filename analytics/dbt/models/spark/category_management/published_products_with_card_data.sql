f994ae239c719cb2ac2682ad20ddbd3343c8bfc6
misha-kh <107080325+misha-kh@users.noreply.github.com>
2023-01-18 18:03:20 +0300

    create BQ table `published products with card data` (#14630)
    * Create published_products_with_card_data.sql
    
    Import some data on the product card to bq
    
    * Update sources.yml
    
    fix sources for published_products_with_card_data.sql
    
    * Create products_diagnoses_current.sql
    
    * Update published_products_with_card_data.sql
    
    * Update sources.yml
    
    * Update published_products_with_card_data.sql
    
    fix issues with naming

---
+++ analytics/dbt/models/spark/category_management/published_products_with_card_data.sql
@@ -0,0 +1,25 @@
+{{ config(
+    schema='category_management',
+    materialized='table',
+    meta = {
+      'team': 'category_management',
+      'bigquery_load': true,
+    }
+) }}
+
+select
+    product_id,
+    videoId as video_id,
+    brandId as brand_id,
+    class as brand_class,
+    brand_auth,
+    1 + size(extraImages) as images_count,
+    size (origTags) as original_tags_count,
+    concat_ws(',', origTags) as original_tags
+
+from 
+    {{ ref('mart', 'published_products_current') }}
+left join {{ ref('mongo', 'product_products_daily_snapshot') }}
+    on published_products_current.product_id = product_products_daily_snapshot._id
+left join  {{ ref('mongo','abu_core_brands_daily_snapshot') }} as brands
+    on product_products_daily_snapshot.brandId = brands._id.oid
