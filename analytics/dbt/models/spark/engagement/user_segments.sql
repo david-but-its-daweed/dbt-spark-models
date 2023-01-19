16512e5a90ac1d6fa413d333b2909069e2905e31
evstviaJoom <61778606+evstviaJoom@users.noreply.github.com>
2023-01-19 11:30:34 +0400

    Update user_segments.sql
    change round to floor

--- analytics/dbt/models/spark/engagement/user_segments.sql
+++ analytics/dbt/models/spark/engagement/user_segments.sql
@@ -46,8 +46,8 @@ activity2 AS (
         week_join_msk,
         user_id,
         num_evs,
-        floor(datediff(week_msk, week_join_msk) / 28) AS cohort_month,
-        floor(datediff(week_msk, week_join_msk) / 7) AS cohort_week
+        cast(floor(datediff(week_msk, week_join_msk) / 28) AS INT64) AS cohort_month,
+        cast(floor(datediff(week_msk, week_join_msk) / 7) AS INT64) AS cohort_week
     FROM
         activity
 ),
@@ -56,7 +56,7 @@ purchases AS (
     SELECT
         real_user_id,
         os_type,
-        floor(datediff(week_msk, week_join_msk) / 28) AS cohort_month,
+        cast(floor(datediff(week_msk, week_join_msk) / 28) AS INT64) AS cohort_month,
         sum(num_orders) AS num_orders
     FROM (
         SELECT
