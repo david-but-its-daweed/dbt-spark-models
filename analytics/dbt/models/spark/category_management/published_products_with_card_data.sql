96ccd50215ff78b0774d12c5d576e9a03aaea54a
misha-kh <107080325+misha-kh@users.noreply.github.com>
2023-01-20 23:37:09 +0300

    Update published_products_with_card_data.sql

--- analytics/dbt/models/spark/category_management/published_products_with_card_data.sql
+++ analytics/dbt/models/spark/category_management/published_products_with_card_data.sql
@@ -12,7 +12,7 @@ select
     videoId as video_id,
     brandId as brand_id,
     class as brand_class,
-    brand_auth,
+    brandAuth as brand_auth,
     1 + size(extraImages) as images_count,
     size (origTags) as original_tags_count,
     concat_ws(',', origTags) as original_tags
