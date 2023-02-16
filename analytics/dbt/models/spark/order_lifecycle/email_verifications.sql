23baac2b3aec818555a5f752ecf28505ef07829e
ilypavlov <iapavlov96@gmail.com>
2023-02-16 18:52:58 +0300

    Create email_verifications.sql (#15384)

---
+++ analytics/dbt/models/spark/order_lifecycle/email_verifications.sql
@@ -0,0 +1,39 @@
+{{ config(
+    schema='support',
+    incremental_strategy='insert_overwrite',
+    materialized='incremental',
+    file_format='delta',
+    partition_by=['partition_date'],
+    meta = {
+       'team': 'analytics',
+       'bigquery_load': 'true',
+       'bigquery_overwrite': 'true',
+       'bigquery_partitioning_date_column': 'partition_date',
+       'alerts_channel': "#olc_dbt_alerts",
+       'bigquery_fail_on_missing_partitions': 'false'
+    }
+) }}
+
+%sql
+
+WITH creation_timestamp AS (
+   SELECT userId,
+          MIN(createdTime) AS createdTime,
+          MAX(createdTime) AS max_createdTime
+   FROM mongo.user_email_credentials_daily_snapshot
+   GROUP BY 1
+),
+
+final AS (
+SELECT
+   t.*,
+   a.confirmed
+FROM creation_timestamp AS t
+LEFT JOIN mongo.user_email_credentials_daily_snapshot AS a ON t.userId = a.userId
+                                                        AND t.max_createdTime = a.createdTime
+)
+
+SELECT
+   t.*,
+   CURRENT_DATE() AS partition_date
+FROM final AS t
