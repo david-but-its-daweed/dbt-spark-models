995bb99afe39263f0274e430fc238ade1b629ec6
Maria Shlyapenko <74596570+mshlyapenko-joom@users.noreply.github.com>
2023-02-21 14:05:34 +0300

    MERCHANT-8897 Add model description for scd2_mongo_merchant_payment_channel (#15465)
    * MERCHANT-8897 Add model description for scd2_mongo_merchant_payment_channel
    
    * MERCHANT-8897 fix
    
    * MERCHANT-8897 fix

---
+++ analytics/dbt/models/spark/merchant/dim_merchant_payment_channel.sql
@@ -0,0 +1,18 @@
+{{ config(
+    schema='merchant',
+    materialized='view',
+    meta = {
+      'team': 'merchant',
+    }
+) }}
+SELECT
+    id,
+    type,
+    merchant_id,
+    method_id,
+    status,
+    error_reasons,
+    updated_time,
+    TIMESTAMP(dbt_valid_from) AS effective_ts_msk,
+    TIMESTAMP(dbt_valid_to) AS next_effective_ts_msk
+FROM {{ ref('scd2_mongo_merchant_payment_channel') }} t
