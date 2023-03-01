8ce88a3fc2e9b6733f9b320fd72d77d556225727
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-03-01 10:47:50 +0300

    Update dim_user_admin.sql

--- analytics/dbt/models/spark/b2b_mart/dim_user_admin.sql
+++ analytics/dbt/models/spark/b2b_mart/dim_user_admin.sql
@@ -9,10 +9,12 @@
 ) }}
 
 select _id as admin_id,
-    email,
-    case when email like "%support.com" then "support"
-         when email like "%joom.com" then "employee"
+    a.email,
+    coalesce(s.role, case when a.email like "%support.com" then "support"
+         when a.email like "%joom.com" then "employee"
          else "unknown" end as role,
+    s.name as owner_name,
     timestamp(millis_to_ts_msk(ctms)) as created_ts_msk
-from {{ source('mongo', 'b2b_core_admin_users_daily_snapshot') }}
+from {{ source('mongo', 'b2b_core_admin_users_daily_snapshot') }} a
+LEFT JOIN {{ ref('support_roles') }} s on a.email = s.email
 
