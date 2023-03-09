8476ee742badc93730ae35f0cdfd985595e75202
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-03-09 17:28:11 +0300

    Update statuses_events.sql

--- analytics/dbt/models/spark/b2b_mart/statuses_events.sql
+++ analytics/dbt/models/spark/b2b_mart/statuses_events.sql
@@ -77,9 +77,9 @@ payload.logisticianOrderStatus as status,
 from_unixtime(payload.updatedTime/1000) as event_ts_msk,
 lag(payload.brokerOrderStatus) over (partition by payload.orderId order by event_ts_utc desc) as lag_status
 from {{ source('b2b_mart', 'operational_events') }}
-    where type = 'logisticianOrderChanged' or lag_status is null
+    where type = 'logisticianOrderChanged'
 )
-where status != lag_status
+where status != lag_status or lag_status is null
 )
 ,
 
