7b09396ce74e1fc3ad8ee396babce93c7647f883
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-03-28 18:26:13 +0300

    Update rfq_metrics.sql

--- analytics/dbt/models/spark/b2b_mart/rfq_metrics.sql
+++ analytics/dbt/models/spark/b2b_mart/rfq_metrics.sql
@@ -32,19 +32,38 @@ orders AS (
 ),
 
 internal_products as (
-    SELECT DISTINCT
-        product_id, mo.order_id, max(type) over (partition by product_id) as product_type,
-         row_number() over (partition by o.user_id, product_id order by o.min_manufactured_ts_msk is null, o.min_manufactured_ts_msk) 
+    select product_id, order_id, product_type,
+        row_number() over (partition by user_id, product_id order by min_manufactured_ts_msk is null, min_manufactured_ts_msk) 
                 as user_product_number
+        from
+    (
+    select distinct * 
+    from 
+    (SELECT DISTINCT
+        product_id, 
+        mo.order_id, 
+        o.user_id, 
+        max(type) over (partition by product_id) as product_type,
+        o.min_manufactured_ts_msk
     FROM (
         select orderId as order_id, _id as merchant_order_id
         from {{ source('mongo', 'b2b_core_merchant_orders_v2_daily_snapshot') }}
-        )mo
+        ) mo
     LEFT JOIN (
         select _id as product_id, type, merchOrdId as merchant_order_id
         from {{ source('mongo', 'b2b_core_order_products_daily_snapshot') }}
         ) op on mo.merchant_order_id = op.merchant_order_id
     LEFT JOIN orders o on o.order_id = mo.order_id
+    union all 
+    SELECT DISTINCT
+        product_id, mo.order_id, o.user_id, 
+        max(product_type) over (partition by product_id) as product_type,
+        o.min_manufactured_ts_msk
+    FROM {{ ref('fact_merchant_order') }} mo
+    LEFT JOIN orders o on o.order_id = mo.order_id
+    WHERE next_effective_ts_msk IS NULL and product_id is not null
+    )
+)
     
 ),
 
