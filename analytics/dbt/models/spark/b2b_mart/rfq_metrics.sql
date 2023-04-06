deea83fcde5ffe9f03756672116d2e8bc1b7f59c
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-04-06 12:59:05 +0300

    Update rfq_metrics.sql

--- analytics/dbt/models/spark/b2b_mart/rfq_metrics.sql
+++ analytics/dbt/models/spark/b2b_mart/rfq_metrics.sql
@@ -144,6 +144,7 @@ rfq as (SELECT
     product_id
 FROM rfq_requests AS rq
 LEFT JOIN response AS rp ON rq.rfq_request_id = rp.rfq_request_id
+        and datediff(rp.response_created_ts_msk, rq.request_sent_ts_msk, MONTH) <= 3
 ),
 
 price as (
