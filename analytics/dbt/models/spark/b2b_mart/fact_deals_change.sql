e90850f789da5e1a39ce9f9c2522aacc04c307bc
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-04-11 15:49:22 +0300

    Update fact_deals_change.sql

--- analytics/dbt/models/spark/b2b_mart/fact_deals_change.sql
+++ analytics/dbt/models/spark/b2b_mart/fact_deals_change.sql
@@ -94,7 +94,7 @@ from {{ ref('users_daily_table') }}
 
 select t1.*, 
 CASE WHEN 
-MAX(status_int > 100 and (prev_status_int is null or prev_status_int = 10)) OVER (PARTITION BY t1.deal_id)
+MAX(status_int > 100 and (prev_status_int is null or prev_status_int = 10) OVER (PARTITION BY t1.deal_id))
 THEN "fast_rejected" ELSE "normal" end as deal_normality,
 d.interaction_id, d.user_id, d.estimated_gmv, d.deal_type,
 i.source, i.type, i.campaign,
