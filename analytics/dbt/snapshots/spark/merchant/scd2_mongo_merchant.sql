c1c66840e7917b8d23d586ef162c28567e5fecf1
gburg@joom.com <gburg@joom.com>
2023-04-21 10:57:29 +0000

    Create scd2_mongo_merchant model

---
+++ analytics/dbt/snapshots/spark/merchant/scd2_mongo_merchant.sql
@@ -0,0 +1,28 @@
+{% snapshot scd2_mongo_merchant %}
+
+{{
+    config(
+      target_schema='merchant',
+      unique_key='merchant_id',
+      strategy='timestamp',
+      updated_at='updated_time',
+      file_format='delta',
+      meta = {
+          'bigquery_load': 'true',
+      }
+    )
+}}
+
+
+select merchant_id,
+       created_time,
+       updated_time,
+       activation_time,
+       name,
+       origin,
+       enabled,
+       disablingReason,
+       disablingNote
+from {{ref('merchant')}}
+
+{% endsnapshot %}
