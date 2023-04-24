2e823976e7f487d1baa75592f4051609c939f3fa
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-04-24 18:13:00 +0300

    Create scd2_mongo_order_products.sql

---
+++ analytics/dbt/snapshots/spark/b2b_mart/scd2_mongo_order_products.sql
@@ -0,0 +1,46 @@
+{% snapshot scd2_mongo_order_products %}
+
+{{
+    config(
+      target_schema='b2b_mart',
+      unique_key='merchant_order_id',
+
+      strategy='timestamp',
+      updated_at='update_ts_msk',
+      file_format='delta',
+      invalidate_hard_deletes=True,
+    )
+}}
+
+
+SELECT _id AS                              order_product_id,
+        id AS product_id,
+       attachments AS attachments,
+       currency AS currency,
+       dangerKindSet AS danger_kind_set,
+       dealId AS deal_id,
+       description,
+       disabled,
+       duty,
+       exportType AS export_type,
+       friendlyId AS product_friendly_id,
+       hsCode AS hs_code,
+       link,
+       linkToCalculation AS link_to_calculation,
+       manufacturerId AS manufacturer_id,
+       merchOrdId AS merchant_order_id,
+       name AS name,
+       nameRu AS name_ru,
+       packaging,
+       prices,
+       productionDeadLine as production_dead_line,
+       psiStatusID AS psi_status_id,
+       status,
+       statuses,
+       trademark,
+       type,
+       variants,
+       vatRate AS vat_rate
+from mongo.b2b_core_order_products_daily_snapshot
+
+{% endsnapshot %}
