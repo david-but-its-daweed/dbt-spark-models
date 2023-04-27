7022e1ce9c5a7318ccf611caec7ac9cc77e6830f
gburg@joom.com <gburg@joom.com>
2023-04-27 14:52:00 +0000

    Add holistics reports to slo tables

--- analytics/dbt/models/spark/platform/slo_tables.sql
+++ analytics/dbt/models/spark/platform/slo_tables.sql
@@ -7,5 +7,10 @@
     tags=['data_readiness']
 ) }}
 
-SELECT *
+select slo_id, full_table_name as table_name, "bq" as table_type
+from {{ref("slo_details_seed")}}
+         inner join platform.holistics_dashboards on business_name = dashboard_title
+where full_table_name is not null
+    UNION ALL
+SELECT slo_id, table_name, table_type
 FROM {{ref("slo_tables_seed")}}
