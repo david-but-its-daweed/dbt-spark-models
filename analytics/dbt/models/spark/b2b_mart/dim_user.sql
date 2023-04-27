f19f3ef512d76721ac97928aaebf0d77eb9d2bd6
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-04-27 16:29:11 +0300

    Update dim_user.sql

--- analytics/dbt/models/spark/b2b_mart/dim_user.sql
+++ analytics/dbt/models/spark/b2b_mart/dim_user.sql
@@ -7,8 +7,17 @@
       'priority_weight': '150'
     }
 ) }}
+
+WITH requests AS (
+    SELECT
+        user_id,
+        MAX(is_joompro_employee) AS is_joompro_employee
+    FROM {{ ref('fact_user_request') }}
+    group by user_id
+)
+
 SELECT
-  user_id,
+  t.user_id,
   anon AS is_anonymous,
   TIMESTAMP(created_ts_msk) AS created_ts_msk,
   TIMESTAMP(update_ts_msk) AS update_ts_msk,
@@ -19,7 +28,11 @@ SELECT
   amo_crm_id,
   amo_id,
   invitedByPromo as invited_by_promo,
+  isPartner as is_partner,
+  coalesce(is_joompro_employee, FALSE) as is_joompro_employee,
+  is_partner or coalesce(is_joompro_employee, FALSE) as fake,
   TIMESTAMP(dbt_valid_from) AS effective_ts_msk,
   TIMESTAMP(dbt_valid_to) AS next_effective_ts_msk
 FROM {{ ref('scd2_mongo_user') }} t
+left join requests as r on t.user_id = r.user_id
 where isPartner is null
