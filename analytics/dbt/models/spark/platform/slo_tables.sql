26aabd4ee8c80f10e9cec72f7b2dd03bce47ba97
gburg@joom.com <gburg@joom.com>
2023-05-02 13:49:24 +0000

    Use holistics_dashboards in slo_tables

--- analytics/dbt/models/spark/platform/slo_tables.sql
+++ analytics/dbt/models/spark/platform/slo_tables.sql
@@ -11,6 +11,7 @@ select slo_id, full_table_name as table_name, "bq" as table_type
 from {{ref("slo_details_seed")}}
          inner join platform.holistics_dashboards on business_name = dashboard_title
 where full_table_name is not null
+and not( slo_id = 'slo_istaff' and full_table_name = 'onfy_mart.ads_spends')
     UNION ALL
 SELECT slo_id, table_name, table_type
 FROM {{ref("slo_tables_seed")}}
