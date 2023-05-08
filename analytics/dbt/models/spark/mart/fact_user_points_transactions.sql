aeafa5f4d9ed58da3495c0e2a5c9d662837a8869
Denis Goihburg <gburg@joom.com>
2023-05-08 12:57:48 +0100

    move mart.fact_user_points_transactions to DBT

---
+++ analytics/dbt/models/spark/mart/fact_user_points_transactions.sql
@@ -0,0 +1,22 @@
+{{ config(
+    schema='mart',
+    materialized='view',
+    meta = {
+      'bigquery_load': 'true',
+      'bigquery_partitioning_date_column': 'date_msk'
+    }
+) }}
+
+SELECT _id                                                                                    as id,
+       userId                                                                                 as user_id,
+       kind,
+       type,
+       cast((effectiveUSD / 1000000) as decimal(38, 6))                                       as effective_usd,
+       hidden,
+       refId                                                                                  as refid,
+       to_date(cast(cast(conv(substring(_id, 0, 8), 16, 10) as bigint) + 10800 as timestamp)) as date_msk,
+       amount,
+       badOrderId                                                                             as bad_order_id,
+       index,
+       pending
+FROM mongo.user_points_transactions_daily_snapshot
