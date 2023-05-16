e5b78c9181d3059a30b6135d86578d637bcbb712
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-05-16 13:02:09 +0300

    Customers (#16963)
    * Create scd2_mongo_customer_schemes.sql
    
    * Create scd2_mongo_customer_markets.sql (#16959)
    
    * Delete scd2_mongo_customer_schemes.sql (#16958)
    
    * Delete scd2_mongo_customer_markets.sql (#16960)
    
    * Create scd2_mongo_customer_main_info.sql (#16961)
    
    * Delete scd2_mongo_customer_main_info.sql (#16962)

---
+++ analytics/dbt/models/spark/b2b_mart/scd2/scd2_mongo_customer_markets.sql
@@ -0,0 +1,30 @@
+{{ config(
+    schema='b2b_mart',
+    materialized='table',
+    file_format='parquet',
+    meta = {
+      'bigquery_load': 'false'
+    }
+) }}
+
+
+SELECT concat_ws('-', customer_id, COALESCE(market_id,0), utms) as id,
+    customer_id,
+    update_ts_msk,
+    market_id,
+    dbt_scd_id,
+    dbt_updated_at,
+    dbt_valid_from,
+    dbt_valid_to
+FROM (
+    SELECT
+       _id                          AS customer_id,
+       utms,
+       millis_to_ts_msk(utms)       AS update_ts_msk,
+       explode(markets)             AS market_id,
+       dbt_scd_id,
+       dbt_updated_at,
+       dbt_valid_from,
+       dbt_valid_to
+    FROM {{ ref('scd2_customers_snapshot') }}
+ )
---
+++ analytics/dbt/models/spark/b2b_mart/scd2/scd2_mongo_customer_schemes.sql
@@ -0,0 +1,30 @@
+{{ config(
+    schema='b2b_mart',
+    materialized='table',
+    file_format='parquet',
+    meta = {
+      'bigquery_load': 'false'
+    }
+) }}
+
+
+SELECT concat_ws('-', customer_id, COALESCE(schema_id,0), utms) as id,
+    customer_id,
+    update_ts_msk,
+    schema_id,
+    dbt_scd_id,
+    dbt_updated_at,
+    dbt_valid_from,
+    dbt_valid_to
+FROM (
+    SELECT
+        _id                          AS customer_id,
+       utms,
+       millis_to_ts_msk(utms)       AS update_ts_msk,
+       explode(schemes)             AS schema_id
+       dbt_scd_id,
+       dbt_updated_at,
+       dbt_valid_from,
+       dbt_valid_to
+    FROM {{ ref('scd2_customers_snapshot') }}
+ )
