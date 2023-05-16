bb28467477d53fa0fd9d65d06130912f39f8b398
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-05-16 09:59:24 +0300

    Create scd2_mongo_customer_categories.sql

---
+++ analytics/dbt/models/spark/b2b_mart/scd2/scd2_mongo_customer_categories.sql
@@ -0,0 +1,30 @@
+{{ config(
+    schema='b2b_mart',
+    materialized='table',
+    file_format='parquet',
+    meta = {
+      'bigquery_load': 'false'
+    }
+) }}
+
+
+SELECT concat_ws('-', customer_id, COALESCE(good_source_id,0), utms) as id,
+    customer_id,
+    update_ts_msk,
+    good_source_id,
+    dbt_scd_id,
+    dbt_updated_at,
+    dbt_valid_from,
+    dbt_valid_to
+FROM (
+    SELECT
+       _id                              AS customer_id,
+       utms,
+       millis_to_ts_msk(utms)           AS update_ts_msk,
+       explode(goodsSource)             AS good_source_id,
+       dbt_scd_id,
+       dbt_updated_at,
+       dbt_valid_from,
+       dbt_valid_to
+    FROM {{ ref('scd2_customers_snapshot') }}
+ )
