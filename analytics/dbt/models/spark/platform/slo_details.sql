223c1fadfb1c37de59e9cb507abd82351140fd2f
vladimir@joom.com <vladimir@joom.com>
2023-05-23 18:05:00 +0000

    slo_details: use float for hours

--- analytics/dbt/models/spark/platform/slo_details.sql
+++ analytics/dbt/models/spark/platform/slo_details.sql
@@ -9,5 +9,5 @@
 ) }}
 
 SELECT *,
-  cast(expected_time_utc_hours_str as int) as expected_time_utc_hours
+  cast(expected_time_utc_hours_str as float) as expected_time_utc_hours
 FROM {{ref("slo_details_seed")}}
