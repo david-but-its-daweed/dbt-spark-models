acd1c3b6d7c42ccb285e42646562111d36cc97c0
anna zaychik <67829640+anyazay@users.noreply.github.com>
2023-05-23 12:19:37 +0200

    Update onfy_price_comparison.sql

--- analytics/dbt/models/spark/onfy/onfy_price_comparison.sql
+++ analytics/dbt/models/spark/onfy/onfy_price_comparison.sql
@@ -1,3 +1,13 @@
+{{ config(
+    schema='onfy',
+    materialized='table',
+    meta = {
+      'team': 'onfy',
+      'bigquery_load': 'true',
+      'alerts_channel': 'onfy-etl-monitoring'
+    }
+) }}
+
 -- calculating orders per day
 with products_day as
 (
