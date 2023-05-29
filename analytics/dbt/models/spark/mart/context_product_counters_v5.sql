ee4a5c7a5ef2ee34f49f903837fde385b1204ce5
Denis Goihburg <gburg@joom.com>
2023-05-29 17:59:22 +0100

    Remove context_product_counters_v5 override_dataset_id
    Replaced it with dbt view.
    It confuses dependency graph and SLO monitoring

---
+++ analytics/dbt/models/spark/mart/context_product_counters_v5.sql
@@ -0,0 +1,13 @@
+{{ config(
+    schema='mart',
+    materialized='view',
+    meta = {
+      'bigquery_load': 'true'
+      'bigquery_clustering_columns': ['context_name']
+      'bigquery_partitioning_date_column': 'partition_date'
+      'bigquery_upload_horizon_days': '3'
+    }
+) }}
+
+select *
+FROM platform.context_product_counters_v5
