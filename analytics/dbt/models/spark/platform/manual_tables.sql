452d64a36840010bf2b9bfcdffbd6ac0e1377497
Denis Goihburg <gburg@joom.com>
2023-06-17 15:30:43 +0100

    Exclude manual tables from table_dependencies_v2

---
+++ analytics/dbt/models/spark/platform/manual_tables.sql
@@ -0,0 +1,17 @@
+{{ config(
+    schema='platform',
+    materialized='view',
+) }}
+
+select split(table_name, '[.]')[0] as db,
+       split(table_name, '[.]')[1] as table_name,
+       table_name                as full_table_name,
+       'spark'                   as type
+from platform.manual_tables_seed
+union all
+select split(table_name, '[.]')[0] as db,
+       split(table_name, '[.]')[1] as table_name,
+       table_name                as full_table_name,
+       'bq'                      as type
+from platform.manual_tables_bq_seed
+
