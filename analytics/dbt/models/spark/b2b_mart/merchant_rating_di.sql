f48b5d1b3c08557ce5e29898b2bd754c8a7c6849
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-06-19 10:42:56 +0300

    Update merchant_rating_di.sql

--- analytics/dbt/models/spark/b2b_mart/merchant_rating_di.sql
+++ analytics/dbt/models/spark/b2b_mart/merchant_rating_di.sql
@@ -7,7 +7,7 @@
       'team': 'general_analytics',
       'bigquery_load': 'true',
       'bigquery_partitioning_date_column': 'partition_date_msk',
-      'bigquery_known_gaps': ['2023-01-27']
+      'bigquery_known_gaps': ['2023-01-27', '2023-06-14']
     }
 ) }}
 
