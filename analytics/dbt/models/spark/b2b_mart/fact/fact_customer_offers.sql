5429e1d248411cfcd0150c49e6f9af3a6b54fc6d
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-06-21 13:52:55 +0300

    Update fact_customer_offers.sql

--- analytics/dbt/models/spark/b2b_mart/fact/fact_customer_offers.sql
+++ analytics/dbt/models/spark/b2b_mart/fact/fact_customer_offers.sql
@@ -15,12 +15,14 @@ comment,
 csmrReqId as customer_request_id,
 dealId as deal_id,
 merchantId as merchant_id,
-offerType as offer_type,
+ot.type as offer_type,
 orderId as order_id,
-status,
+ko.status,
 userId as user_id,
 brokerProps.link as broker_link,
 millis_to_ts_msk(ctms) as created_time,
 millis_to_ts_msk(utms) as updated_time
-from {{ ref('scd2_customer_offers_snapshot') }}
+from {{ ref('scd2_customer_offers_snapshot') }} co
+left join {{ ref('key_offer_status') }} ko on co.status = cast(ko.id as int)
+left join {{ ref('key_offer_type') }} ot on co.offerType = cast(ot.id as int)
 where dbt_valid_to is null
