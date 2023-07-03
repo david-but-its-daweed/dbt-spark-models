4a56ee83bfc2d23e321a00fce07e2dd9c2485a72
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-07-03 23:15:59 +0300

    Update key_funnel_status.sql

--- analytics/dbt/models/spark/b2b_mart/key/key_funnel_status.sql
+++ analytics/dbt/models/spark/b2b_mart/key/key_funnel_status.sql
@@ -9,7 +9,7 @@
 ) }}
 
 
-select key as id, value as status
+select key as id, upper(substr(value, 0, 1))||(substr(value, 2, length(value) - 1 )) as status
 from (
     select explode(values)
     from {{ source('mongo', 'b2b_core_enumregistry_daily_snapshot') }}
