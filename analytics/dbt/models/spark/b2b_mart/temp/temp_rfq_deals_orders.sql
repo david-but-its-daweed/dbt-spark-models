53390e593651607eec228673b5c6f57ffc4b8a29
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-07-10 14:38:44 +0300

    Update temp_rfq_deals_orders.sql

--- analytics/dbt/models/spark/b2b_mart/temp/temp_rfq_deals_orders.sql
+++ analytics/dbt/models/spark/b2b_mart/temp/temp_rfq_deals_orders.sql
@@ -36,7 +36,7 @@ group by 1, 2, 3, 4, 5, 6),
 
 order_product as (select order_id, product_id, deal_id, 1 as order_product
     from {{ ref('dim_deal_products') }}
-    where order_id is not null
+    where order_id is not null and product_id is not null
     ),
 
 rfq_order as (
@@ -57,7 +57,7 @@ status,
 pId as product_id,
 coalesce(order_product, 0) as order_product,
 null as offer_product,
-coalesce(converted, 0) as converted,
+case when converted = 1 and order_product = 1 and opp.product_id is not null then 1 else 0 end as converted,
 rejectReason as reject_reason,
 rejectReasonGroup as reject_reason_group
 from
@@ -69,6 +69,7 @@ left join (
     where last_interaction_type) a on o.user_id = a.user_id
 left join order_product op on o.order_id = op.order_id and rr.pId = op.product_id
 left join (select distinct order_id, 1 as converted from {{ ref('gmv_by_sources') }}) g on o.order_id = g.order_id
+left join (select distinct order_id, product_id from {{ ref('order_product_prices') }}) opp on opp.order_id = g.order_id and opp.product_id = rr.pId
 where created_time >= date('2023-01-01')
 order by created_time desc
 ),
