4f9e8deda09294136e33680aab08918bfab33d45
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-07-11 11:49:58 +0300

    Update jp_sla_logistics.sql

--- analytics/dbt/models/spark/b2b_mart/jp_sla_logistics.sql
+++ analytics/dbt/models/spark/b2b_mart/jp_sla_logistics.sql
@@ -221,21 +221,9 @@ SELECT DISTINCT
     oh.customs_declaration_released,
     oh.upload_to_temporary_warehouse,
     oh.delivering,
-    oh.delivered,
-    curr.current_status_days,
-    curr.current_status_declared_days
+    oh.delivered
 FROM orders AS o
 INNER JOIN orders_hist AS oh ON o.order_id = oh.order_id
 LEFT JOIN linehaul AS l ON o.linehaul_channel_id = l.id
 LEFT JOIN merchant_order AS mo ON o.order_id = mo.order_id
-LEFT JOIN (
-    SELECT DISTINCT
-        order_id,
-        current_status_days,
-        current_status_declared_days
-     FROM {{ ref('forecast_warehousing') }}
-     WHERE partition_date_msk = (
-         SELECT MAX(partition_date_msk)
-         FROM {{ ref('forecast_warehousing') }})
-) curr on curr.order_id = o.order_id
 WHERE oh.manufacturing IS NOT NULL
