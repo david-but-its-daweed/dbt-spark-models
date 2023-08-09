cdbe3b1cf2a70e1adec1a7b9a071e59988bfa75e
msafonov <msaf@joom.com>
2023-08-09 15:52:00 +0400

    Enable persisting docs

---
+++ models/spark/testing_persisting_docs/table_with_comments.sql
@@ -0,0 +1,12 @@
+{{ config(
+    schema='msaf',
+    materialized='table',
+    file_format='delta',
+    meta = {
+      'team': 'msaf',
+    }
+) }}
+select
+    1 as field1,
+    'a' as field2
+    ;
