b2881fcef25ef9e970bf653f509bc358596fcfaf
msafonov <msaf@joom.com>
2023-08-15 23:31:31 +0400

    fix datetime_trunc

--- models/spark/gold/gold_merchants.sql
+++ models/spark/gold/gold_merchants.sql
@@ -13,7 +13,7 @@ SELECT
     merchant_id,
     name AS merchant_name,
     origin_name,
-    DATETIME(TIMESTAMP_MILLIS(created_time)) AS created_datetime_utc,
-    FROM_UNIXTIME(updated_time / 1000, 'yyyy-MM-dd HH:mm:ss') AS updated_datetime_utc,
+    cast(created_time / 1000 as timestamp) AS created_datetime_utc,
+    cast(updated_time / 1000 as timestamp) AS updated_datetime_utc,
     enabled AS is_enabled
 FROM {{ source('mart', 'dim_merchant') }}
