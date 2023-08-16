68424fc6c91ae8b87f6f4210eccf52fac5c39dd3
anna zaychik <67829640+anyazay@users.noreply.github.com>
2023-08-16 14:28:57 +0200

    add promocodes to ads spends for cppu

--- models/spark/onfy/onfy_cppu_day.sql
+++ models/spark/onfy/onfy_cppu_day.sql
@@ -8,13 +8,30 @@
     }
 ) }}
 
-with numbered_purchases as 
+with promocodes as 
+(
+    select 
+        order_id,
+        sum(price) as discount_sum
+    from 
+        {{ source('onfy', 'transactions') }}
+    where 1=1
+        and type = 'DISCOUNT'
+        and currency = 'EUR'
+    group by 
+        order_id
+),
+
+numbered_purchases as 
 (
     select
-        *, 
-        rank() over (partition by user_email_hash order by created asc) as purchase_num
+        order.*, 
+        rank() over (partition by order.user_email_hash order by order.created asc) as purchase_num,
+        promocodes.discount_sum as discount_sum
     from 
-        {{ source('pharmacy_landing', 'order') }}
+        {{ source('pharmacy_landing', 'order') }} as order
+    left join promocodes
+        on order.id = promocodes.order_id
 ),
 
 first_second_purchases as 
@@ -24,7 +41,9 @@ first_second_purchases as
         from_utc_timestamp(first_purchases.created, 'Europe/Berlin') as first_purchase_date_ts_cet,
         first_purchases.device_id as first_purchase_device_id,
         from_utc_timestamp(second_purchases.created, 'Europe/Berlin') as second_purchase_date_ts_cet,
-        second_purchases.device_id as second_purchase_device_id
+        second_purchases.device_id as second_purchase_device_id,
+        first_purchases.discount_sum as first_purchase_discount_sum,
+        second_purchases.discount_sum as second_purchase_discount_sum
     from 
         numbered_purchases as first_purchases
     left join numbered_purchases as second_purchases
@@ -38,8 +57,8 @@ sources as
 (
     select 
         sources.user_email_hash,
-        sources.source_corrected,
-        sources.campaign_corrected,
+        lower(sources.source_corrected) as source_corrected,
+        lower(sources.campaign_corrected) as campaign_corrected,
         sources.partner,
         sources.source_app_device_type,
         sources.source_event_type,
@@ -65,7 +84,9 @@ sources as
             else 'other'
         end as second_purchase_app_device_type,
         source_ts_cet,
-        source_ts_utc
+        source_ts_utc,
+        first_second_purchases.first_purchase_discount_sum,
+        first_second_purchases.second_purchase_discount_sum
     from 
         {{ source('onfy', 'lndc_user_attribution') }} as sources
     join first_second_purchases 
@@ -79,8 +100,8 @@ sources as
 installs as 
 (
     select 
-        source_corrected,
-        campaign_corrected,
+        lower(sources.source_corrected) as source_corrected,
+        lower(sources.campaign_corrected) as campaign_corrected,
         if(lower(sources.source_corrected) = 'facebook', sources.medium, '') as medium,
         date_trunc('day', source_ts_utc) as source_day, 
         if(source_app_device_type like '%web%', 'web', source_app_device_type) as source_app_device_type,
@@ -89,8 +110,8 @@ installs as
     from 
         {{ source('onfy', 'lndc_user_attribution') }} as sources
     group by 
-        source_corrected,
-        campaign_corrected,
+        lower(sources.source_corrected),
+        lower(sources.campaign_corrected),
         if(lower(sources.source_corrected) = 'facebook', sources.medium, ''),
         date_trunc('day', source_ts_utc), 
         if(source_app_device_type like '%web%', 'web', source_app_device_type)
@@ -156,6 +177,8 @@ users_spends_campaigns as
         sources.source_app_device_type,
         sources.source_corrected,
         sources.campaign_corrected,
+        sources.first_purchase_discount_sum,
+        sources.second_purchase_discount_sum,
         coalesce(max(ads_spends_corrected.partition_date), date_trunc('day', sources.first_purchase_date_ts_utc)) as spend_date,
         coalesce(date_trunc('day', sources.first_purchase_date_ts_utc), max(ads_spends_corrected.partition_date)) as first_purchase_date
     from 
@@ -184,7 +207,9 @@ users_spends_campaigns as
         sources.second_purchase_app_device_type,
         sources.source_app_device_type,
         sources.source_corrected,
-        sources.campaign_corrected
+        sources.campaign_corrected,
+        sources.first_purchase_discount_sum,
+        sources.second_purchase_discount_sum
 ),
 
 users_by_day as 
@@ -201,7 +226,9 @@ users_by_day as
                 when users_spends_campaigns.second_purchase_date_ts_cet is not null
                 then users_spends_campaigns.user_email_hash
                 else null
-            end) as second_purchases
+            end) as second_purchases,
+        sum(first_purchase_discount_sum) as first_purchase_discount_sum,   
+        sum(second_purchase_discount_sum) as second_purchase_discount_sum
     from 
         users_spends_campaigns
     group by
@@ -238,12 +265,14 @@ select distinct
     coalesce(ads_spends_corrected_day.campaign_corrected, users_by_day.campaign_corrected, install.campaign_corrected) as campaign,
     coalesce(ads_spends_corrected_day.medium, users_by_day.medium, install.medium) as medium,
     coalesce(ads_spends_corrected_day.campaign_platform, users_by_day.first_purchase_app_device_type, install.source_app_device_type) as platform,
-    coalesce(users_by_day.first_purchases, 0) as first_purchases,
-    coalesce(users_by_day.second_purchases, 0) as second_purchases, 
-    coalesce(ads_spends_corrected_day.spend, 0) as spend,
-    coalesce(ads_spends_corrected_day.clicks, 0) as clicks,
-    coalesce(install.installs, 0) as installs,
-    coalesce(install.visits, 0) as visits
+    sum(coalesce(users_by_day.first_purchases, 0)) as first_purchases,
+    sum(coalesce(users_by_day.second_purchases, 0)) as second_purchases, 
+    sum(coalesce(ads_spends_corrected_day.spend, 0)) as spend,
+    sum(coalesce(ads_spends_corrected_day.clicks, 0)) as clicks,
+    sum(coalesce(install.installs, 0)) as installs,
+    sum(coalesce(install.visits, 0)) as visits,
+    sum(coalesce(first_purchase_discount_sum, 0)) as first_purchase_discount_sum,
+    sum(coalesce(second_purchase_discount_sum, 0)) as second_purchase_discount_sum
 from 
     ads_spends_corrected_day
 full join users_by_day
@@ -258,3 +287,9 @@ full join installs as install
     and lower(ads_spends_corrected_day.source_corrected) = lower(install.source_corrected)
     and lower(ads_spends_corrected_day.medium) = lower(install.medium)
     and lower(ads_spends_corrected_day.campaign_platform) = lower(install.source_app_device_type)
+group by 
+    coalesce(ads_spends_corrected_day.spend_day, users_by_day.first_purchase_date, install.source_day), 
+    coalesce(ads_spends_corrected_day.source_corrected, users_by_day.source_corrected, install.source_corrected),
+    coalesce(ads_spends_corrected_day.campaign_corrected, users_by_day.campaign_corrected, install.campaign_corrected),
+    coalesce(ads_spends_corrected_day.medium, users_by_day.medium, install.medium),
+    coalesce(ads_spends_corrected_day.campaign_platform, users_by_day.first_purchase_app_device_type, install.source_app_device_type)
