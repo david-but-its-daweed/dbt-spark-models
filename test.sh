7c62c3840737eca13f43712e6367c316c487617a
msafonov <msaf@joom.com>
2023-08-31 19:39:14 +0400

    Allow .ms files

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@
 DBT_DIR=`pwd`
 # Produce target/manifest.json
 cd $DBT_DIR
-WRONG_FILES=`find ./models/ -type f | grep -v .sql | grep -v yml | grep -v gitkeep`
-WRONG_FILES_COUNT=`find ./models/ -type f | grep -v .sql | grep -v yml | grep -v gitkeep | wc -l`
+WRONG_FILES=`find ./models/ -type f | grep -v .sql | grep -v yml | grep -v gitkeep | grep -v \.md`
+WRONG_FILES_COUNT=`find ./models/ -type f | grep -v .sql | grep -v yml | grep -v gitkeep | grep -v \.md | wc -l`
 if [ $WRONG_FILES_COUNT -ne 0 ]; then
         echo "All filenames should end with .sql. Found $WRONG_FILES_COUNT files with wrong filename: \n$WRONG_FILES"
         exit 1
