10a1af7a4cc25aee4a7561dc2511c91a23b43163
Denis Goihburg <gburg@joom.com>
2023-09-04 19:40:16 +0100

    Check if branch is up-to-date with master before prod dbt run

--- run_production.sh
+++ run_production.sh
@@ -20,4 +20,11 @@ DEFAULT_START_DATE=$(days_ago 1)
 DEFAULT_END_DATE=$(days_ago 0)
 DBT_VARS="{'start_date_ymd':'$DEFAULT_START_DATE','end_date_ymd':'$DEFAULT_END_DATE','table_name':'table_name'}"
 
+if [[ $(git fetch --all --prune && git diff origin/master) ]]; then
+    echo "Your brunch is not up-to-date with master"
+    exit 1
+else
+    echo "All up to date"
+fi
+
 dbt run --profiles-dir production/adhoc --vars $DBT_VARS $@
