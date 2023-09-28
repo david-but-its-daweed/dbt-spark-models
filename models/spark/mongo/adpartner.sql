46ea63ad94df1b4c80d42158d0e3374631b4d4b8
msafonov <msaf@joom.com>
2023-09-28 08:39:07 +0400

    PD: drop email from adpartner

--- models/spark/mongo/adpartner.sql
+++ models/spark/mongo/adpartner.sql
@@ -6,6 +6,6 @@
     materialized='view',
 ) }}
 select name,
-       email,
+       null AS email,
        transform(trackers, x -> named_struct( 'id', x._id, 'kind', x.kind)) as trackers
 from {{ source('mongo', 'events_ad_partners_daily_snapshot') }}
