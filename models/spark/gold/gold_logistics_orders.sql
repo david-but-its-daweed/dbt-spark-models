dd7f506e6256d8832afad58e85bec0cd0eb14ed3
nbragin@joom.com <nbragin@joom.com>
2023-10-06 16:06:20 +0000

    add flag of planning to consolidate

--- models/spark/gold/gold_logistics_orders.sql
+++ models/spark/gold/gold_logistics_orders.sql
@@ -40,6 +40,7 @@ SELECT
     a.category_id AS merchant_category_id,
     mc.business_line AS business_line,
 
+    a.consolidation_group_id IS NOT NULL AS is_planned_for_consolidation_by_hecny,
     a.is_consolidated AS is_consolidated_by_hecny,
     a.is_consolidated_by_merchant,
     a.is_refunded,
