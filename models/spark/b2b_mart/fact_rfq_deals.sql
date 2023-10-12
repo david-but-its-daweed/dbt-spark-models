eece48af4b30fc63e2a59ddfabe2c16221f21408
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-10-12 11:59:10 +0300

    Update fact_rfq_deals.sql

--- models/spark/b2b_mart/fact_rfq_deals.sql
+++ models/spark/b2b_mart/fact_rfq_deals.sql
@@ -137,7 +137,7 @@ left join (
 left join (
     select distinct _id as merchant_id,
     coalesce(companyName, name) as merchant_name
-    from {{ ref('dim_merchant') }}
+    from {{ source('b2b_mart', 'dim_merchant') }}
     where next_effective_ts >= current_timestamp()
 ) m on r.merchant_id = m.merchant_id
 
