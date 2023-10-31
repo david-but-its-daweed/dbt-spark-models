f3c7c769b6a75b3a59e35f6a960257d49d24c72a
Denis Goihburg <gburg@joom.com>
2023-10-31 17:11:11 +0000

    Move mart.context_device_counters_v6 upload to DBT

---
+++ models/spark/mart/context_device_counters_v6.sql
@@ -0,0 +1,25 @@
+{{ config(
+    schema='mart',
+    materialized='view',
+    meta = {
+      'model_owner' : '@gburg',
+      'bigquery_load': 'true',
+      'bigquery_overwrite': 'true',
+      'priority_weight' : '150',
+      'bigquery_clustering_columns' : ['type', 'context_name'],
+      'bigquery_partitioning_date_column' : 'partition_date',
+      'bigquery_upload_horizon_days' : '3',
+      'bigquery_cast_types_dict' : {'partition_date': joom_bq.CastToTypeEnum.DATE}
+    }
+) }}
+
+select device_id,
+       ephemeral,
+       context_name,
+       is_adtech_promoted,
+       is_buying_user,
+       is_last_context,
+       type,
+       count,
+       CAST(partition_date as DATE) partition_date
+FROM {{ source('recom', 'context_device_counters_v6') }}
