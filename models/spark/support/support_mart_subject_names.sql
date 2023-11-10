872d17bfe8d686d704584854f332b05ea5acfb6d
ilypavlov <iapavlov96@gmail.com>
2023-11-10 13:25:52 +0300

    Del jp support (#199)
    Co-authored-by: ilypavlov <ilypavlov@users.noreply.github.com>

--- models/spark/support/support_mart_subject_names.sql
+++ models/spark/support/support_mart_subject_names.sql
@@ -13,18 +13,19 @@
        'model_owner' : '@ilypavlov'
      }
  ) }}
- 
+
 WITH prebase_marketplace AS (
-SELECT
-    payload.ticketId AS ticket_id,
-    payload.changedByType AS state_owner,
-    EXPLODE(payload.tagIds) AS tag,
-    partition_date,
-    event_ts_msk AS event_ts_msk
-FROM {{ source('mart', 'babylone_events') }}
-WHERE `type` = 'ticketChangeJoom' 
-AND partition_date > '2021-12-31'
-AND payload.changedByType IN ('agent', 'automation')
+    SELECT
+        payload.ticketid AS ticket_id,
+        payload.changedbytype AS state_owner,
+        EXPLODE(payload.tagids) AS tag,
+        partition_date,
+        event_ts_msk AS event_ts_msk
+    FROM {{ source('mart', 'babylone_events') }}
+    WHERE
+        type = 'ticketChangeJoom'
+        AND partition_date > '2021-12-31'
+        AND payload.changedbytype IN ('agent', 'automation')
 )
 ,
 
@@ -39,9 +40,9 @@ t_marketplace AS (
         MIN(t.event_ts_msk) AS event_ts_msk
     FROM prebase_marketplace AS t
     LEFT JOIN {{ source('mongo', 'babylone_joom_tags_daily_snapshot') }} AS a ON t.tag = a._id
-    LEFT JOIN {{ source('mongo', 'babylone_joom_tag_categories_daily_snapshot') }} AS b ON a.categoryId = b._id
+    LEFT JOIN {{ source('mongo', 'babylone_joom_tag_categories_daily_snapshot') }} AS b ON a.categoryid = b._id
     GROUP BY 1, 2, 3, 4, 5, 6
- )
+)
 ,
 
 cnt_categories_marketplace AS (
@@ -68,49 +69,50 @@ tt_marketplace AS (
 ),
 
 base_marketplace AS (
-SELECT
-    t.ticket_id,
-    t.tag,
-    t.tag_type,
-    t.subject_category,
-    t.state_owner,
-    t.partition_date,
-    t.cnt_subject_categories,
-    MIN(t.event_ts_msk) AS event_ts_msk
-FROM tt_marketplace AS t
-GROUP BY 1, 2, 3, 4, 5, 6, 7
-    ),
+    SELECT
+        t.ticket_id,
+        t.tag,
+        t.tag_type,
+        t.subject_category,
+        t.state_owner,
+        t.partition_date,
+        t.cnt_subject_categories,
+        MIN(t.event_ts_msk) AS event_ts_msk
+    FROM tt_marketplace AS t
+    GROUP BY 1, 2, 3, 4, 5, 6, 7
+),
 
 marketplace AS (
-SELECT
-    'marketplace' AS bu,
-    t.ticket_id,
-    t.state_owner,
-    t.tag,
-    t.tag_type,
-    t.subject_category,
-    t.cnt_subject_categories,
-    TIMESTAMP(t.event_ts_msk) AS event_ts_msk,
-    t.partition_date,
-    a.first_queue,
-    a.current_queue,
-    a.country,
-    a.language
-FROM base_marketplace AS t
-LEFT JOIN {{ ref('support_mart_ticket_id_ext') }} AS a ON t.ticket_id = a.ticket_id
+    SELECT
+        'marketplace' AS bu,
+        t.ticket_id,
+        t.state_owner,
+        t.tag,
+        t.tag_type,
+        t.subject_category,
+        t.cnt_subject_categories,
+        TIMESTAMP(t.event_ts_msk) AS event_ts_msk,
+        t.partition_date,
+        a.first_queue,
+        a.current_queue,
+        a.country,
+        a.language
+    FROM base_marketplace AS t
+    LEFT JOIN {{ ref('support_mart_ticket_id_ext') }} AS a ON t.ticket_id = a.ticket_id
 ),
 
 prebase_jl AS (
-SELECT
-    payload.ticketId AS ticket_id,
-    payload.changedByType AS state_owner,
-    EXPLODE(payload.tagIds) AS tag,
-    partition_date,
-    event_ts_msk AS event_ts_msk
-FROM mart.logistics_babylone_events
-WHERE `type` = 'ticketChange' 
-AND partition_date > '2021-12-31'
-AND payload.changedByType IN ('agent', 'automation')
+    SELECT
+        payload.ticketid AS ticket_id,
+        payload.changedbytype AS state_owner,
+        EXPLODE(payload.tagids) AS tag,
+        partition_date,
+        event_ts_msk AS event_ts_msk
+    FROM mart.logistics_babylone_events
+    WHERE
+        type = 'ticketChange'
+        AND partition_date > '2021-12-31'
+        AND payload.changedbytype IN ('agent', 'automation')
 )
 ,
 
@@ -125,9 +127,9 @@ t_jl AS (
         MIN(t.event_ts_msk) AS event_ts_msk
     FROM prebase_jl AS t
     LEFT JOIN {{ source('mongo', 'babylone_logistics_tags_daily_snapshot') }} AS a ON t.tag = a._id
-    LEFT JOIN {{ source('mongo', 'babylone_logistics_tag_categories_daily_snapshot') }} AS b ON a.categoryId = b._id
+    LEFT JOIN {{ source('mongo', 'babylone_logistics_tag_categories_daily_snapshot') }} AS b ON a.categoryid = b._id
     GROUP BY 1, 2, 3, 4, 5, 6
- )
+)
 ,
 
 cnt_categories_jl AS (
@@ -150,139 +152,54 @@ tt_jl AS (
         a.cnt_subject_categories,
         FIRST_VALUE(t.state_owner) OVER (PARTITION BY t.ticket_id, t.tag, t.tag_type, t.subject_category, t.partition_date ORDER BY t.event_ts_msk) AS state_owner
     FROM t_jl AS t
-    LEFT JOIN cnt_categories_jl AS a ON t.ticket_id = a.ticket_id 
+    LEFT JOIN cnt_categories_jl AS a ON t.ticket_id = a.ticket_id
 ),
 
 base_jl AS (
-SELECT
-    t.ticket_id,
-    t.tag,
-    t.tag_type,
-    t.subject_category,
-    t.state_owner,
-    t.partition_date,
-    t.cnt_subject_categories,
-    MIN(t.event_ts_msk) AS event_ts_msk
-FROM tt_jl AS t
-GROUP BY 1, 2, 3, 4, 5, 6, 7
-    ),
-
-jl AS (
-SELECT
-    'logistics' AS bu,
-    t.ticket_id,
-    t.state_owner,
-    t.tag,
-    t.tag_type,
-    t.subject_category,
-    t.cnt_subject_categories,
-    TIMESTAMP(t.event_ts_msk) AS event_ts_msk,
-    t.partition_date,
-    a.first_queue,
-    a.current_queue,
-    a.country,
-    a.language
-FROM base_jl AS t
-LEFT JOIN {{ ref('support_mart_ticket_id_ext_jl') }} AS a ON t.ticket_id = a.ticket_id
-),
-
-prebase_joompay AS (
-SELECT
-    payload.ticketId AS ticket_id,
-    payload.changedByType AS state_owner,
-    EXPLODE(payload.tagIds) AS tag,
-    partition_date,
-    event_ts_msk AS event_ts_msk
-FROM mart.narwhal_babylone_events
-WHERE `type` = 'ticketChange' 
-AND partition_date > '2021-12-31'
-AND payload.changedByType IN ('agent', 'automation')
-)
-,
-
-t_joompay AS (
-    SELECT
-        t.ticket_id,
-        a.name AS tag,
-        a.type AS tag_type,
-        b.name AS subject_category,
-        t.state_owner AS state_owner,
-        t.partition_date,
-        MIN(t.event_ts_msk) AS event_ts_msk
-    FROM prebase_joompay AS t
-    LEFT JOIN {{ source('mongo', 'babylone_narwhal_tags_daily_snapshot') }} AS a ON t.tag = a._id
-    LEFT JOIN {{ source('mongo', 'babylone_narwhal_tag_categories_daily_snapshot') }} AS b ON a.categoryId = b._id
-    GROUP BY 1, 2, 3, 4, 5, 6
- )
-,
-
-cnt_categories_joompay AS (
-    SELECT
-        t.ticket_id,
-        COUNT(DISTINCT t.subject_category) AS cnt_subject_categories
-    FROM t_joompay AS t
-    GROUP BY 1
-)
-,
-
-tt_joompay AS (
     SELECT
         t.ticket_id,
         t.tag,
         t.tag_type,
         t.subject_category,
-        t.event_ts_msk,
+        t.state_owner,
         t.partition_date,
-        a.cnt_subject_categories,
-        FIRST_VALUE(t.state_owner) OVER (PARTITION BY t.ticket_id, t.tag, t.tag_type, t.subject_category, t.partition_date ORDER BY t.event_ts_msk) AS state_owner
-    FROM t_joompay AS t
-    LEFT JOIN cnt_categories_joompay AS a ON t.ticket_id = a.ticket_id
+        t.cnt_subject_categories,
+        MIN(t.event_ts_msk) AS event_ts_msk
+    FROM tt_jl AS t
+    GROUP BY 1, 2, 3, 4, 5, 6, 7
 ),
 
-base_joompay AS (
-SELECT
-    t.ticket_id,
-    t.tag,
-    t.tag_type,
-    t.subject_category,
-    t.state_owner,
-    t.partition_date,
-    t.cnt_subject_categories,
-    MIN(t.event_ts_msk) AS event_ts_msk
-FROM tt_joompay AS t
-GROUP BY 1, 2, 3, 4, 5, 6, 7
-    ),
-
-joompay AS (
-SELECT
-    'joompay' AS bu,
-    t.ticket_id,
-    t.state_owner,
-    t.tag,
-    t.tag_type,
-    t.subject_category,
-    t.cnt_subject_categories,
-    TIMESTAMP(t.event_ts_msk) AS event_ts_msk,
-    t.partition_date,
-    a.first_queue,
-    a.current_queue,
-    a.country,
-    a.language
-FROM base_joompay AS t
-LEFT JOIN {{ ref('support_mart_ticket_id_ext_joompay') }} AS a ON t.ticket_id = a.ticket_id
+jl AS (
+    SELECT
+        'logistics' AS bu,
+        t.ticket_id,
+        t.state_owner,
+        t.tag,
+        t.tag_type,
+        t.subject_category,
+        t.cnt_subject_categories,
+        TIMESTAMP(t.event_ts_msk) AS event_ts_msk,
+        t.partition_date,
+        a.first_queue,
+        a.current_queue,
+        a.country,
+        a.language
+    FROM base_jl AS t
+    LEFT JOIN {{ ref('support_mart_ticket_id_ext_jl') }} AS a ON t.ticket_id = a.ticket_id
 ),
 
 prebase_onfy AS (
-SELECT
-    payload.ticketId AS ticket_id,
-    payload.changedByType AS state_owner,
-    EXPLODE(payload.tagIds) AS tag,
-    event_ts_msk AS event_ts_msk,
-    partition_date
-FROM {{ source('mart', 'onfy_babylone_events') }}
-WHERE `type` = 'ticketChange' 
-AND partition_date > '2021-12-31'
-AND payload.changedByType IN ('agent', 'automation')
+    SELECT
+        payload.ticketid AS ticket_id,
+        payload.changedbytype AS state_owner,
+        EXPLODE(payload.tagids) AS tag,
+        event_ts_msk AS event_ts_msk,
+        partition_date
+    FROM {{ source('mart', 'onfy_babylone_events') }}
+    WHERE
+        type = 'ticketChange'
+        AND partition_date > '2021-12-31'
+        AND payload.changedbytype IN ('agent', 'automation')
 )
 ,
 
@@ -295,11 +212,11 @@ t_onfy AS (
         t.state_owner AS state_owner,
         t.partition_date,
         MIN(t.event_ts_msk) AS event_ts_msk
-    FROM prebase_jl AS t
+    FROM prebase_onfy AS t
     LEFT JOIN {{ source('mongo', 'babylone_onfy_tags_daily_snapshot') }} AS a ON t.tag = a._id
-    LEFT JOIN {{ source('mongo', 'babylone_onfy_tag_categories_daily_snapshot') }} AS b ON a.categoryId = b._id
+    LEFT JOIN {{ source('mongo', 'babylone_onfy_tag_categories_daily_snapshot') }} AS b ON a.categoryid = b._id
     GROUP BY 1, 2, 3, 4, 5, 6
- )
+)
 ,
 
 cnt_categories_onfy AS (
@@ -326,51 +243,44 @@ tt_onfy AS (
 ),
 
 base_onfy AS (
-SELECT
-    t.ticket_id,
-    t.tag,
-    t.tag_type,
-    t.subject_category,
-    t.state_owner,
-    t.partition_date,
-    t.cnt_subject_categories,
-    MIN(t.event_ts_msk) AS event_ts_msk
-FROM tt_onfy AS t
-GROUP BY 1, 2, 3, 4, 5, 6, 7
-    ),
+    SELECT
+        t.ticket_id,
+        t.tag,
+        t.tag_type,
+        t.subject_category,
+        t.state_owner,
+        t.partition_date,
+        t.cnt_subject_categories,
+        MIN(t.event_ts_msk) AS event_ts_msk
+    FROM tt_onfy AS t
+    GROUP BY 1, 2, 3, 4, 5, 6, 7
+),
 
 onfy AS (
-SELECT
-    'onfy' AS bu,
-    t.ticket_id,
-    t.state_owner,
-    t.tag,
-    t.tag_type,
-    t.subject_category,
-    t.cnt_subject_categories, 
-    TIMESTAMP(t.event_ts_msk) AS event_ts_msk,
-    t.partition_date,
-    a.first_queue,
-    a.current_queue,
-    a.country,
-    a.language
-FROM base_onfy AS t
-LEFT JOIN {{ ref('support_mart_ticket_id_ext_onfy') }} AS a ON t.ticket_id = a.ticket_id
+    SELECT
+        'onfy' AS bu,
+        t.ticket_id,
+        t.state_owner,
+        t.tag,
+        t.tag_type,
+        t.subject_category,
+        t.cnt_subject_categories,
+        TIMESTAMP(t.event_ts_msk) AS event_ts_msk,
+        t.partition_date,
+        a.first_queue,
+        a.current_queue,
+        a.country,
+        a.language
+    FROM base_onfy AS t
+    LEFT JOIN {{ ref('support_mart_ticket_id_ext_onfy') }} AS a ON t.ticket_id = a.ticket_id
 )
 
 
-SELECT
-    *
+SELECT *
 FROM marketplace
 UNION DISTINCT
-SELECT
-    *
+SELECT *
 FROM jl
 UNION DISTINCT
-SELECT
-    *
-FROM joompay
-UNION DISTINCT
-SELECT
-    *
+SELECT *
 FROM onfy
