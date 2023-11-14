5938d1bfdc8b9b9268d047700da0fbfbf54d6d61
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-11-14 11:32:01 +0300

    Update request_offer_product.sql (#216)

--- models/spark/b2b_mart/request_offer_product.sql
+++ models/spark/b2b_mart/request_offer_product.sql
@@ -34,4 +34,4 @@ op.type as product_type
 from {{ ref('fact_customer_requests') }} AS c
 left join {{ ref('fact_customer_offers') }} AS o ON c.customer_request_id = o.customer_request_id
 left join {{ ref('scd2_offer_products_snapshot') }} AS op ON o.offer_id = op.offer_id
-WHERE c.next_effective_ts_msk IS NULL AND o.next_effective_ts_msk IS NULL AND op.next_effective_ts_msk IS NULL
+WHERE c.next_effective_ts_msk IS NULL AND o.next_effective_ts_msk IS NULL AND TIMESTAMP(op.dbt_valid_to) IS NULL
