21a8f2cd0c9fb48202c4c276151149900aa423f3
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-11-24 14:56:55 +0300

    Create key_amocrm_source.sql (#238)

---
+++ models/spark/b2b_mart/key/key_amocrm_source.sql
@@ -0,0 +1,17 @@
+{{ config(
+    schema='b2b_mart',
+    materialized='table',
+    file_format='parquet',
+    meta = {
+      'model_owner' : '@amitiushkina',
+      'bigquery_load': 'true',
+      'priority_weight': '150'
+    }
+) }}
+
+select key as id, value as source
+from (
+    select explode(values)
+    from {{ source('mongo', 'b2b_core_enumregistry_daily_snapshot') }}
+    where key = 'amocrm.source'
+)
