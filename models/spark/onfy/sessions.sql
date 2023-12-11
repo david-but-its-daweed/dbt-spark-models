7143608b5b2d0cda1b616bb6629b16eca1a6cc9e
anna zaychik <67829640+anyazay@users.noreply.github.com>
2023-12-11 17:08:07 +0000

    Create sessions.sql (#283)
    add dataset for sessions (product + marketing)
    
    ---------
    
    Co-authored-by: anyazay <anyazay@users.noreply.github.com>

---
+++ models/spark/onfy/sessions.sql
@@ -0,0 +1,167 @@
+{{ config(
+    schema='onfy',
+    materialized='table',
+    meta = {
+      'model_owner' : '@annzaychik',
+      'team': 'onfy',
+      'bigquery_load': 'true',
+      'alerts_channel': 'onfy-etl-monitoring'
+    }
+) }}
+
+WITH interaction_events AS (
+    SELECT DISTINCT
+        event_ts_cet,
+        device_id,
+        type,
+        NULL AS order_id,
+        NULL AS gmv_initial,
+        NULL AS promocode_discount
+    FROM {{ source('onfy_mart', 'device_events') }}
+    WHERE
+        1 = 1
+        AND (
+            type IN (
+                'addToCart', 'cartInitiateCheckout', 'cartOpen', 'catalogOpen',
+                'homeOpen', 'paymentComplete', 'paymentStart', 'productOpen', 'search'
+            ) -- funnel events
+            OR
+            type IN (
+                'aboutUsOpen', 'adsBannerClick', 'adsBannerShow', 'allowPushPushNotificationInApp',
+                'alternativePackagingChoose', 'analogsOpen', 'bannerPageAction', 'bonusesPromoScreenOpen',
+                'cartAddToFreeDeliveryClick', 'checkoutConfirmOpen', 'compilationOpen', 'contactsOpen',
+                'cookieBannerClick', 'faqSectionExpand', 'fastDeliveryButtonClick', 'filterDiscard',
+                'filterDiscardAll', 'filterListChange', 'filterRangeChange', 'getPrescriptionButtonClick',
+                'goToCartClick', 'imprintOpen', 'logInOpen', 'logOut', 'orderStatusDetailsClick',
+                'orderTrackingOpen', 'ordersListProductsScreenOpen', 'partnersOpen', 'pharmacyStockOpen',
+                'privacyOpen', 'productPictureOpen', 'productReviewFormOpen', 'productReviewFormSubmit',
+                'productShare', 'profilePageOpen', 'promocodeCopyClick', 'promocodeFieldActivate',
+                'promocodeInfoOpen', 'promocodesPageOpen', 'registrationFlowAction', 'removeFromCart',
+                'savePaymentMethod', 'searchInputClick', 'termsOpen', 'tileClick', 'verificationCodeValidation'
+            ) -- general activity events
+        )
+    UNION ALL
+    SELECT
+        transaction_date,
+        device_id,
+        'purchase_server' AS type,
+        order_id,
+        SUM(gmv_initial) AS gmv_initial,
+        SUM(IF(type = 'DISCOUNT', price, 0)) AS promocode_discount
+    FROM {{ source('onfy', 'transactions') }}
+    WHERE
+        1 = 1
+        AND currency = 'EUR'
+        AND device_id IS NOT NULL
+    GROUP BY
+        transaction_date,
+        device_id,
+        order_id
+
+),
+
+
+source_change_events AS (
+    SELECT
+        source_dt,
+        device_id,
+        type,
+        next_source_dt,
+        IF(LOWER(COALESCE(source_corrected, 'unknown')) NOT IN ('unknown', 'unmarked_facebook_or_instagram', 'e-rezept'), NULL, 1) AS session_starter,
+        source_corrected,
+        campaign_corrected,
+        utm_medium
+    FROM {{ source('onfy', 'sources') }}
+    WHERE 1 = 1
+),
+
+
+sessions_predata AS (
+    SELECT
+        event_ts_cet,
+        device_id,
+        type,
+        order_id,
+        gmv_initial,
+        promocode_discount,
+        NULL AS source_corrected,
+        NULL AS campaign_corrected,
+        NULL AS utm_medium,
+        LEAD(event_ts_cet) OVER (PARTITION BY device_id ORDER BY event_ts_cet, type) AS next_event_ts_cet,
+        NULL AS session_starter
+    FROM interaction_events
+    UNION ALL
+    SELECT
+        source_dt,
+        device_id,
+        type,
+        NULL AS order_id,
+        NULL AS gmv_initial,
+        NULL AS promocode_discount,
+        source_corrected,
+        campaign_corrected,
+        utm_medium,
+        next_source_dt,
+        session_starter
+    FROM source_change_events
+
+),
+
+
+first_sessions AS (
+    SELECT
+        IF(
+            ROW_NUMBER() OVER (PARTITION BY device_id ORDER BY event_ts_cet) = 1
+            OR LAG(event_ts_cet) OVER (PARTITION BY device_id ORDER BY event_ts_cet, type) + INTERVAL 30 MINUTES < event_ts_cet, 1, session_starter
+        ) AS session_starter,
+        event_ts_cet,
+        device_id,
+        type,
+        next_event_ts_cet,
+        order_id,
+        gmv_initial,
+        promocode_discount,
+        source_corrected,
+        campaign_corrected,
+        utm_medium
+    FROM sessions_predata
+    ORDER BY
+        device_id
+),
+
+sessions_calculation AS (
+    SELECT
+        COUNT(session_starter) OVER (PARTITION BY device_id ORDER BY event_ts_cet) AS session_num,
+        *
+    FROM first_sessions
+),
+
+output AS (
+    SELECT
+        session_num,
+        device_id,
+        CONCAT(device_id, '-', CAST(FIRST_VALUE(event_ts_cet) AS INT)) AS session_id,
+        FIRST_VALUE(source_corrected, TRUE) AS source,
+        IF(FIRST_VALUE(source_corrected) IN ('facebook', 'google', 'idealo', 's24', 'billiger', 'tiktok', 'bing', 'criteo', 'apomio', 'geizhals'), 'paid', 'free') AS channel_type,
+        FIRST_VALUE(campaign_corrected, TRUE) AS campaign,
+        FIRST_VALUE(utm_medium, TRUE) AS medium,
+        FIRST_VALUE(event_ts_cet) AS session_start,
+        LAST_VALUE(event_ts_cet) AS session_end,
+        FIRST_VALUE(type) AS starting_session_event,
+        LAST_VALUE(type) AS ending_session_event,
+        COUNT(type) AS events_in_session,
+        COUNT(DISTINCT type) AS unique_events_in_session,
+        ARRAY_AGG(order_id) AS orders_ids,
+        MIN(IF(type = 'purchase_server', event_ts_cet, NULL)) AS first_transaction_date,
+        MIN_BY(gmv_initial, IF(type = 'purchase_server', event_ts_cet, NULL)) AS first_transaction_gmv_initial,
+        COUNT(order_id) AS orders,
+        SUM(gmv_initial) AS gmv_initial,
+        SUM(promocode_discount) AS promocode_discount
+    FROM sessions_calculation
+    GROUP BY
+        session_num,
+        device_id
+)
+
+SELECT *
+FROM output
