f9c9db6e4b7b1792a25af7d36d408eadfd1b3bf4
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2023-12-25 12:41:38 +0300

    Create fact_amo_attribution_interaction.sql (#307)

---
+++ models/spark/b2b_mart/fact/fact_amo_attribution_interaction.sql
@@ -0,0 +1,48 @@
+{{ config(
+    schema='b2b_mart',
+    materialized='table',
+    file_format='parquet',
+    meta = {
+      'model_owner' : '@amitiushkina',
+      'bigquery_load': 'false',
+      'priority_weight': '150'
+    }
+) }}
+
+select
+    interaction_id,
+    interaction_created_at,
+    type,
+    source,
+    campaign,
+    amo_id,
+    months_between(next_interaction_ts, interaction_created_at) >= 6 as incorrect_attribution,
+    next_interaction_ts is null as last_interaction_type,
+    rn = 1 as first_interaction_type
+from
+(
+select
+interaction_id,
+interaction_created_at,
+type,
+source,
+campaign,
+amo_id,
+lead(interaction_created_at) over (partition by amo_id order by interaction_created_at) as next_interaction_ts,
+row_number() over (partition by amo_id order by interaction_created_at) as rn
+from
+(
+select distinct
+col._id as interaction_id,
+millis_to_ts_msk(col.createdAt) as interaction_created_at,
+col.interaction.type as type,
+col.interaction.source as source,
+col.interaction.campaign as campaign,
+leadId as amo_id
+from
+(
+select explode(interactionChangedEvents), leadId, interaction as current_interaction
+from {{ source('mongo', 'b2b_core_amo_crm_raw_leads_daily_snapshot') }}
+)
+)
+)
