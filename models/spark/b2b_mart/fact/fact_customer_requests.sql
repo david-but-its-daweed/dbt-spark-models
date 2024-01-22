19363cea70d1ffb7895742348086e64048b29b9c
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2024-01-22 11:16:16 +0300

    Update fact_customer_requests.sql (#398)

--- models/spark/b2b_mart/fact/fact_customer_requests.sql
+++ models/spark/b2b_mart/fact/fact_customer_requests.sql
@@ -9,12 +9,19 @@
 ) }}
 
 
+WITH country AS (
+    SELECT DISTINCT deal_id, country
+    FROM {{ ref('fact_deals') }}
+)
+
+
 select 
     _id as customer_request_id,
     categoryId as category_id,
     category_name,
     millis_to_ts_msk(ctms) as created_time,
     dealId as deal_id,
+    country,
     desc,
     expectedQuantity as expected_quantity,
     link,
@@ -32,11 +39,12 @@ select
     millis_to_ts_msk(utms) as updated_time,
     TIMESTAMP(dbt_valid_from) as effective_ts_msk,
     TIMESTAMP(dbt_valid_to) as next_effective_ts_msk
-from {{ ref('scd2_customer_requests_snapshot') }} rfq
+from {{ ref('scd2_customer_requests_snapshot') }} request
+left join country AS c ON request.dealId = c.deal_id
 left join 
 (
 select category_id, name as category_name
     from {{ source('mart', 'category_levels') }}
-) cat on rfq.categoryId = cat.category_id
-left join {{ ref('key_customer_request_status') }} k on rfq.status = cast(k.id as int)
-left join {{ ref('key_calc_price_types') }} c on rfq.priceType = cast(c.id as int)
+) cat on request.categoryId = cat.category_id
+left join {{ ref('key_customer_request_status') }} k on request.status = cast(k.id as int)
+left join {{ ref('key_calc_price_types') }} c on request.priceType = cast(c.id as int)
