9d179173060ae9cc5e94db250f8abddb8ae7404b
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2024-02-01 16:08:23 +0300

    Update fact_amo_crm_raw_leads.sql (#431)

--- models/spark/b2b_mart/fact/fact_amo_crm_raw_leads.sql
+++ models/spark/b2b_mart/fact/fact_amo_crm_raw_leads.sql
@@ -25,18 +25,36 @@ from {{ source('mongo', 'b2b_core_amo_crm_raw_leads_daily_snapshot') }}
 )
 
 
+
 select distinct
-        AccountId as account_id,
-        loss_reason,
-        companyId as company_id,
-        companyName as company_name,
-        coalesce(contactId, leadId) as contact_id,
-        millis_to_ts_msk(createdAt) as created_at,
-        max(type) over (partition by coalesce(contactId, leadId)) as type, 
-        max(source) over (partition by coalesce(contactId, leadId)) as source, 
-        max(campaign) over (partition by coalesce(contactId, leadId)) as campaign,
-        leadId as lead_id,
-        pipeline_name,
+    pipeline_id,
+    validation,
+    pipeline_name,
+    lead_id,
+    company_id,
+    company_name,
+    contact_id,
+    created_at,
+    created_ts_msk,
+    validated_ts_msk,
+    case when validated_ts_msk is not null then 'Validated'
+        when current_status_id = 143 then 'Rejected'
+        else 'In Progress' end as validation_status,
+    loss_reason,
+    type,
+    source,
+    campaign,
+    admin_email,
+    owner_id,
+    country,
+    current_status_id,
+    current_status,
+    current_status_ts,
+    funnel_status,
+    user_id
+from
+(
+select distinct
         pipelineId as pipeline_id,
         case when pipelineId in (
             '7249567',
@@ -46,34 +64,47 @@ select distinct
             '7403522',
             '7120186'
         ) then true else false end as validation,
+        pipeline_name,
+        leadId as lead_id,
+        companyId as company_id,
+        companyName as company_name,
+        coalesce(contactId, leadId) as contact_id,
+        millis_to_ts_msk(createdAt) as created_at,
+        millis_to_ts_msk(
+            min(
+                case when 
+                pipelineId = '7314451'
+                or (pipelineId = '7249567' and status in ('IN PROGRESS', 'In progress'))
+                or (pipelineId = '7553579' and status = 'In progress')
+                or pipelineId = '7120174'
+                or (pipelineId = '7403522' and status = 'Взят в работу')
+                or (pipelineId = '7120186' and status in ('Взят в работу', 'Взяли в работу'))
+                then coalesce(status_ts, createdAt) end
+            ) over (partition by coalesce(contactId, leadId))
+        ) as created_ts_msk,
+
+        millis_to_ts_msk(
+            min(
+            case when 
+                (pipelineId = '7314451' and status in ('Closed - won'))
+                or (pipelineId = '7249567' and status in ('Closed - won'))
+                or (pipelineId = '7120174' and status in ('Квалифицирован', 'Лид квалифицирован', 'Уточняем запрос клиента'))
+                or (pipelineId = '7403522' and status in ('Квалифицирован', 'Лид квалифицирован'))
+                or (pipelineId = '7120186' and status in ('Квалифицирован', 'Лид квалифицирован', 'Предложение отправлено'))
+            then status_ts end) 
+            over (partition by coalesce(contactId, leadId))
+        ) as validated_ts_msk,
+        loss_reason,
+    
+        max(type) over (partition by coalesce(contactId, leadId)) as type, 
+        max(source) over (partition by coalesce(contactId, leadId)) as source, 
+        max(campaign) over (partition by coalesce(contactId, leadId)) as campaign,
         responsibleUser as admin_email,
         _id as owner_id,
         country,
         current_status as current_status_id,
         max(case when status_id = current_status then status end) over (partition by coalesce(contactId, leadId)) as current_status,
         millis_to_ts_msk(max(case when status_id = current_status then status_ts end) over (partition by coalesce(contactId, leadId))) as current_status_ts,
-        
-        millis_to_ts_msk(min(
-        case when 
-        pipelineId = '7314451'
-        or (pipelineId = '7249567' and status in ('IN PROGRESS', 'In progress'))
-        or (pipelineId = '7553579' and status = 'In progress')
-        or pipelineId = '7120174'
-        or (pipelineId = '7403522' and status = 'Взят в работу')
-        or (pipelineId = '7120186' and status in ('Взят в работу', 'Взяли в работу'))
-        then coalesce(status_ts, createdAt) end
-        ) over (partition by coalesce(contactId, leadId))) as created_ts_msk,
-        
-        millis_to_ts_msk(min(
-        case when 
-            (pipelineId = '7314451' and status in ('discussions', 'Discussions', 'DISCUSSIONS'))
-            or (pipelineId = '7249567' and status in ('discussions', 'Discussions', 'DISCUSSIONS'))
-            or (pipelineId = '7120174' and status in ('Квалифицирован', 'Лид квалифицирован', 'Уточняем запрос клиента'))
-            or (pipelineId = '7403522' and status in ('Квалифицирован', 'Лид квалифицирован'))
-            or (pipelineId = '7120186' and status in ('Квалифицирован', 'Лид квалифицирован', 'Предложение отправлено'))
-        then status_ts end) 
-        
-        over (partition by coalesce(contactId, leadId))) as validated_ts_msk,
         max(funnel_status) over (partition by coalesce(contactId, leadId)) as funnel_status, 
         max(user_id) over (partition by coalesce(contactId, leadId)) as user_id
     from
@@ -125,3 +156,4 @@ select distinct
     left join (select distinct funnel_status, user_id, amo_id from {{ ref('fact_customers') }}) on leadId = amo_id
     ) left join statuses using (leadId)
     )
+)
