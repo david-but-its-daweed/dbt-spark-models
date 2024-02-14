1ace4f3769f73df80adb879dd07238f7246a7996
Ekaterina Saltykova <34067050+ea-sal@users.noreply.github.com>
2024-02-14 16:32:10 +0100

    PhAn-672-fixing-ctes (#458)

--- models/spark/onfy/orders_info.sql
+++ models/spark/onfy/orders_info.sql
@@ -18,9 +18,9 @@ WITH product_names_cte AS (
 
 devices_mart_cte AS (
     SELECT
-        ord.device_id,
-        ord.user_id,
-        ord.user_email_hash,
+        order.device_id,
+        order.user_id,
+        order.user_email_hash,
         CASE
             WHEN device.app_type = 'WEB' AND device.device_type = 'DESKTOP' THEN 'web_desktop'
             WHEN device.app_type = 'WEB' AND device.device_type IN ('PHONE', 'TABLET') THEN 'web_mobile'
@@ -28,22 +28,22 @@ devices_mart_cte AS (
             WHEN device.app_type = 'IOS' THEN 'ios'
             ELSE device.app_type || '_' || device.device_type
         END AS app_device_type,
-        ord.created AS order_dt,
-        MIN(ord.created) OVER (PARTITION BY ord.user_email_hash) AS min_purchase_ts
-    FROM {{ source('pharmacy_landing', 'order') }} AS ord
+        order.created AS order_dt,
+        MIN(order.created) OVER (PARTITION BY order.user_email_hash) AS min_purchase_ts
+    FROM {{ source('pharmacy_landing', 'order') }} AS order
     INNER JOIN {{ source('pharmacy_landing', 'device') }} AS device
-        ON device.id = ord.device_id
+        ON device.id = order.device_id
 )
 SELECT
     ord.user_id,
     ord.device_id,
-    devices_mart.user_email_hash,
-    devices_mart.app_device_type,
+    devices_mart_cte.user_email_hash,
+    devices_mart_cte.app_device_type,
     CASE
-        WHEN FROM_UTC_TIMESTAMP(ord.created, 'Europe/Berlin') > devices_mart.min_purchase_ts THEN 1
+        WHEN FROM_UTC_TIMESTAMP(ord.created, 'Europe/Berlin') > devices_mart_cte.min_purchase_ts THEN 1
         ELSE 0
     END AS is_buyer,
-    DENSE_RANK() OVER (PARTITION BY devices_mart.user_email_hash ORDER BY FROM_UTC_TIMESTAMP(ord.created, 'Europe/Berlin') ASC) AS purchase_num,
+    DENSE_RANK() OVER (PARTITION BY devices_mart_cte.user_email_hash ORDER BY FROM_UTC_TIMESTAMP(ord.created, 'Europe/Berlin') ASC) AS purchase_num,
     ord.id AS order_id,
     FROM_UTC_TIMESTAMP(ord.created, 'Europe/Berlin') AS order_created_time_cet,
     ord.city,
@@ -56,7 +56,7 @@ SELECT
     order_parcel.delivery_price AS parcel_delivery_price,
     order_parcel_item.product_id,
     medicine.country_local_id AS pzn,
-    product_names.product_name,
+    product_names_cte.product_name,
     order_parcel_item.quantity,
     CAST(order_parcel_item.price AS DOUBLE) AS item_price,
     CAST(order_parcel_item.price * order_parcel_item.quantity AS DOUBLE) AS products_price
@@ -78,10 +78,10 @@ LEFT JOIN {{ source('pharmacy_landing', 'medicine') }} AS medicine
         order_parcel_item.product_id = medicine.id
 INNER JOIN devices_mart_cte
     ON
-        ord.device_id = devices_mart.device_id
+        ord.device_id = devices_mart_cte.device_id
 INNER JOIN product_names_cte
     ON
-        product_names.product_id = order_parcel_item.product_id
+        product_names_cte.product_id = order_parcel_item.product_id
 LEFT JOIN {{ source('pharmacy_landing', 'order_paketshop') }} AS order_paketshop
     ON
         ord.id = order_paketshop.order_id
