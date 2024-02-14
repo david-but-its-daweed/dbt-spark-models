dbc559b9e002a5fb8a8ee506153f5e695acb7407
Artem Plotnikov <aplotnikov@joom.com>
2024-02-14 11:24:12 +0000

    JPA customer_360

---
+++ models/spark/jpa/customer_360.sql
@@ -0,0 +1,29 @@
+{{ config(
+    schema='joompro_analytics',
+    materialized='view',
+    meta = {
+      'model_owner': '@aplotnikov',
+      'bigquery_load': 'true',
+      'bigquery_project_id': 'joom-analytics-joompro-public',
+      'bigquery_table_name': 'joompro_analytics_internal_mart.customer_360',
+    }
+) }}
+
+WITH t AS (
+    SELECT
+        user_id,
+        COUNT(DISTINCT partition_date) user_activity_days,
+        APPROX_COUNT_DISTINCT(request_id) user_activity_total_requests,
+        MIN(partition_date) first_activity,
+        MAX(partition_date) last_activity
+    FROM {{ ref('fact_user_activity') }}
+    GROUP BY 1
+)
+SELECT
+    t.*,
+    welcome_form.mlhs as welcome_form_is_selling,
+    welcome_form.mlsl as welcome_form_shop_url
+FROM t
+LEFT JOIN {{ source('mongo', 'b2b_core_analytics_users_extras_daily_snapshot') }} welcome_form
+    ON user_id = _id
+
