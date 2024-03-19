2e1b6d89cf67899782c4b727fa8005387cfb19b0
Nikolay Gusev <36819289+g00sek@users.noreply.github.com>
2024-03-19 15:14:40 +0300

    :sparkles: created `gross_profit_estimation_change` (#516)
    [Тикет](https://joom-team.atlassian.net/jira/software/c/projects/AN/boards/123?quickFilter=280&selectedIssue=AN-3032)
    
    Создаю модель для использования в детекторе. В BQ буду смотреть, как
    меняется оценка на разных временных окнах по отношению к первой оценке
    
    ---------
    
    Co-authored-by: gusev@joom.com <gusev@joom.com>
    Co-authored-by: g00sek <g00sek@users.noreply.github.com>

---
+++ models/spark/anomaly_detection/gross_profit_estimation_change.sql
@@ -0,0 +1,43 @@
+{{
+  config(
+    materialized='table',
+    alias='gross_profit_estimation_change',
+    schema='models',
+    file_format='parquet',
+    meta = {
+        'model_owner' : '@gusev',
+        'bigquery_load': 'true',
+        'bigquery_overwrite': 'true'
+    }
+  )
+}}
+
+WITH main AS (
+    SELECT
+        cube_profit.t AS order_date_msk,
+        cube_profit.country AS country_code,
+        DATEDIFF(cube_profit.prediction_date, cube_profit.t) - 1 AS days_since_first_estimation,
+        SUM(cube_profit.order_gross_profit_final_estimated) AS order_gross_profit_final_estimated, -- оценка GP
+        MAX(
+            IF(
+                DATEDIFF(cube_profit.prediction_date, cube_profit.t) - 1 = 0,
+                SUM(cube_profit.order_gross_profit_final_estimated),
+                NULL
+            )
+        ) OVER (PARTITION BY cube_profit.t, cube_profit.country) AS order_gross_profit_final_estimated_first_estimation -- оценка GP на следующий день после даты создания заказа
+
+    FROM {{ source('cube', 'profit') }} AS cube_profit
+    INNER JOIN {{ ref('gold_countries') }} AS c
+        ON cube_profit.country = c.country_code -- оставляю только страны
+    WHERE cube_profit.t >= '2022-01-01'
+    GROUP BY 1, 2, 3
+    ORDER BY 1, 2, 3
+)
+
+SELECT *
+FROM main
+WHERE
+    TRUE
+    AND days_since_first_estimation <= 180
+    AND days_since_first_estimation > 0
+ORDER BY 1, 2, 3
