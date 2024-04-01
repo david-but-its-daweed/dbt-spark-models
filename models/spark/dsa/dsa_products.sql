047d987fab510ee7d640bb136573323bccc098e0
Nikolay Gusev <36819289+g00sek@users.noreply.github.com>
2024-04-01 12:37:51 +0300

    :bug: Update dsa_products.sql (#541)
    Co-authored-by: g00sek <g00sek@users.noreply.github.com>

--- models/spark/dsa/dsa_products.sql
+++ models/spark/dsa/dsa_products.sql
@@ -9,6 +9,6 @@
 
 SELECT DISTINCT
     product_id,
-    labels[0]['key'] = 'dangerousProductRecall' AS reason
+    labels[0]['key'] AS reason
 FROM {{ source('mart', 'published_products_current') }}
 WHERE labels[0]['key'] = 'dangerousProductRecall'
