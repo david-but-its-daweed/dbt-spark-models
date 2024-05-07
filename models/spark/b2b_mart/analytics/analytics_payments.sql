647c3d420febdf6d0d499a3f2a604c3f8d895cca
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2024-05-07 11:38:05 +0300

    Analytics Payments (#622)

---
+++ models/spark/b2b_mart/analytics/analytics_payments.sql
@@ -0,0 +1,27 @@
+{{ config(
+    schema='b2b_mart',
+    materialized='table',
+    file_format='parquet',
+    meta = {
+      'model_owner' : '@amitiushkina',
+      'bigquery_load': 'true',
+      'priority_weight': '150'
+    }
+) }}
+
+select
+        _id as payment_id,
+        millis_to_ts(createdTimeMs) as payment_created_time,
+        packageSnapshot._id as package_id,
+        packageSnapshot.duration.unit as package_duration_unit,
+        packageSnapshot.duration.value as package_duration,
+        packageSnapshot.price.amount/1000000 as package_price,
+        packageSnapshot.price.ccy as package_price_ccy,
+        millis_to_ts(paidTimeMs) as paid_time,
+        to_date(millis_to_ts(paidTimeMs)) as paid_date,
+        price.amount/1000000 as price,
+        price.ccy as currency,
+        promocodeSnapshot._id as promocode_id,
+        promocodeSnapshot.code as promocode,
+        status
+from {{ source('mongo', 'b2b_core_analytics_payments_daily_snapshot') }}
