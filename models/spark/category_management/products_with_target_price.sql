b2bb1f692d994b141e90462e59fd1266b8dc8735
EKutynina <68954157+EKutynina@users.noreply.github.com>
2024-05-09 00:05:35 +0100

    add manual product list and fix of the price spiral (#627)
    https://joom-team.atlassian.net/browse/AN-3245 - задача
    
    Также в ходе тестирования были выявлены проблемы:
    1. Мы ВСЕГДА запрашиваем скидку 5% от текущей цены, не важно есть ли уже
    аппрувнутый оффер - пофикшено тем, что теперь смотрим на цену мерчанта в
    периоды, когда у товара не было аппрувнутого пропозла.
    2. Есть баг в снижении цены после аппрува пропозлов (цена не снижается)
    - разработка занимается
    
    ---------
    
    Co-authored-by: EKutynina <EKutynina@users.noreply.github.com>

--- models/spark/category_management/products_with_target_price.sql
+++ models/spark/category_management/products_with_target_price.sql
@@ -29,7 +29,9 @@ WITH edlp_products AS (----------- a temporary filter: only for non edlp product
         AND partition_date >= DATE("2024-02-18") - INTERVAL 90 DAY
     {% endif %}
 ),
-
+------------------------------------------------------------------------------
+------------------Forming the list of automatically selected products---------
+------------------------------------------------------------------------------
 filtered_products AS (
     SELECT
         m.product_id,
@@ -47,7 +49,7 @@ filtered_products AS (
         c.merchant_cancel_rate_maximum AS criteria_merchant_cancel_rate_maximum,
         c.business_line,
         d.name AS merchant_name,
-        "automatically" AS reason_of_participation
+        1 AS reason_of_participation
     FROM {{ ref('initial_metrics_set') }} AS m
     LEFT JOIN {{ source('category_management', 'joom_select_manual_criteria') }} AS c ON m.main_category = c.category
     LEFT JOIN {{ source('mart', 'dim_merchant') }} AS d ON m.merchant_id = d.merchant_id
@@ -73,18 +75,183 @@ filtered_products AS (
     {% endif %}
         
 ),
+------------------------------------------------------------------------------
+------------------Forming the list of manually selected products--------------
+------------------------------------------------------------------------------
+manual_products AS (
+    SELECT
+        product_id,
+        MAX(discount) AS discount,
+        0 AS reason_of_participation
+    FROM {{ source('category_management', 'joom_select_product_white_list') }}
+    GROUP BY 1
+),
+
+all_products AS (
+    SELECT
+        product_id,
+        partition_date,
+        main_category,
+        merchant_id,
+        business_line,
+        merchant_name,
+        MAX(product_rating_60_days) AS product_rating_60_days,
+        MAX(gmv_60_days) AS gmv_60_days,
+        MAX(orders_60_days) AS orders_60_days,
+        MAX(orders_with_nf_share_1_year) AS orders_with_nf_share_1_year,
+        MAX(merchant_cancel_rate_1_year) AS merchant_cancel_rate_1_year,
+        MAX(merchant_price_index) AS merchant_price_index,
+        MAX(criteria_days_orders_minimum_60) AS criteria_days_orders_minimum_60,
+        MAX(criteria_days_gmv_minimum_60) AS criteria_days_gmv_minimum_60,
+        MAX(criteria_merchant_cancel_rate_maximum) AS criteria_merchant_cancel_rate_maximum,
+        MIN(reason_of_participation) AS reason_of_participation
+    FROM (
+        SELECT
+            product_id,
+            partition_date,
+            main_category,
+            merchant_id,
+            product_rating_60_days,
+            gmv_60_days,
+            orders_60_days,
+            orders_with_nf_share_1_year,
+            merchant_cancel_rate_1_year,
+            merchant_price_index,
+            criteria_days_orders_minimum_60,
+            criteria_days_gmv_minimum_60,
+            criteria_merchant_cancel_rate_maximum,
+            business_line,
+            merchant_name,
+            reason_of_participation
+        FROM filtered_products
+        UNION ALL
+        SELECT
+            p.product_id,
+            DATE('{{ var("start_date_ymd") }}') AS partition_date,
+            c.l1_merchant_category_name AS main_category,
+            p.merchant_id,
+            0 AS product_rating_60_days,
+            0 AS gmv_60_days,
+            0 AS orders_60_days,
+            0 AS orders_with_nf_share_1_year,
+            0 AS merchant_cancel_rate_1_year,
+            0 AS merchant_price_index,
+            0 AS criteria_days_orders_minimum_60,
+            0 AS criteria_days_gmv_minimum_60,
+            0 AS criteria_merchant_cancel_rate_maximum,
+            c.business_line,
+            dm.name AS merchant_name,
+            0 AS reason_of_participation
+        FROM {{ source('mart', 'dim_published_product_min') }} AS p
+        INNER JOIN manual_products AS m ON p.product_id = m.product_id
+        INNER JOIN {{ source('mart', 'dim_merchant') }} AS dm ON dm.merchant_id = p.merchant_id
+        INNER JOIN {{ ref('gold_merchant_categories') }} AS c ON p.category_id = c.merchant_category_id
+    )
+    GROUP BY
+        product_id,
+        partition_date,
+        main_category,
+        merchant_id,
+        business_line,
+        merchant_name
+),
 --------------------------------------------------------------------------
 -----------------------Forming target price------------------------
 --------------------------------------------------------------------------    
-
+---- we need to exclude periods, when proposal was approved
+---- otherwise we will demand discount from JS price over and over again
+approved_proposals_periods AS (
+    SELECT DISTINCT
+        product_id,
+        status_effective_from,
+        COALESCE(status_effective_to, "2999-01-01") AS status_effective_to
+    FROM {{ ref('js_proposal_backend_status_history_raw') }}
+    WHERE status = "approved"
+),
+--------------------------------------------------------------------------    
+---- As far as dim_published_variant_with_merchant contains 
+---- many lines for each variant with the same price,
+---- we shoud define effective period of a particular price for each variant
+variants_stg AS (
+    SELECT
+        variant_id,
+        product_id,
+        price,
+        currency,
+        value_partition,
+        MAX(current_price) AS current_price,
+        MIN(effective_ts) AS effective_ts,
+        MAX(next_effective_ts) AS next_effective_ts
+    FROM (
+        SELECT
+            variant_id,
+            product_id,
+            price,
+            currency,
+            effective_ts,
+            next_effective_ts,
+            SUM(value_partition) over (PARTITION BY variant_id, product_id ORDER BY effective_ts ) AS value_partition,
+            FIRST(price) OVER (PARTITION BY variant_id, product_id ORDER BY effective_ts DESC) AS current_price
+        FROM (
+            SELECT
+                variant_id,
+                product_id,
+                price,
+                currency,
+                effective_ts AS effective_ts,
+                next_effective_ts AS next_effective_ts,
+                CASE WHEN IF(price_lag IS NULL OR price_lag!= price, effective_ts, NULL) IS NULL THEN 0 ELSE 1 END AS value_partition    
+            FROM (
+                SELECT
+                    variant_id,
+                    product_id,
+                    price / 1000000 AS price,
+                    currency,
+                    effective_ts,
+                    next_effective_ts,
+                    LAG(p.price / 1000000) OVER (PARTITION BY variant_id, product_id ORDER BY next_effective_ts) AS price_lag
+                FROM {{ source('mart', 'dim_published_variant_with_merchant') }} AS p
+                )
+            )
+        )
+    GROUP BY 1, 2, 3, 4, 5
+),
+------------------------------------------------------------------------------    
+---- Then we should exclude price periods, when the product participated in JS 
+---- Otherwise we will demand discount over and over again 
 variants AS (
     SELECT
         variant_id,
         product_id,
-        price / 1000000 AS price,
-        currency
-    FROM {{ source('mart', 'dim_published_variant_with_merchant') }}
-    WHERE next_effective_ts > "9999-12-31"
+        price,
+        current_price,
+        currency,
+        effective_ts
+    FROM (
+        SELECT
+            p.variant_id,
+            p.product_id,
+            p.price AS price,
+            p.currency,
+            p.effective_ts,
+            p.current_price,
+            MAX(p.effective_ts) OVER (PARTITION BY p.variant_id, p.product_id) AS max_effective_ts
+        FROM variants_stg AS p
+        LEFT JOIN approved_proposals_periods AS ap
+            ON
+                p.product_id = ap.product_id
+                AND
+                    (
+                        (DATE(p.effective_ts - INTERVAL 3 HOURS) = DATE(ap.status_effective_from) 
+                        AND DATE(p.next_effective_ts - INTERVAL 3 HOURS) = DATE(ap.status_effective_to))
+                    OR 
+                        (DATE(p.effective_ts - INTERVAL 3 HOURS) = DATE(ap.status_effective_from) 
+                        AND DATE(p.next_effective_ts - INTERVAL 3 HOURS) = '9999-12-31')
+                    )
+        WHERE p.next_effective_ts > "2024-01-01"
+            AND ap.product_id IS NULL
+    )
+    WHERE effective_ts = max_effective_ts
 ),
 
 currency_rates AS (
@@ -95,13 +262,16 @@ currency_rates AS (
         next_effective_date
     FROM {{ source('mart', 'dim_currency_rate') }}
 ),
-
+------------------------------------------------------------------------------    
+---------- Collecting info about vatiants and merchant prices in usd  without promo
+------------------------------------------------------------------------------    
 products_n_variants AS (
     SELECT
         pt.*,
         v.variant_id,
-        v.price * c.rate AS current_price_usd
-    FROM filtered_products AS pt
+        v.price * c.rate AS last_not_js_price_usd,
+        v.current_price * c.rate AS current_price_usd
+    FROM all_products AS pt
     INNER JOIN variants AS v ON pt.product_id = v.product_id
     LEFT JOIN currency_rates AS c
         ON
@@ -109,16 +279,9 @@ products_n_variants AS (
             AND pt.partition_date >= c.effective_date
             AND pt.partition_date < c.next_effective_date
 ),
----- we need to exclude periods, when proposal was approved
----- otherwise we will demand discount from JS price over and over again
-approved_proposals_periods AS (
-    SELECT
-        product_id,
-        status_effective_from,
-        COALESCE(status_effective_to, "2999-01-01") AS status_effective_to
-    FROM {{ ref('js_proposal_backend_status_history_raw') }}
-    WHERE status = "approved"
-),
+------------------------------------------------------------------------------    
+---------- Collecting info about vatiants and prices in usd with promo -------
+------------------------------------------------------------------------------   
 
 prices AS (
     SELECT
@@ -145,7 +308,9 @@ prices AS (
         AND a.product_id IS NULL
     GROUP BY 1, 2, 3
 ),
-
+------------------------------------------------------------------------------    
+----------------- Collecting all vatiant's info together ----------------
+------------------------------------------------------------------------------  
 products_n_variants_n_prices AS (
     SELECT
         v.product_id,
@@ -166,9 +331,11 @@ products_n_variants_n_prices AS (
         MIN(v.criteria_days_gmv_minimum_60) AS criteria_days_gmv_minimum_60,
         MIN(v.criteria_merchant_cancel_rate_maximum) AS criteria_merchant_cancel_rate_maximum,
         MIN(v.current_price_usd) AS current_price_usd,
+        MIN(v.last_not_js_price_usd) AS last_not_js_price_usd,
         MIN(p.min_merchant_list_price) AS min_merchant_list_price,
         MIN(p.min_merchant_sale_price) AS min_merchant_sale_price,
-        MIN(d.rate) AS discount_rate -- The rate of an additional discount from the handbook https://docs.google.com/spreadsheets/d/1JXqKXvYhJcaZWf69e1G5EXB0sZady_EEM6pfeGk6JHU/edit#gid=0
+        MIN(COALESCE(m.discount, d.rate)) AS discount_rate -- The rate of an additional discount from the handbook https://docs.google.com/spreadsheets/d/1JXqKXvYhJcaZWf69e1G5EXB0sZady_EEM6pfeGk6JHU/edit#gid=0 
+                                                 -- or https://docs.google.com/spreadsheets/d/1fJpQl_JwumbWikePJkqkh3lhGXdmXf6B39VXmrFT1GI/edit#gid=0
     FROM products_n_variants AS v
     LEFT JOIN prices AS p
         ON
@@ -177,47 +344,68 @@ products_n_variants_n_prices AS (
             AND v.partition_date >= p.order_date_msk
             AND p.order_date_msk >= v.partition_date - INTERVAL 90 DAY
     LEFT JOIN {{ source('category_management', 'js_merchant_discount_rate') }} AS d ON v.merchant_id = d.merchant_id    
+    LEFT JOIN manual_products AS m ON p.product_id = m.product_id
     GROUP BY 1, 2, 3, 4, 5, 6, 7, 8
 ),
-
+------------------------------------------------------------------------------    
+----------------- Forming target price for each variant ----------------
+------------------------------------------------------------------------------  
 target_price_stg AS (
     SELECT
         *,
         ROUND(current_price_usd / merchant_price_index, 3) AS merchant_price_index_price,
         CASE
-            WHEN
+            WHEN ---- just taking discount rate from manual table and apply to the last not JS price - manual way of calculation
+                reason_of_participation = 0 AND discount_rate IS NOT NULL
+                THEN last_not_js_price_usd * discount_rate
+            WHEN ---- when we have no information about sales or price index, we calculate price for variant as an average price for other variants of the product
                 min_merchant_list_price IS NULL
                 AND min_merchant_sale_price IS NULL
                 AND ROUND(current_price_usd / merchant_price_index, 3) IS NULL
                 THEN NULL
-            WHEN
-                current_price_usd <= COALESCE(min_merchant_list_price, 1000000000) 
-                AND current_price_usd <= COALESCE(min_merchant_sale_price, 1000000000) 
-                AND current_price_usd <= COALESCE(ROUND(current_price_usd / merchant_price_index, 3), 1000000000)
-                THEN current_price_usd * COALESCE(discount_rate, 0.95)
-            WHEN
+            WHEN ---- when last not JS price still is the minimal price for the variant we take it with the discount as the target price
+                last_not_js_price_usd < COALESCE(min_merchant_list_price, 1000000000) 
+                AND last_not_js_price_usd < COALESCE(min_merchant_sale_price, 1000000000) 
+                AND last_not_js_price_usd < COALESCE(ROUND(current_price_usd / merchant_price_index, 3), 1000000000)
+                AND last_not_js_price_usd * COALESCE(discount_rate, 0.95) < current_price_usd
+                THEN last_not_js_price_usd * COALESCE(discount_rate, 0.95)
+            WHEN ---- when current price is the minimal price for the variant we take it without the discount as the target price
+                current_price_usd < COALESCE(min_merchant_list_price, 1000000000) 
+                AND current_price_usd < COALESCE(min_merchant_sale_price, 1000000000) 
+                AND current_price_usd < COALESCE(ROUND(current_price_usd / merchant_price_index, 3), 1000000000)
+                THEN current_price_usd
+            WHEN ---- when merchant list price in order is the minimal price for the variant we take it without the discount as the target price
                 min_merchant_list_price <= COALESCE(min_merchant_sale_price, 1000000000)
                 AND min_merchant_list_price <= COALESCE(ROUND(current_price_usd / merchant_price_index, 3), 1000000000)
-                THEN min_merchant_list_price -- * COALESCE(discount_rate, 0.95)
-            WHEN
+                THEN min_merchant_list_price
+            WHEN ---- when merchant sale price in order is the minimal price for the variant we take it without the discount as the target price
                 min_merchant_sale_price <= COALESCE(min_merchant_list_price, 1000000000)
                 AND min_merchant_sale_price <= COALESCE(ROUND(current_price_usd / merchant_price_index, 3), 1000000000)
-                THEN min_merchant_sale_price -- * COALESCE(discount_rate, 0.95)
-            WHEN
+                THEN min_merchant_sale_price
+            WHEN ---- when pi list price in order is the minimal price for the variant we take it without the discount as the target price
                 ROUND(current_price_usd / merchant_price_index, 3) <= COALESCE(min_merchant_list_price, 1000000000)
                 AND ROUND(current_price_usd / merchant_price_index, 3) <= COALESCE(min_merchant_sale_price, 1000000000)
-                THEN ROUND(current_price_usd / merchant_price_index, 3) -- * 0.95
-        END AS target_price_stg,     -- take minimum price current_price_usd, among min_merchant_list_price, min_merchant_sale_price, price_index_price
+                THEN ROUND(current_price_usd / merchant_price_index, 3)
+        END AS target_price_stg,     -- take minimum price current_price_usd, among min_merchant_list_price, min_merchant_sale_price, price_index_price, last_not_js_price_usd
         CASE
+            WHEN
+                reason_of_participation = 0 AND discount_rate IS NOT NULL
+                THEN "manual_discount"
             WHEN
                 min_merchant_list_price IS NULL
                 AND min_merchant_sale_price IS NULL
                 AND ROUND(current_price_usd / merchant_price_index, 3) IS NULL
                 THEN NULL
             WHEN
-                current_price_usd <= COALESCE(min_merchant_list_price, 1000000000) 
-                AND current_price_usd <= COALESCE(min_merchant_sale_price, 1000000000) 
-                AND current_price_usd <= COALESCE(ROUND(current_price_usd / merchant_price_index, 3), 1000000000)
+                last_not_js_price_usd < COALESCE(min_merchant_list_price, 1000000000) 
+                AND last_not_js_price_usd < COALESCE(min_merchant_sale_price, 1000000000) 
+                AND last_not_js_price_usd < COALESCE(ROUND(current_price_usd / merchant_price_index, 3), 1000000000)
+                AND last_not_js_price_usd * COALESCE(discount_rate, 0.95) < current_price_usd
+                THEN "last_not_js_price_usd_with_discount"
+            WHEN
+                current_price_usd < COALESCE(min_merchant_list_price, 1000000000) 
+                AND current_price_usd < COALESCE(min_merchant_sale_price, 1000000000) 
+                AND current_price_usd < COALESCE(ROUND(current_price_usd / merchant_price_index, 3), 1000000000)
                 THEN "current_price_usd"
             WHEN
                 min_merchant_list_price <= COALESCE(min_merchant_sale_price, 1000000000)
@@ -232,7 +420,7 @@ target_price_stg AS (
                 AND ROUND(current_price_usd / merchant_price_index, 3) <= COALESCE(min_merchant_sale_price, 1000000000)
                 THEN "price_index"
         END AS target_price_reason
-    FROM products_n_variants_n_prices AS p
+    FROM products_n_variants_n_prices
 )
 
 SELECT
@@ -244,7 +432,10 @@ SELECT
     variant_id,
     business_line,
     merchant_name,
-    reason_of_participation,
+    CASE
+        WHEN reason_of_participation = 1 THEN "automatically"
+        WHEN reason_of_participation = 0 THEN "manually"
+    END AS reason_of_participation,
     product_rating_60_days,
     gmv_60_days,
     orders_60_days,
