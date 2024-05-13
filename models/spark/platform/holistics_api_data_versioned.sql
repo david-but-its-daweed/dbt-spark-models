8c417bd3a3d75c9d965515ed5002a08717a89c45
Nikolay Gusev <36819289+g00sek@users.noreply.github.com>
2024-05-13 16:37:39 +0300

    :sparkles: created `holistics_api_data_versioned` (#633)
    Co-authored-by: gusev@joom.com <gusev@joom.com>
    Co-authored-by: g00sek <g00sek@users.noreply.github.com>

---
+++ models/spark/platform/holistics_api_data_versioned.sql
@@ -0,0 +1,16 @@
+{{ config (
+    schema='holistics',
+    materialized='incremental',
+    meta = {
+      'model_owner' : '@gusev',
+      'bigquery_load': 'true'
+    },
+    incremental_strategy='insert_overwrite',
+    partition_by=['date_msk'],
+    ) 
+}}
+
+SELECT
+    CURRENT_DATE() AS date_msk,
+    *
+FROM {{ ref('holistics_api_data') }}
