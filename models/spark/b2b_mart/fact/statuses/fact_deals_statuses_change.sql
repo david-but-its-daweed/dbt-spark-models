6a09d9fddd47a1ce550d5ae8dfce11811333134e
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2024-05-17 12:12:40 +0300

    Update fact_deals_statuses_change.sql (#638)

--- models/spark/b2b_mart/fact/statuses/fact_deals_statuses_change.sql
+++ models/spark/b2b_mart/fact/statuses/fact_deals_statuses_change.sql
@@ -17,37 +17,40 @@ from {{ ref('key_issue_status')}}),
 t as (select distinct 
 deal_id, 
 status,
-min_date,
+date as min_date,
 status_int,
-lead(min_date) over (partition by deal_id order by min_date) as next_status_date,
-lag(min_date) over (partition by deal_id order by min_date) as prev_status_date,
-lead(status) over (partition by deal_id order by min_date) as next_status,
-lag(status_int) over (partition by deal_id order by min_date) as prev_status_int,
-lead(status_int) over (partition by deal_id order by min_date) as next_status_int,
-lag(status) over (partition by deal_id order by min_date) as prev_status
+lead(date) over (partition by deal_id order by date) as next_status_date,
+lag(date) over (partition by deal_id order by date) as prev_status_date,
+lead(status) over (partition by deal_id order by date) as next_status,
+lag(status_int) over (partition by deal_id order by date) as prev_status_int,
+lead(status_int) over (partition by deal_id order by date) as next_status_int,
+lag(status) over (partition by deal_id order by date) as prev_status
 from
 (
-select distinct 
-deal_id, status, status_int, min_date
-from
-(select distinct
-status as current_status,
-min(date) over (partition by deal_id, status) as min_date,
-deal_id
-from
-(
-select deal_id, millis_to_ts_msk(statuses.ctms) as date, 
-statuses.status as status, statuses.moderatorId as owner_id
-from
-(select entityId as deal_id, explode(statusHistory) as statuses, etms
-    from {{ source('mongo', 'b2b_core_issues_daily_snapshot') }} i
-    left join {{ ref('key_issue_type') }} it ON CAST(i.type AS INT) =  CAST(it.id AS INT)
-    where it.type like '%CloseTheDeal%'
-    )
-    order by date desc, deal_id
+    select distinct 
+    deal_id, status, status_int, date
+    from
+    (select distinct
+    status as current_status,
+    lead(status) over (partition by deal_id order by date) as lead_status,
+    date,
+    deal_id
+    from
+    (
+    select deal_id, millis_to_ts_msk(statuses.ctms) as date, 
+    statuses.status as status, statuses.moderatorId as owner_id
+    from
+        (
+            select entityId as deal_id, explode(statusHistory) as statuses, etms
+            from {{ source('mongo', 'b2b_core_issues_daily_snapshot') }} i
+            left join {{ ref('key_issue_type') }} it ON CAST(i.type AS INT) =  CAST(it.id AS INT)
+            where it.type like '%CloseTheDeal%'
+        )
+        order by date desc, deal_id
+        )
+    ) d left join statuses s on s.status_int = d.current_status
+    where status != lead_status or lead_status is null
 )
-) d left join statuses s on s.status_int = d.current_status
 )
-     )
 
 select * from t
