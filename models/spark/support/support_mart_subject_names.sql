25b1597ced047ccb1a34daf5596f9ff9ca64cade
ilypavlov <iapavlov96@gmail.com>
2024-06-11 16:57:08 +0300

    Update support_mart_subject_names.sql (#665)
    Delete Onfy from our data
    
    ---------
    
    Co-authored-by: ilypavlov <ilypavlov@users.noreply.github.com>

--- models/spark/support/support_mart_subject_names.sql
+++ models/spark/support/support_mart_subject_names.sql
@@ -186,93 +186,6 @@ jl AS (
         a.language
     FROM base_jl AS t
     LEFT JOIN {{ ref('support_mart_ticket_id_ext_jl') }} AS a ON t.ticket_id = a.ticket_id
-),
-
-prebase_onfy AS (
-    SELECT
-        payload.ticketid AS ticket_id,
-        payload.changedbytype AS state_owner,
-        EXPLODE(payload.tagids) AS tag,
-        event_ts_msk AS event_ts_msk,
-        partition_date
-    FROM {{ source('mart', 'onfy_babylone_events') }}
-    WHERE
-        type = 'ticketChange'
-        AND partition_date > '2021-12-31'
-        AND payload.changedbytype IN ('agent', 'automation')
-)
-,
-
-t_onfy AS (
-    SELECT
-        t.ticket_id,
-        a.name AS tag,
-        a.type AS tag_type,
-        b.name AS subject_category,
-        t.state_owner AS state_owner,
-        t.partition_date,
-        MIN(t.event_ts_msk) AS event_ts_msk
-    FROM prebase_onfy AS t
-    LEFT JOIN {{ source('mongo', 'babylone_onfy_tags_daily_snapshot') }} AS a ON t.tag = a._id
-    LEFT JOIN {{ source('mongo', 'babylone_onfy_tag_categories_daily_snapshot') }} AS b ON a.categoryid = b._id
-    GROUP BY 1, 2, 3, 4, 5, 6
-)
-,
-
-cnt_categories_onfy AS (
-    SELECT
-        t.ticket_id,
-        COUNT(DISTINCT t.subject_category) AS cnt_subject_categories
-    FROM t_onfy AS t
-    GROUP BY 1
-)
-,
-
-tt_onfy AS (
-    SELECT
-        t.ticket_id,
-        t.tag,
-        t.tag_type,
-        t.subject_category,
-        t.event_ts_msk,
-        t.partition_date,
-        a.cnt_subject_categories,
-        FIRST_VALUE(t.state_owner) OVER (PARTITION BY t.ticket_id, t.tag, t.tag_type, t.subject_category, t.partition_date ORDER BY t.event_ts_msk) AS state_owner
-    FROM t_onfy AS t
-    LEFT JOIN cnt_categories_onfy AS a ON t.ticket_id = a.ticket_id
-),
-
-base_onfy AS (
-    SELECT
-        t.ticket_id,
-        t.tag,
-        t.tag_type,
-        t.subject_category,
-        t.state_owner,
-        t.partition_date,
-        t.cnt_subject_categories,
-        MIN(t.event_ts_msk) AS event_ts_msk
-    FROM tt_onfy AS t
-    GROUP BY 1, 2, 3, 4, 5, 6, 7
-),
-
-onfy AS (
-    SELECT
-        'onfy' AS bu,
-        t.ticket_id,
-        t.state_owner,
-        t.tag,
-        t.tag_type,
-        t.subject_category,
-        t.cnt_subject_categories,
-        TIMESTAMP(t.event_ts_msk) AS event_ts_msk,
-        t.partition_date,
-        a.first_queue,
-        a.current_queue,
-        a.country,
-        a.language
-    FROM base_onfy AS t
-    LEFT JOIN {{ ref('support_mart_ticket_id_ext_onfy') }} AS a ON t.ticket_id = a.ticket_id
 )
 
 
@@ -281,6 +194,4 @@ FROM marketplace
 UNION DISTINCT
 SELECT *
 FROM jl
-UNION DISTINCT
-SELECT *
-FROM onfy
+
