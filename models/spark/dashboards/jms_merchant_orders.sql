67d749afbb184fcb9ca3a4f8e8659336cfef3e5e
Kirill Yatsushko <yatsushko@joom.com>
2024-06-20 15:39:28 +0300

    [DASHBOARDS-106] Add JMS GMV model (#678)
    Привет!
    
    В этом PR я делаю модель для построения графика по GMV JMS в Holistics.
    Эта модель будет переливаться в DBT BQ, где на неё сверху я еще прикручу
    плановые значения.
    
    ---------
    
    Co-authored-by: justakir <justakir@users.noreply.github.com>

---
+++ models/spark/dashboards/jms_merchant_orders.sql
@@ -0,0 +1,37 @@
+{{
+  config(
+    materialized='table',
+    meta = {
+      'model_owner' : '@general_analytics',
+      'priority_weight': '1000',
+      'bigquery_load': 'true'
+    }
+  )
+}}
+
+WITH cr_data AS (
+    SELECT
+        currency_code,
+        rate,
+        effective_date,
+        next_effective_date
+    FROM {{ ref('dim_pair_currency_rate') }}
+    WHERE
+        currency_code_to = 'USD'
+)
+
+SELECT
+    mo.order_id AS merchant_order_id,
+    mo.country AS country_code,
+    TO_DATE(mo.created_time_utc) AS created_date,
+    mo.marketplace_id.marketplace AS marketplace_name,
+    mo.product_id,
+    mo.money_info.customer_gmv * cr.rate - mo.money_info.customer_vat * cr.rate AS gmv_initial_without_vat
+FROM {{ source('mongo', 'merchant_order') }} AS mo
+LEFT JOIN cr_data AS cr
+    ON
+        mo.created_time_utc BETWEEN cr.effective_date AND cr.next_effective_date
+        AND mo.money_info.merchant_currency = cr.currency_code
+WHERE
+    source.kind = 'jms'
+    AND TO_DATE(mo.created_time_utc) >= '2023-02-16' -- date of first JMS order
