c7575ffa0216174d312e76ab1bf0675becd2b73d
Kirill Yatsushko <yatsushko@joom.com>
2024-06-21 15:13:33 +0300

    [DASHBOARDS-106] Fix the date (#688)
    Привет!
    
    В этом PR я поменял дату на ту, которая отображает время создания заказа
    на маркетплейсе
    
    ---------
    
    Co-authored-by: justakir <justakir@users.noreply.github.com>

--- models/spark/dashboards/jms_merchant_orders.sql
+++ models/spark/dashboards/jms_merchant_orders.sql
@@ -23,15 +23,15 @@ WITH cr_data AS (
 SELECT
     mo.order_id AS merchant_order_id,
     mo.country AS country_code,
-    TO_DATE(mo.created_time_utc) AS created_date,
+    TO_DATE(mo.marketplace_created_time) AS created_date,
     mo.marketplace_id.marketplace AS marketplace_name,
     mo.product_id,
     mo.money_info.customer_gmv * cr.rate - mo.money_info.customer_vat * cr.rate AS gmv_initial_without_vat
 FROM {{ source('mongo', 'merchant_order') }} AS mo
 LEFT JOIN cr_data AS cr
     ON
-        mo.created_time_utc BETWEEN cr.effective_date AND cr.next_effective_date
+        mo.marketplace_created_time BETWEEN cr.effective_date AND cr.next_effective_date
         AND mo.money_info.merchant_currency = cr.currency_code
 WHERE
     source.kind = 'jms'
-    AND TO_DATE(mo.created_time_utc) >= '2023-02-16' -- date of first JMS order
+    AND TO_DATE(mo.marketplace_created_time) >= '2023-02-16' -- date of first JMS order
