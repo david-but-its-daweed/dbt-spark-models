2845e0425d227a226acff9055426e27fcca9ab7f
anna zaychik <67829640+anyazay@users.noreply.github.com>
2024-06-27 16:37:09 +0100

    PhAn-764 Awin revision (#694)

---
+++ models/spark/onfy/awin_revision.sql
@@ -0,0 +1,91 @@
+{{ config(
+    schema='onfy',
+    materialized='table',
+    meta = {
+      'model_owner' : '@annzaychik',
+      'team': 'onfy',
+      'bigquery_load': 'true',
+      'alerts_channel': 'onfy-etl-monitoring'
+    }
+) }}
+
+with vats as 
+(
+    select 
+        order_parcel_id,
+        sum(total_price_after_discount_price / (1+vat)) as products_price_before_vat
+    from {{source('pharmacy_landing', 'order_parcel_item')}}
+    join {{source('pharmacy_landing', 'medicine')}}
+        on medicine.id = order_parcel_item.product_id
+    group by 
+        order_parcel_id
+), 
+
+transactions as
+(
+    select 
+        transactions.order_id,
+        transactions.order_parcel_id,
+        order_num,
+        (sum(if(type = 'PAYMENT', price, 0)) - sum(if(type='DISCOUNT', price, 0))) as payment,
+        (sum(if(type = 'ORDER_REVERSAL', price, 0)) - sum(if(type='DISCOUNT_REVERSAL', price, 0))) as reversal,
+        sum(if(type = 'ORDER_REVERSAL', 1, 0)) as parcels_reversed,
+        sum(if(type = 'DISCOUNT', price, 0)) as discount,
+        products_price_before_vat
+    from {{source('onfy', 'transactions')}}
+    join vats
+        on transactions.order_parcel_id = vats.order_parcel_id
+    join {{source('pharmacy_landing', 'order')}}
+        on order.id = transactions.order_id
+    where 1=1
+        and currency = 'EUR'
+        and transactions.order_parcel_id is not null
+    group by 
+        transactions.order_id,
+        purchase_num,
+        transactions.order_parcel_id,
+        products_price_before_vat,
+        order_num
+)
+
+select 
+    source,
+    order_num,
+    first_user_source,
+    attributed_session_dt,
+    purchase_num,
+    ad.order_id,
+    sum(gmv_initial) as gmv_initial,
+    sum(gross_profit_initial) as gross_profit_initial,
+    count(distinct order_parcel_id) as parcels,
+    sum(payment) as payment,
+    sum(reversal) as reversal,
+    sum(parcels_reversed) as parcels_reversed,
+    max(parcels_reversed) as had_reversal,
+    sum(products_price_before_vat) as products_price_before_vat,
+    case
+        when sum(discount) = 0 and purchase_num = 1 then 0.15
+        when sum(discount) > 0 and purchase_num = 1 then 0.1
+        when sum(discount) = 0 and purchase_num > 1 then 0.045
+        when sum(discount) > 0 and purchase_num > 1 then 0.03
+    end as comission,
+    case
+        when sum(discount) = 0 and purchase_num = 1 then 0.15
+        when sum(discount) > 0 and purchase_num = 1 then 0.1
+        when sum(discount) = 0 and purchase_num > 1 then 0.045
+        when sum(discount) > 0 and purchase_num > 1 then 0.03
+    end * sum(products_price_before_vat) as comission_sum
+from {{source('onfy', 'ads_dashboard')}} as ad
+join transactions
+    on ad.order_id = transactions.order_id
+where 1=1
+    and source = 'awin'
+group by 
+    source,
+    order_num,
+    first_user_source,
+    attributed_session_dt,
+    purchase_num,
+    ad.order_id
+order by 
+    attributed_session_dt desc
