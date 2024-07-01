a3927a55bf18de54fcbf37fb132bfe25fce74f9c
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2024-07-01 14:36:16 +0300

    Update fact_orders_costs.sql (#701)

--- models/spark/b2b_mart/fact/fact_orders_costs.sql
+++ models/spark/b2b_mart/fact/fact_orders_costs.sql
@@ -175,9 +175,9 @@ order_owners as (
     from
     (select distinct 
         order_id,
-        first_value(owner_moderator_id) over (partition by order_id order by event_ts_msk) as owner_id
+        first_value(owner_moderator_id) over (partition by order_id order by event_ts_msk desc) as owner_id
         from {{ ref('fact_order_change') }}
-        where status = 'manufacturing'
+        where status in ('manufacturing', 'shipping', 'claim', 'done')
     ) o
     left join admins a 
     on o.owner_id = a.admin_id 
