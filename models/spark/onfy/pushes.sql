1da40627be204bec8e58d82407453b18a9699927
Ekaterina Saltykova <34067050+ea-sal@users.noreply.github.com>
2024-07-10 17:06:39 +0200

    PhAn-769-create-pushes-table (#729)
    Co-authored-by: ea-sal <ea-sal@users.noreply.github.com>

---
+++ models/spark/onfy/pushes.sql
@@ -0,0 +1,98 @@
+{{ config(
+    schema='onfy',
+    materialized='table',
+    meta = {
+      'model_owner' : '@easaltykova',
+      'team': 'onfy',
+      'bigquery_load': 'true',
+      'alerts_channel': 'onfy-etl-monitoring'
+    }
+) }}
+
+
+WITH pushes_sent AS (
+
+    SELECT
+        userid,
+        mailingname AS campaign,
+        CASE
+            WHEN mailingname LIKE '%items have been added to the cart but not purchased%' THEN TRUE
+            WHEN mailingname LIKE '%Registration without order within%' THEN TRUE
+            WHEN mailingname LIKE '%Abandoned Category%' THEN TRUE
+            WHEN mailingname LIKE '%The total basket price has been discounted%' THEN TRUE
+            WHEN mailingname LIKE '%The price of the viewed item has been reduced%' THEN TRUE
+            WHEN mailingname LIKE '%items have been viewed but not purchased%' THEN TRUE
+            WHEN mailingname LIKE '%Would you like to repeat the experience%' THEN TRUE
+            ELSE FALSE
+        END AS is_transactional,
+        FROM_UTC_TIMESTAMP(datetimeutc, 'Europe/Berlin') AS push_sent_dt,
+        COALESCE(
+            LEAD(FROM_UTC_TIMESTAMP(datetimeutc, 'Europe/Berlin')) OVER (PARTITION BY userid, mailingname ORDER BY datetimeutc),
+            '2100-01-01 00:00:00'
+        ) AS next_push_sent_dt
+    FROM {{ source('pharmacy', 'mindbox_events') }}
+    WHERE
+        channelname = 'MobilePush'
+        AND mailingaction = 'MailingSend'
+        AND userid IS NOT NULL
+),
+
+pushes_clicked AS (
+    SELECT
+        userid,
+        mailingname AS campaign,
+        FROM_UTC_TIMESTAMP(datetimeutc, 'Europe/Berlin') AS push_clicked_dt
+    FROM {{ source('pharmacy', 'mindbox_events') }}
+    WHERE
+        channelname = 'MobilePush'
+        AND mailingaction = 'MailingClick'
+        AND userid IS NOT NULL
+),
+
+pushes AS (
+
+    SELECT
+        pushes_sent.*,
+        pushes_clicked.push_clicked_dt
+    FROM pushes_sent
+    LEFT JOIN pushes_clicked
+        ON
+            pushes_sent.userid = pushes_clicked.userid
+            AND pushes_sent.campaign = pushes_clicked.campaign
+            AND pushes_sent.push_sent_dt <= pushes_clicked.push_clicked_dt
+            AND pushes_sent.next_push_sent_dt > pushes_clicked.push_clicked_dt
+),
+
+sessions_orders AS (
+    SELECT
+        device.user_id,
+        sessions.device_id,
+        sessions.session_id,
+        sessions.session_start,
+        sessions.session_end,
+        sessions.orders,
+        sessions.gmv_initial,
+        sessions.gross_profit_initial,
+        sessions.promocode_discount
+    FROM {{ source('onfy', 'sessions') }} AS sessions
+        JOIN {{ source('pharmacy_landing', 'device') }} AS device
+        ON sessions.device_id = device.id
+)
+
+SELECT
+    pushes.userid AS user_id,
+    pushes.campaign,
+    pushes.is_transactional,
+    pushes.push_sent_dt,
+    pushes.push_clicked_dt,
+    COUNT(sessions_orders.session_id) AS sessions,
+    SUM(sessions_orders.orders) AS orders,
+    SUM(sessions_orders.gmv_initial) AS gmv_initial,
+    SUM(sessions_orders.gross_profit_initial) AS gross_profit_initial,
+    SUM(sessions_orders.promocode_discount) AS promocode_discount
+FROM pushes
+LEFT JOIN sessions_orders
+    ON
+        pushes.userid = sessions_orders.user_id
+        AND sessions_orders.session_start BETWEEN pushes.push_clicked_dt AND (pushes.push_clicked_dt + INTERVAL '30 minute')
+GROUP BY 1, 2, 3, 4, 5
