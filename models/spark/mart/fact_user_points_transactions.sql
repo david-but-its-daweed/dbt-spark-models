891ee61e35cc90d3afd879503525662ff79dceb3
Nikolay Gusev <36819289+g00sek@users.noreply.github.com>
2024-07-18 11:06:58 +0300

    :sparkles: update `model_owner` and materialized (#738)

--- models/spark/mart/fact_user_points_transactions.sql
+++ models/spark/mart/fact_user_points_transactions.sql
@@ -1,13 +1,15 @@
 {{ config(
     schema='mart',
-    materialized='view',
+    materialized='table',
+    partition_by='date_msk',
     meta = {
-      'model_owner' : '@general_analytics',
+      'model_owner' : '@analytics.duty',
       'bigquery_load': 'true',
       'bigquery_partitioning_date_column': 'date_msk',
       'bigquery_upload_horizon_days': '14',
       'priority_weight': '150'
-    }
+    },
+    
 ) }}
 
 SELECT
@@ -15,14 +17,14 @@ SELECT
     userid AS user_id,
     kind,
     type,
-    cast((effectiveusd / 1000000) AS DECIMAL(38, 6)) AS effective_usd,
+    CAST((effectiveusd / 1000000) AS DECIMAL(38, 6)) AS effective_usd,
     hidden,
     refid AS refid,
-    to_date(cast(cast(conv(substring(_id, 0, 8), 16, 10) AS BIGINT) + 10800 AS TIMESTAMP)) AS date_msk,
+    TO_DATE(CAST(CAST(CONV(SUBSTRING(_id, 0, 8), 16, 10) AS BIGINT) + 10800 AS TIMESTAMP)) AS date_msk,
     amount,
-    cast(NULL AS BOOLEAN) AS bad_order_id,
+    CAST(NULL AS BOOLEAN) AS bad_order_id,
     index,
     pending
 FROM {{ source('mongo', 'points_points_transactions_daily_snapshot') }}
-WHERE to_date(cast(cast(conv(substring(_id, 0, 8), 16, 10) AS BIGINT) + 10800 AS TIMESTAMP)) < to_date(now())
+WHERE TO_DATE(CAST(CAST(CONV(SUBSTRING(_id, 0, 8), 16, 10) AS BIGINT) + 10800 AS TIMESTAMP)) < TO_DATE(NOW())
 
