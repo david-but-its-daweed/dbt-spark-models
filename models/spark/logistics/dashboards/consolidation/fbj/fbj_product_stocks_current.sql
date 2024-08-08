4e0067bf76dd27122f1a169c89ebe1d2b8f22bad
Nikolay Gusev <36819289+g00sek@users.noreply.github.com>
2024-08-08 12:27:39 +0300

    An 3507 (#753)
    Co-authored-by: gusev@joom.com <gusev@joom.com>
    Co-authored-by: g00sek <g00sek@users.noreply.github.com>
    Co-authored-by: MVasyukova <34578801+MVasyukova@users.noreply.github.com>

--- models/spark/logistics/dashboards/consolidation/fbj/fbj_product_stocks_current.sql
+++ models/spark/logistics/dashboards/consolidation/fbj/fbj_product_stocks_current.sql
@@ -11,18 +11,18 @@
 SELECT
     CURRENT_DATE() AS partition_date,
     published_variant.product_id,
-    info.externalid AS product_variant_id,
+    info.externalId AS product_variant_id,
     info._id AS logistics_product_id,
     stocks.reserved AS number_of_reserved,
     stocks.stock AS number_of_products_in_stock,
-    stocks.utm AS stock_update_ts,
+    stocks.uTm AS stock_update_ts,
     info.brand AS brand_name,
-    info.nameen AS product_name,
+    info.nameEn AS product_name,
     info.url AS product_url,
-    info.udimensions AS product_dimensions,
-    info.merchantid AS merchant_id,
-    info.uprice.amount / 1000000 AS original_merchant_price,
-    info.uweight AS product_weight
+    info.uDimensions AS product_dimensions,
+    info.merchantId AS merchant_id,
+    info.uPrice.amount / 1000000 AS original_merchant_price,
+    info.uWeight AS product_weight
 FROM {{ source('mongo', 'logistics_product_stocks_daily_snapshot') }} AS stocks
 LEFT JOIN {{ source('mongo', 'logistics_products_v2_daily_snapshot') }} AS info
     ON stocks._id.pid = info._id
