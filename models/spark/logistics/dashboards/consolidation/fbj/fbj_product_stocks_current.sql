9e8d316c5519602760f16fb84d41497d81a1dbbe
Nikolay Gusev <36819289+g00sek@users.noreply.github.com>
2024-08-09 14:57:16 +0300

    An 3507 (#759)
    Co-authored-by: gusev@joom.com <gusev@joom.com>
    Co-authored-by: g00sek <g00sek@users.noreply.github.com>
    Co-authored-by: MVasyukova <34578801+MVasyukova@users.noreply.github.com>

--- models/spark/logistics/dashboards/consolidation/fbj/fbj_product_stocks_current.sql
+++ models/spark/logistics/dashboards/consolidation/fbj/fbj_product_stocks_current.sql
@@ -21,7 +21,7 @@ SELECT
     info.url AS product_url,
     info.uDimensions AS product_dimensions,
     info.merchantId AS merchant_id,
-    info.uPrice.amount / 1000000 AS original_merchant_price,
+    info.uPrice.amount * currency_rate.rate / 1000000 AS original_merchant_price,
     info.uWeight AS product_weight
 FROM {{ source('mongo', 'logistics_product_stocks_daily_snapshot') }} AS stocks
 LEFT JOIN {{ source('mongo', 'logistics_products_v2_daily_snapshot') }} AS info
@@ -30,4 +30,9 @@ LEFT JOIN mart.dim_published_variant AS published_variant
     ON
         info.externalid = published_variant.variant_id
         AND CURRENT_DATE() BETWEEN published_variant.effective_ts AND published_variant.next_effective_ts
+LEFT JOIN {{ ref('dim_pair_currency_rate') }} AS currency_rate
+    ON
+        info.uPrice.ccy = currency_rate.currency_code
+        AND currency_rate.effective_date = CURRENT_DATE()
+        AND currency_rate.currency_code_to = 'USD'
 WHERE stocks._id.wid = '66719d184d731a97c6a58b28'
