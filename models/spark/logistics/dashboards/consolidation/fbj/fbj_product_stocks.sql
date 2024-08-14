50abcb71156272eea44d6d3d64363d16da7ad208
Kirill Yatsushko <yatsushko@joom.com>
2024-08-14 23:56:48 +0300

    Revert "[DASHBOARDS-127] Preparing a model for backfill" (#777)
    Reverts joomcode/dbt-spark-models#774

--- models/spark/logistics/dashboards/consolidation/fbj/fbj_product_stocks.sql
+++ models/spark/logistics/dashboards/consolidation/fbj/fbj_product_stocks.sql
@@ -1,11 +1,30 @@
 {{
   config(
+    materialized='incremental',
+    file_format='parquet',
+    incremental_strategy='insert_overwrite',
+    partition_by=['partition_date'],
     meta = {
-        'model_owner' : '@catman-analytics.duty'
+        'model_owner' : '@catman-analytics.duty',
+        'bigquery_load': 'true'
     }
   )
 }}
 
 SELECT
-  *
-FROM junk_yatsushko.fbj_table_for_backfill_kirill
+    partition_date,
+    product_variant_id,
+    product_id,
+    business_line,
+    logistics_product_id,
+    number_of_reserved,
+    number_of_products_in_stock,
+    stock_update_ts,
+    brand_name,
+    product_name,
+    product_url,
+    product_dimensions,
+    merchant_id,
+    original_merchant_price,
+    product_weight
+FROM {{ ref('fbj_product_stocks_current') }}
