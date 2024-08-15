0e675ef686f764604280fd21dba9c4ea461ce9c3
Kirill Yatsushko <yatsushko@joom.com>
2024-08-15 22:04:28 +0300

    [DASHBOARDS-130] Added business line to JMS orders (#778)
    Привет!
    
    В этом PR я добавил информацию о принадлежности к бизнес-линиям JMS
    заказов

--- models/spark/dashboards/jms_merchant_orders.sql
+++ models/spark/dashboards/jms_merchant_orders.sql
@@ -26,12 +26,14 @@ SELECT
     TO_DATE(mo.marketplace_created_time) AS created_date,
     mo.marketplace_id.marketplace AS marketplace_name,
     mo.product_id,
+    gp.business_line,
     mo.money_info.customer_gmv * cr.rate - mo.money_info.customer_vat * cr.rate AS gmv_initial_without_vat
 FROM {{ source('mongo', 'merchant_order') }} AS mo
 LEFT JOIN cr_data AS cr
     ON
         mo.marketplace_created_time BETWEEN cr.effective_date AND cr.next_effective_date
         AND mo.money_info.merchant_currency = cr.currency_code
+LEFT JOIN {{ ref('gold_products') }} AS gp ON gp.product_id = mo.product_id
 WHERE
     source.kind = 'jms'
     AND TO_DATE(mo.marketplace_created_time) >= '2023-02-16' -- date of first JMS order
