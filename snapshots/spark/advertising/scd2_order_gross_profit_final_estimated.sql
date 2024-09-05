1f3338368b5264995895cc0aa6acd25a0d21cbe5
Alexey Yushkov <55638415+lexx-a@users.noreply.github.com>
2024-09-05 12:15:05 +0200

    Orders add order gross profit final estimated history (#794)
    создал scd2 таблицу для отслеживания динамики order gross profit final estimated, в частности для фиксации order gross profit final estimated в первый день заказа
    
    ---------
    
    Co-authored-by: lexx-a <lexx-a@users.noreply.github.com>

---
+++ snapshots/spark/advertising/scd2_order_gross_profit_final_estimated.sql
@@ -0,0 +1,25 @@
+{% snapshot scd2_order_gross_profit_final_estimated %}
+
+{{
+    config(
+      target_schema='ads_models',
+      unique_key='order_id',
+      strategy='check',
+      check_cols='all',
+      file_format='delta',
+      invalidate_hard_deletes=True,
+      meta = {
+      'model_owner' : '@yushkov',
+          'team' : 'advertising',
+      }
+    )
+}}
+
+    SELECT
+        order_id,
+        order_date_msk,
+        order_gross_profit_final,
+        order_gross_profit_final_estimated
+    FROM {{ ref('gold_orders') }}
+
+{% endsnapshot %}
