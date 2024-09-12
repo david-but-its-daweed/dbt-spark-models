087b8e5c5673c8bdd30fbb9a8673c4804a281830
anna zaychik <67829640+anyazay@users.noreply.github.com>
2024-09-12 15:32:28 +0200

    created new automatic job data (#813)
    Co-authored-by: annzaychik@joom.com <annzaychik@joom.com>
    Co-authored-by: anyazay <anyazay@users.noreply.github.com>

---
+++ models/spark/onfy/onfy_discount_rules.sql
@@ -0,0 +1,16 @@
+{{ config(
+    schema='onfy',
+    materialized='table',
+    meta = {
+      'model_owner' : '@annzaychik',
+      'team': 'onfy',
+      'bigquery_load': 'true',
+      'alerts_channel': 'onfy-etl-monitoring'
+    }
+) }}
+
+SELECT *
+FROM {{ source('onfy', 'auto_discount_rules') }}
+UNION ALL
+SELECT *
+FROM {{ ref('onfy_discount_rules_manual') }}
