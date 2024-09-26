8c8708be99dbc1279c7d346df13ee30eac122100
MVasyukova <34578801+MVasyukova@users.noreply.github.com>
2024-09-26 15:35:33 +0300

    [AN-3567] new source for product diagnotic (#828)
    Co-authored-by: vasiukova_mn@joom.com <vasiukova_mn@joom.com>
    Co-authored-by: MVasyukova <MVasyukova@users.noreply.github.com>

--- models/spark/category_management/products_diagnoses_current.sql
+++ models/spark/category_management/products_diagnoses_current.sql
@@ -2,32 +2,22 @@
     schema='category_management',
     materialized='table',
     meta = {
-      'model_owner' : '@troyanovskaya',
+      'model_owner' : '@catman-analytics.duty',
       'team': 'category_management',
       'bigquery_load': 'true',
     }
 ) }}
 
 
--- статусы (see https://www.notion.so/joomteam/53e8529cdc924c64ab7d1a653548f6c2 ):
---   0 = диагностики нет
---   1 = диагностика есть, но неактивна
---   2 = запрещённое состояние, не бывает
---   3 = диагностика активна.
- select
+-- коды: https://www.notion.so/joomteam/Diagnostics-Origins-Codes-11eb1f1bbd3645d5b65858057b20e47a
+
+SELECT DISTINCT
     product_id,
-    diag_code
-from 
-    (
-    select 
-        *,
-         coalesce (lead(event_ts) over (partition by product_id, diag_code, diag_digest order by event_ts asc), "9999-12-31 23:59:59") as next_effective_ts
-    from {{ source('product', 'diagnoses_changes') }}
-    where true
-        and partition_date > "2022-01-11" -- hardcoded; there are no J1178 earlier
-        and diag_code  in ('J1178')
-    )
-    where true
-        and next_effective_ts > current_date() + interval 1 day
-        and diag_now_state = 3
-    group by 1, 2
+    d.code AS diag_code
+FROM (
+    SELECT
+        _id AS product_id,
+        EXPLODE(diags) AS d
+    FROM {{ source('mongo', 'product_product_diagnoses_daily_snapshot') }}
+)
+WHERE d.code IN ('J1178')
