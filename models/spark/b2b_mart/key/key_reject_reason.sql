c2caaa4da5bf9f0a60858e6dc89964f4967806d5
mkirusha <kirill_melnikov@joom.com>
2024-10-21 13:46:33 +0200

    Create key_reject_reason.sql (#890)
    Co-authored-by: mkirusha <mkirusha@users.noreply.github.com>

---
+++ models/spark/b2b_mart/key/key_reject_reason.sql
@@ -0,0 +1,19 @@
+{{ config(
+    schema='b2b_mart',
+    materialized='table',
+    file_format='parquet',
+    meta = {
+      'model_owner' : '@mkirusha',
+      'bigquery_load': 'true',
+      'priority_weight': '150'
+    }
+) }}
+
+SELECT
+    key AS id,
+    UPPER(SUBSTR(value, 0, 1)) || (SUBSTR(value, 2, LENGTH(value) - 1)) AS type
+FROM (
+    SELECT EXPLODE(values)
+    FROM {{ source('mongo', 'b2b_core_enumregistry_daily_snapshot') }}
+    WHERE key = 'issue.statusRejectReason'
+)
