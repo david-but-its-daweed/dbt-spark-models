c895615532dc53602319627c3db623da95ecdc70
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2024-10-23 15:18:24 +0300

    Update creation_date_null_check.sql (#903)
    Co-authored-by: NastiaMitiushkina <NastiaMitiushkina@users.noreply.github.com>

--- tests/spark/pulse/creation_date_null_check.sql
+++ tests/spark/pulse/creation_date_null_check.sql
@@ -3,4 +3,4 @@ FROM {{ source('joompro_analytics_mart', 'mercadolibre_products_snapshot') }}
 WHERE
     listing_creation_date IS NULL
     AND effective_ts >= DATE_SUB(CURRENT_DATE(), 7)
-    AND effective_ts < DATE_SUB(CURRENT_DATE(), 1)
+    AND effective_ts < DATE_SUB(CURRENT_DATE(), 2)
