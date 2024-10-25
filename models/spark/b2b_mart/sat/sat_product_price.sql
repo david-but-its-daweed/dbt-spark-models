fc3f05bd16ade827e6860898f0c9fbbf7a955748
mkirusha <kirill_melnikov@joom.com>
2024-10-25 17:56:02 +0200

    Update sat_product_price.sql (#913)

--- models/spark/b2b_mart/sat/sat_product_price.sql
+++ models/spark/b2b_mart/sat/sat_product_price.sql
@@ -9,7 +9,10 @@
     }
 ) }}
 SELECT
-    *,
+    product_id,
+    min_price/1000000 as min_price,
+    min_price_ccy,
+    update_ts_msk,
     TIMESTAMP(dbt_valid_from) AS effective_ts_msk,
     TIMESTAMP(dbt_valid_to) AS next_effective_ts_msk
 FROM {{ ref('scd2_mongo_product_prices_daily_snapshot') }}
