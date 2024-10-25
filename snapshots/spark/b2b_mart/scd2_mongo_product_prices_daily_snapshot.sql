98ba3701b83a3331544b5c640d578e12af3ea745
mkirusha <kirill_melnikov@joom.com>
2024-10-25 13:09:01 +0200

    Create scd2_mongo_product_prices_daily_snapshot.sql (#911)
    Co-authored-by: mkirusha <mkirusha@users.noreply.github.com>

---
+++ snapshots/spark/b2b_mart/scd2_mongo_product_prices_daily_snapshot.sql
@@ -0,0 +1,23 @@
+{% snapshot scd2_mongo_product_prices_daily_snapshot %}
+
+{{
+    config(
+    meta = {
+      'model_owner' : '@mkirusha'
+    },
+      target_schema='b2b_mart',
+      unique_key='product_id',
+
+      strategy='timestamp',
+      updated_at='update_ts_msk',
+      file_format='delta'
+    )
+}}
+
+    SELECT
+        _id AS product_id,
+        P.brMin.amount AS min_price,
+        P.brMin.ccy AS min_price_ccy,
+        MILLIS_TO_TS_MSK(utms) AS update_ts_msk
+    FROM {{ source('mongo', 'b2b_core_product_prices_daily_snapshot') }}
+{% endsnapshot %}
