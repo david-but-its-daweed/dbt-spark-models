6ac2d2eb1dc37434a2d682c37315cfed4e9180ec
mkirusha <kirill_melnikov@joom.com>
2024-11-14 16:33:54 +0100

    Update gmv_by_sources.sql (#940)
    Co-authored-by: mkirusha <mkirusha@users.noreply.github.com>

--- models/spark/b2b_mart/gmv_by_sources.sql
+++ models/spark/b2b_mart/gmv_by_sources.sql
@@ -9,6 +9,25 @@
     }
 ) }}
 
-SELECT *
+SELECT
+    t,
+    order_id,
+    gmv_initial,
+    initial_gross_profit,
+    final_gross_profit,
+    utm_campaign,
+    utm_source,
+    utm_medium,
+    source,
+    type,
+    campaign,
+    retention,
+    user_id,
+    country,
+    owner_email,
+    owner_role,
+    first_order,
+    client,
+    current_client
 FROM {{ ref('gmv_by_sources_wo_filters') }}
 WHERE order_id NOT IN ('657c58febbbdb8729dd7d39e', '658d3fc317e10341173c1f20', '659d3c4dddc19670cf999989', '65aa2a7ff11499f63900def9')
