636f50ca75444f87713e3f119ba9c120e10b6ae3
mkirusha <kirill_melnikov@joom.com>
2024-11-20 17:09:18 +0100

    Create ss_events_customer_ journey.sql (#963)

---
+++ models/spark/b2b_mart/ss/ss_events_customer_journey.sql
@@ -0,0 +1,32 @@
+{{ config(
+    schema='b2b_mart',
+    materialized='view',
+    partition_by={
+         "field": "event_msk_date"
+    },
+    meta = {
+      'model_owner' : '@kirill_melnikov',
+      'team': 'general_analytics',
+      'bigquery_load': 'true',
+      'priority_weight': '150'
+    }
+) }}
+
+                 
+ Select
+    user['userId'] AS user_id,
+    de.device.id as device_id,
+    type,
+    event_ts_msk,
+    CAST(event_ts_msk AS DATE) AS event_msk_date,
+    payload.pageUrl,
+    payload.pageName,
+    payload.source,
+    payload.isRegistrationCompleted
+    from  {{ source('b2b_mart', 'device_events') }} AS de
+    where partition_date >= '2024-04-01'
+         and  type in ( 'sampleCheckoutOpen',
+                        'pageLeave',
+                        'userProfileClick'
+                        )
+                            
