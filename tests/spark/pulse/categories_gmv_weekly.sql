2ee1947cda8b82923b39a35ac185a160cb78498e
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2024-11-25 13:05:21 +0300

    Update categories_gmv_weekly.sql (#972)

--- tests/spark/pulse/categories_gmv_weekly.sql
+++ tests/spark/pulse/categories_gmv_weekly.sql
@@ -6,7 +6,7 @@ FROM (
     FROM {{ source('joompro_analytics', 'mercadolibre_categories_reviews_cube_js_weekly') }}
     WHERE
         l2_id IS NULL
-        AND partition_date >= '2024-07-01'
+        AND partition_date >= '2024-07-01' and partition_date != '2024-10-28'
     GROUP BY partition_date
 )
 WHERE gmv_total > 2700000000 * 1.25 * 1.5 OR gmv_total < 2700000000 / 1.25 / 1.5
