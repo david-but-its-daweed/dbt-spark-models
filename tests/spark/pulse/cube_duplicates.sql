2c4dce08fe60c225c67346553afb554f1600019b
Dmitrii <BorisovDm@users.noreply.github.com>
2024-11-28 14:54:19 +0100

    Update cube_duplicates.sql (#985)

--- tests/spark/pulse/cube_duplicates.sql
+++ tests/spark/pulse/cube_duplicates.sql
@@ -1,6 +1,8 @@
-SELECT 1 FROM
-(
-SELECT COUNT(DISTINCT p.id) as unique_items, COUNT(p.id) as n_rows
-FROM {{ source('joompro_analytics_mart', 'cube_mlb_products') }} AS p
-)
+SELECT 1
+FROM (
+    SELECT
+        COUNT(DISTINCT p.id) as unique_items,
+        COUNT(p.id) as n_rows
+    FROM {{ source('joompro_analytics_mart', 'cube_mlb_products') }} AS p
+) AS T
 WHERE unique_items != n_rows
