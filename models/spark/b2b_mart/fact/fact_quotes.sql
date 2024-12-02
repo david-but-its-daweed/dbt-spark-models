0009cd5eea2fb93b977dd7550619e43ad88a286e
abadoyan-lab <a.badoyan@joom.com>
2024-12-02 17:56:00 +0400

    [PRO-8715] Create fact_quotes.sql (#997)
    Co-authored-by: abadoyan-lab <abadoyan-lab@users.noreply.github.com>

---
+++ models/spark/b2b_mart/fact/fact_quotes.sql
@@ -0,0 +1,30 @@
+{{ config(
+    schema='b2b_mart',
+    materialized='table',
+    file_format='parquet',
+    meta = {
+      'model_owner' : '@abadoyan',
+      'bigquery_load': 'true',
+      'priority_weight': '150'
+    }
+) }}
+
+SELECT
+    _id,
+    cartPrices AS cart_prices,
+    cnpj,
+    comment,
+    commercialProposalDocSentDate AS commercial_proposal_doc_sent_date,
+    created_ts_msk,
+    currencyRates AS currency_rates,
+    dealFriendlyId AS deal_friendly_id,
+    dealId AS deal_id,
+    deliveryChannel AS delivery_channel,
+    index,
+    paymentMethod AS payment_method,
+    products,
+    stage,
+    subsidies,
+    TIMESTAMP(dbt_valid_from) AS effective_ts_msk,
+    TIMESTAMP(dbt_valid_to) AS next_effective_ts_msk
+FROM {{ ref('scd2_deals_snapshot') }}
