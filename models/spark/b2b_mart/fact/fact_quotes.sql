8ca2158b75afb7134d089ab2c6d635f94947f342
abadoyan-lab <a.badoyan@joom.com>
2024-12-02 18:30:31 +0400

    Update fact_quotes.sql (#998)

--- models/spark/b2b_mart/fact/fact_quotes.sql
+++ models/spark/b2b_mart/fact/fact_quotes.sql
@@ -9,22 +9,21 @@
     }
 ) }}
 
-SELECT
-    _id,
-    cartPrices AS cart_prices,
-    cnpj,
-    comment,
-    commercialProposalDocSentDate AS commercial_proposal_doc_sent_date,
-    created_ts_msk,
-    currencyRates AS currency_rates,
-    dealFriendlyId AS deal_friendly_id,
-    dealId AS deal_id,
-    deliveryChannel AS delivery_channel,
-    index,
-    paymentMethod AS payment_method,
-    products,
-    stage,
-    subsidies,
-    TIMESTAMP(dbt_valid_from) AS effective_ts_msk,
-    TIMESTAMP(dbt_valid_to) AS next_effective_ts_msk
-FROM {{ ref('scd2_deals_snapshot') }}
+SELECT _id AS quote_id,
+       cartPrices AS cart_prices,
+       cnpj,
+       comment,
+       commercialProposalDocSentDate AS commercial_proposal_doc_sent_date,
+       created_ts_msk,
+       currencyRates AS currency_rates,
+       dealFriendlyId AS deal_friendly_id,
+       dealId AS deal_id,
+       deliveryChannel AS delivery_channel,
+       index,
+       paymentMethod AS payment_method,
+       products,
+       stage,
+       subsidies,
+       TIMESTAMP(dbt_valid_from) AS effective_ts_msk,
+       TIMESTAMP(dbt_valid_to) AS next_effective_ts_msk
+FROM {{ ref('scd2_mongo_quotes') }}
