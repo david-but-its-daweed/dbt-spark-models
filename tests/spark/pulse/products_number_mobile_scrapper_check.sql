50901bc01fd70e252bdfcec386c5d5b842e4dc24
Dmitrii <BorisovDm@users.noreply.github.com>
2024-12-12 21:44:34 +0100

    JPA-1110 (#1022)

---
+++ tests/spark/pulse/products_number_mobile_scrapper_check.sql
@@ -0,0 +1,7 @@
+SELECT items_count
+FROM (
+    SELECT APPROX_COUNT_DISTINCT(id) AS items_count,
+    FROM {{ source('joompro_analytics', 'ods_mlb_mobile_api_product_previews') }}
+    WHERE partition_date >= DATE_SUB(CURRENT_DATE(), 3)
+)
+WHERE items_count < 10000000
