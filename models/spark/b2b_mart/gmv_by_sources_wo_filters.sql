7fda8a633e525d582cdeaa35983052b4d16d1b8f
mkirusha <kirill_melnikov@joom.com>
2024-12-14 16:21:38 +0100

    Update gmv_by_sources_wo_filters.sql (#1023)

--- models/spark/b2b_mart/gmv_by_sources_wo_filters.sql
+++ models/spark/b2b_mart/gmv_by_sources_wo_filters.sql
@@ -39,7 +39,18 @@ order_v2_mongo AS (
         u.owner_moderator_id
     FROM {{ ref('fact_order') }} AS fo
     INNER JOIN not_jp_users AS u ON fo.user_id = u.user_id
-    WHERE (order_id in ('6735ffb2f182e3f4a318c0ee','673b8dd150f7107c6a2ee8f0' ) AND fo.next_effective_ts_msk IS NULL) or 
+    WHERE (order_id in ('6735ffb2f182e3f4a318c0ee',
+                        '673b8dd150f7107c6a2ee8f0',
+                        '675af7b808433ed244d897e9',
+                        '675c67042e1d04dfe942898d',
+                        '6759f0a917c167b3cf1d5bad',
+                        '675b02d4781d3609c1e97225',
+                        '675c7411efa9f41236d87439',
+                        '675c6ea3efa9f41236d87427',
+                        '675c462f3703512d44aaa91b',
+                        '675c3d6b3703512d44aaa8e0',
+                        '675c69e52e1d04dfe94289c5'
+    ) AND fo.next_effective_ts_msk IS NULL) or 
         (fo.last_order_status < 60
         AND fo.last_order_status >= 10
         AND fo.next_effective_ts_msk IS NULL
