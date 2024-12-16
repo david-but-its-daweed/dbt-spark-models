2d07af73d1344b5c99c2b36c70d26853a01b34ff
Dmitrii <BorisovDm@users.noreply.github.com>
2024-12-16 21:02:16 +0100

    Update products_number_mobile_scrapper_check.sql (#1033)

--- tests/spark/pulse/products_number_mobile_scrapper_check.sql
+++ tests/spark/pulse/products_number_mobile_scrapper_check.sql
@@ -1,6 +1,6 @@
 SELECT items_count
 FROM (
-    SELECT APPROX_COUNT_DISTINCT(id) AS items_count,
+    SELECT APPROX_COUNT_DISTINCT(id) AS items_count
     FROM {{ source('joompro_analytics', 'ods_mlb_mobile_api_product_previews') }}
     WHERE partition_date >= DATE_SUB(CURRENT_DATE(), 3)
 )
