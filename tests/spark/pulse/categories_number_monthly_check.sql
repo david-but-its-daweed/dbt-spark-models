1a41dc924fd6f5fac1a5bd990d95ac6ce392d739
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2024-12-25 11:30:55 +0300

    Update categories_number_monthly_check.sql (#1052)

--- tests/spark/pulse/categories_number_monthly_check.sql
+++ tests/spark/pulse/categories_number_monthly_check.sql
@@ -1,17 +1,10 @@
 WITH cub AS (
-    SELECT COUNT(*) AS cube_count
+    SELECT DISTINCT category_id AS cube_count
     FROM {{ source('joompro_analytics', 'mercadolibre_categories_cube_js_monthly') }} AS c
     WHERE c.is_current
-),
-
-cat AS (
-    SELECT COUNT(*) AS cat_count
-    FROM {{ source('joompro_mart', 'mercadolibre_categories_view') }}
 )
 
-SELECT
-    cub.cube_count,
-    cat.cat_count
-FROM cub
-CROSS JOIN cat
-WHERE cub.cube_count != cat.cat_count
+SELECT cat.category_id
+FROM {{ source('joompro_mart', 'mercadolibre_categories_view') }} AS cat
+WHERE cat.category_id NOT IN cub
+AND DATE(cat.date_created) < (SELECT MAX(partition_date) + interval 1 MONTH from {{ source('joompro_analytics', 'mercadolibre_categories_cube_js_monthly') }})
