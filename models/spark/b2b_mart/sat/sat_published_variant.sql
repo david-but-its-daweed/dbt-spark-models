13c49a24ba21148d6c13116c76758fbe112f226d
abadoyan-lab <a.badoyan@joom.com>
2025-01-14 19:02:54 +0400

    Delete "public" from scd2_mongo_published_variant.sql (#1078)
    Delete "public" column because it doesn't exist in mongo anymore
    
    ---------
    
    Co-authored-by: abadoyan-lab <abadoyan-lab@users.noreply.github.com>

--- models/spark/b2b_mart/sat/sat_published_variant.sql
+++ models/spark/b2b_mart/sat/sat_published_variant.sql
@@ -7,19 +7,19 @@
       'bigquery_load': 'true'
     }
 ) }}
-SELECT variant_id,
-       TIMESTAMP(created_ts_msk) AS created_ts_msk,
-       enabled,
-       gtin,
-       hs_code,
-       merchant_id,
-       orig_colors,
-       orig_main_image_url,
-       product_id,
-       public,
-       TIMESTAMP(published_ts_msk) AS published_ts_msk,
-       shipping_weight,
-       sku,
-       TIMESTAMP(dbt_valid_from) AS effective_ts_msk,
-       TIMESTAMP(dbt_valid_to) AS next_effective_ts_msk
+SELECT
+    variant_id,
+    TIMESTAMP(created_ts_msk) AS created_ts_msk,
+    enabled,
+    gtin,
+    hs_code,
+    merchant_id,
+    orig_colors,
+    orig_main_image_url,
+    product_id,
+    TIMESTAMP(published_ts_msk) AS published_ts_msk,
+    shipping_weight,
+    sku,
+    TIMESTAMP(dbt_valid_from) AS effective_ts_msk,
+    TIMESTAMP(dbt_valid_to) AS next_effective_ts_msk
 FROM {{ ref('scd2_mongo_published_variant') }}
--- snapshots/spark/b2b_mart/scd2_mongo_published_variant.sql
+++ snapshots/spark/b2b_mart/scd2_mongo_published_variant.sql
@@ -14,19 +14,19 @@
     )
 }}
 
-SELECT   _id AS variant_id,
-         millis_to_ts_msk(createdTimeMs) AS created_ts_msk,
-         enabled,
-         gtin,
-         hsCode AS hs_code,
-         merchantId AS merchant_id,
-         origColors AS orig_colors,
-         origMainImageUrl AS orig_main_image_url,
-         productid AS product_id,
-         public,
-         millis_to_ts_msk(publishedTimeMs) AS published_ts_msk,
-         shippingWeight AS shipping_weight,
-         sku,
-         millis_to_ts_msk(updatedTimeMs) AS update_ts_msk
-FROM {{ source('mongo', 'b2b_core_published_variants_daily_snapshot') }}
+    SELECT
+        _id AS variant_id,
+        MILLIS_TO_TS_MSK(createdTimeMs) AS created_ts_msk,
+        enabled,
+        gtin,
+        hsCode AS hs_code,
+        merchantId AS merchant_id,
+        origColors AS orig_colors,
+        origMainImageUrl AS orig_main_image_url,
+        productid AS product_id,
+        MILLIS_TO_TS_MSK(publishedTimeMs) AS published_ts_msk,
+        shippingWeight AS shipping_weight,
+        sku,
+        MILLIS_TO_TS_MSK(updatedTimeMs) AS update_ts_msk
+    FROM {{ source('mongo', 'b2b_core_published_variants_daily_snapshot') }}
 {% endsnapshot %}
