4aa424443a5b9b2da75d63d879f4471065159571
tigran-joom <tigran@joom.com>
2025-01-29 17:04:35 +0400

    [AN-3866] Added Adtech_revenue Model (#1140)
    Added Adtech Revenue Model in order to use it in Spark in the future &
    calculate CP, Margin for the dashboards
    
    ---------
    
    Co-authored-by: tigran-joom <tigran-joom@users.noreply.github.com>

---
+++ models/spark/core/ml_product/adtech_revenue.sql
@@ -0,0 +1,33 @@
+{{
+    config(
+        materialized='table',
+        meta = {
+            'model_owner' : '@general_analytics',
+            'bigquery_load': 'true',
+            'bigquery_partitioning_date_column': 'date_msk',
+            'bigquery_check_counts_max_diff_fraction': '0.01'
+        }
+    )
+}}
+
+
+SELECT
+    adtech.partition_date AS date_msk,
+    adtech.device_id,
+    adtech.user_id,
+    link.real_user_id,
+    COALESCE(active_devices.country_code, UPPER(dim_device.pref_country), UPPER(adtech.country)) AS country_code,
+    COALESCE(active_devices.platform, dim_device.os_type, adtech.os_type) AS platform,
+    SUM(adtech.adtech_revenue) AS adtech_revenue
+FROM {{ source('mart', 'adtech_revenues') }} AS adtech
+LEFT JOIN {{ source('mart', 'dim_device') }} AS dim_device -- используем в случае, если по gold не находится пара (из-за стыка дат)
+    ON
+        adtech.device_id = dim_device.device_id
+        AND dim_device.next_effective_ts = TIMESTAMP '9999-12-31 23:59:59 UTC'
+LEFT JOIN {{ ref('gold_active_devices') }} AS active_devices -- используем с первым периоритетом, там самая корректная логика для страны сейчас
+    ON
+        active_devices.device_id = adtech.device_id
+        AND active_devices.date_msk = adtech.partition_date
+LEFT JOIN {{ source('mart', 'link_device_real_user') }} AS link USING (device_id) -- берем real_user_id, из gold не берем также из-за проблемы стыка дат
+WHERE adtech.partition_date >= '2022-01-01'
+GROUP BY 1, 2, 3, 4, 5, 6
