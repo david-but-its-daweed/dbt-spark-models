60075b063a88940366ba64e9e0dc4e74115d7b2b
abadoyan-lab <a.badoyan@joom.com>
2025-02-14 18:42:25 +0400

    Update sat_published_variant.sql (#1172)

--- models/spark/b2b_mart/sat/sat_published_variant.sql
+++ models/spark/b2b_mart/sat/sat_published_variant.sql
@@ -7,19 +7,20 @@
       'bigquery_load': 'true'
     }
 ) }}
-SELECT
-    variant_id,
-    TIMESTAMP(created_ts_msk) AS created_ts_msk,
-    enabled,
-    gtin,
-    hs_code,
-    merchant_id,
-    orig_colors,
-    orig_main_image_url,
-    product_id,
-    TIMESTAMP(published_ts_msk) AS published_ts_msk,
-    shipping_weight,
-    sku,
-    TIMESTAMP(dbt_valid_from) AS effective_ts_msk,
-    TIMESTAMP(dbt_valid_to) AS next_effective_ts_msk
-FROM {{ ref('scd2_mongo_published_variant') }}
+
+
+SELECT _id AS variant_id,
+       TIMESTAMP(MILLIS_TO_TS_MSK(createdTimeMs)) AS created_ts_msk,
+       enabled,
+       gtin,
+       hsCode AS hs_code,
+       merchantId AS merchant_id,
+       origColors AS orig_colors,
+       origMainImageUrl AS orig_main_image_url,
+       productid AS product_id,
+       TIMESTAMP(MILLIS_TO_TS_MSK(createdTimeMs)) AS published_ts_msk,
+       shippingWeight AS shipping_weight,
+       sku,
+       TIMESTAMP(dbt_valid_from) AS effective_ts_msk,
+       TIMESTAMP(dbt_valid_to) AS next_effective_ts_msk
+FROM {{ ref('scd2_published_variants_snapshot') }}
