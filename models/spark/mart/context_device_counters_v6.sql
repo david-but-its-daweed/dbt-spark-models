801c44b611bd102116ee5d22a0d7ba238f557c5d
aleksandrov@joom.com <aleksandrov@joom.com>
2025-02-26 22:22:04 +0000

    Reload only last 3 days to BQ for context_device_counters_v6 model

--- models/spark/mart/context_device_counters_v6.sql
+++ models/spark/mart/context_device_counters_v6.sql
@@ -4,7 +4,6 @@
     meta = {
       'model_owner' : '@analytics.duty',
       'bigquery_load': 'true',
-      'bigquery_overwrite': 'true',
       'priority_weight' : '150',
       'bigquery_clustering_columns' : ['type', 'context_name'],
       'bigquery_partitioning_date_column' : 'partition_date',
@@ -12,13 +11,14 @@
     }
 ) }}
 
-select device_id,
-       ephemeral,
-       context_name,
-       is_adtech_promoted,
-       is_buying_user,
-       is_last_context,
-       type,
-       count,
-       CAST(partition_date as DATE) partition_date
+SELECT
+    device_id,
+    ephemeral,
+    context_name,
+    is_adtech_promoted,
+    is_buying_user,
+    is_last_context,
+    type,
+    count,
+    CAST(partition_date AS DATE) AS partition_date
 FROM {{ source('recom', 'context_device_counters_v6') }}
