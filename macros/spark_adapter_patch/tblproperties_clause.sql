c78240fcd63dddc432fc368dab9f6204f1d949d9
msaf <msaf@joom.com>
2025-03-14 13:47:51 +0000

    support passing tblproperties using vars

--- macros/spark_adapter_patch/tblproperties_clause.sql
+++ macros/spark_adapter_patch/tblproperties_clause.sql
@@ -1,9 +1,16 @@
 {% macro tblproperties_clause() %}
   {%- set tblproperties = config.get('tblproperties', {}) -%}
+  {%- set tblproperties2 = var("tblproperties") or {} -%}
 
-  {%- if tblproperties %}
+  {%- if tblproperties or tblproperties2 %}
     TBLPROPERTIES (
     {%- for prop, val in tblproperties.items() %}
+      {%- set escaped_value = val | replace('\'', '\\\'') %}
+      '{{ prop }}'='{{ escaped_value }}'
+      {%- if not loop.last -%},{%- endif -%}
+    {% endfor %}
+
+    {%- for prop, val in tblproperties2.items() %}
       {%- set escaped_value = val | replace('\'', '\\\'') %}
       '{{ prop }}'='{{ escaped_value }}'
       {%- if not loop.last -%},{%- endif -%}
