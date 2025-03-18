7ef36c75a9de1077889928012a3d119e599280db
mkirusha <kirill_melnikov@joom.com>
2025-03-18 15:40:22 +0100

    Update fact_marketing_deals_interactions.sql (#1323)

--- models/spark/b2b_mart/fact/marketing/fact_marketing_deals_interactions.sql
+++ models/spark/b2b_mart/fact/marketing/fact_marketing_deals_interactions.sql
@@ -64,15 +64,42 @@ first_utm_medium,
 first_source,
 first_type,
 number_visit as number_of_interactions,
-row_number() over (partition by user_id,deal_id order by cast(visit_ts_msk as date) desc, traffic_type ) qualify_rn 
+row_number() over (partition by user_id,deal_id order by cast(visit_ts_msk as date) desc, traffic_type ) qualify_rn,
+row_number() over (partition by user_id,deal_id,traffic_type order by visit_ts_msk  desc ) qualify_rn_type,
+datediff(created_ts_msk, visit_date) AS days_difference
 from deals 
 left join interactions using(user_id ) 
 left join dop_info using(user_id ) 
 where  1=1
 and cast(created_ts_msk as date) >=  visit_date
-order by user_id,deal_id, visit_ts_msk)
-
+order by user_id,deal_id, visit_ts_msk),
+adding_window as (
 select 
-* 
+*,
+row_number() over (partition by user_id,deal_id order by case when type = 'organic' then 100 else 1  end ) filter_rn  
 from data 
 where qualify_rn = 1 
+or ( qualify_rn_type = 1 and type != 'organic' and days_difference < 14)
+)
+ 
+select 
+user_id,
+deal_id,
+visit_ts_msk,
+visit_date,
+utm_campaign,
+utm_source,
+utm_medium,
+source,
+type,
+first_utm_campaign ,
+first_utm_sourceas ,
+first_utm_medium,
+first_source,
+first_type,
+number_of_interactions,
+qualify_rn,
+qualify_rn_type,
+days_difference
+from adding_window    
+where filter_rn = 1 
