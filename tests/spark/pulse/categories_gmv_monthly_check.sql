99a09d6b5a535695a0ddedf1b7dd7b326243cb05
NastiaMitiushkina <115141688+NastiaMitiushkina@users.noreply.github.com>
2025-03-19 18:02:43 +0300

    Update categories_gmv_monthly_check.sql (#1337)

--- tests/spark/pulse/categories_gmv_monthly_check.sql
+++ tests/spark/pulse/categories_gmv_monthly_check.sql
@@ -6,4 +6,4 @@ FROM (
         l2_id IS NULL
         AND partition_date >= '2024-07-01'
 )
-WHERE gmv_total > 2700000000 * 4 * 1.25 * 1.7 OR gmv_total < 2700000000 * 4 / 1.25 / 1.5
+WHERE (gmv_total > 3645000000 * 4 * 1.25 * 1.7 OR gmv_total < 3645000000 * 4 / 1.25 / 1.5) AND dayofmonth(current_date()) <= 4
