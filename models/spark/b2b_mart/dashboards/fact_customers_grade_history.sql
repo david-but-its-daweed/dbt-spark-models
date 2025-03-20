fd8144920d70f4d9e59998a159711e3ea4b70fc7
abadoyan-lab <a.badoyan@joom.com>
2025-03-20 18:16:46 +0400

    [PRO-9821] Create fact_customers_grade_history.sql (#1345)

---
+++ models/spark/b2b_mart/dashboards/fact_customers_grade_history.sql
@@ -0,0 +1,61 @@
+{{ config(
+    schema='b2b_mart',
+    materialized='view',
+    meta = {
+      'model_owner' : '@abadoyan',
+      'team': 'general_analytics',
+      'bigquery_load': 'true',
+      'priority_weight': '150'
+    }
+) }}
+
+
+SELECT _id AS user_id,
+       CASE
+           WHEN grade = 0 THEN 'unknown'
+           WHEN grade = 1 THEN 'A'
+           WHEN grade = 2 THEN 'B'
+           WHEN grade = 3 THEN 'C'
+           WHEN grade = 4 THEN 'D'
+           ELSE 'unknown'
+       END AS grade,
+       CASE
+           WHEN ROW_NUMBER() OVER (PARTITION BY _id ORDER BY updated_ts) = 1
+               THEN TIMESTAMP('2020-01-01 00:00:00')
+           ELSE updated_ts
+       END AS grade_from_ts,
+       LEAD(updated_ts) OVER (PARTITION BY _id ORDER BY updated_ts) AS grade_to_ts
+FROM (
+    SELECT
+        _id,
+        initialGrade AS grade,
+        CURRENT_TIMESTAMP() + INTERVAL '3 hour' AS updated_ts
+    FROM mongo.b2b_core_customers_daily_snapshot
+    WHERE SIZE(gradeInfoHistory) = -1
+    UNION ALL
+    SELECT 
+        _id,
+        grade_info.grade AS grade,
+        millis_to_ts_msk(grade_info.utms) AS updated_ts
+    FROM (
+        SELECT 
+            _id,
+            initialGrade,
+            EXPLODE(gradeInfoHistory) AS grade_info
+        FROM mongo.b2b_core_customers_daily_snapshot
+    ) AS history
+    UNION ALL
+    SELECT 
+        _id,
+        initialGrade AS grade,
+        MIN(millis_to_ts_msk(grade_info.utms)) - INTERVAL '1 minute' AS updated_ts
+    FROM (
+        SELECT
+            _id,
+            initialGrade,
+            EXPLODE(gradeInfoHistory) AS grade_info
+        FROM mongo.b2b_core_customers_daily_snapshot
+    ) AS history
+    GROUP BY 1,2
+) AS m
+ORDER BY 1,3
