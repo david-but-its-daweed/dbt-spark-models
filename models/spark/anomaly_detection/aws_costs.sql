05e2fd030d3615d9a8229f37596dee1ab01110cd
aleksandrov@joom.com <aleksandrov@joom.com>
2025-04-13 12:40:34 +0000

    Custom owners for anomalies

--- models/spark/anomaly_detection/aws_costs.sql
+++ models/spark/anomaly_detection/aws_costs.sql
@@ -15,7 +15,21 @@
       'anomalies_significance_score': 'cumulative_deviation',
       'anomalies_significance_threshold': '0.2',
       'anomalies_max_count': '10',
-      'anomalies_main_owners': ['@aleksandrov', '@aleksandrov'],
+      'anomalies_main_owners': ['@aleksandrov'],
+      'anomalies_custom_owners': [
+          {
+            'owners': ['@aleksandrov', '@aleksandrov'],
+            'dimensions': {
+              'service': ['AmazonVPC', 'AWSELB'],
+            },
+          },
+          {
+            'owners': ['@aleksandrov', '@aleksandrov'],
+            'dimensions': {
+              'service': ['AmazonRDS'],
+            },
+          },
+        ]
     },
     tags=['platform']
 ) }}
