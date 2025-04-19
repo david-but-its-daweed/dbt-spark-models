03d12f61bc9c76070679f5691a9f26f027540fb2
anna zaychik <67829640+anyazay@users.noreply.github.com>
2025-04-19 22:44:11 +0100

    PhAn-989 New dashboard for Onfy Updates (#1458)

--- models/spark/onfy/onfy_metric_sheet.sql
+++ models/spark/onfy/onfy_metric_sheet.sql
@@ -34,11 +34,9 @@ with order_data as
         and currency = 'EUR'
         and date_trunc('day', transaction_date) < current_date() 
     group by
-        grouping sets (
-            (date_trunc('day', transaction_date), date_trunc('month', transaction_date), date_trunc('quarter', transaction_date)), 
-            (date_trunc('month', transaction_date), date_trunc('quarter', transaction_date)),
-            (date_trunc('quarter', transaction_date))
-        )
+        date_trunc('day', transaction_date),
+        date_trunc('month', transaction_date),
+        date_trunc('quarter', transaction_date)
 ),
 
 
@@ -50,30 +48,20 @@ ads_spends_data as
         date_trunc('quarter', campaign_date_utc) as ads_spends_quarter,
         sum(spend) as spend,
         sum(clicks) as clicks
-    from  {{source('onfy_mart', 'ads_spends')}}
+    from {{source('onfy_mart', 'ads_spends')}}
+    where 1=1
+        and campaign_date_utc < current_date()
     group by 
-        grouping sets (
-            (campaign_date_utc, date_trunc('month', campaign_date_utc), date_trunc('quarter', campaign_date_utc)), 
-            (date_trunc('month', campaign_date_utc), date_trunc('quarter', campaign_date_utc)),
-            (date_trunc('quarter', campaign_date_utc))
-        )
+        date_trunc('day', campaign_date_utc),
+        date_trunc('month', campaign_date_utc),
+        date_trunc('quarter', campaign_date_utc)
 ),
 
 base as 
 (
     select 
         current_date() as date_updated,
-        case
-            when coalesce(transaction_date, ads_spends_date) is null and 
-                coalesce(transaction_month, ads_spends_month) is not null and
-                coalesce(transaction_quarter, ads_spends_quarter) is not null 
-            then 'month'
-            when coalesce(transaction_date, ads_spends_date) is null and 
-                coalesce(transaction_month, ads_spends_month) is null and
-                coalesce(transaction_quarter, ads_spends_quarter) is not null 
-            then 'quarter'
-            else 'day'
-        end as mode,
+        'day' as mode,
 
         date_trunc('month', current_date()) = transaction_month as current_month,
         if(date_trunc('month', current_date()) = transaction_month, date_part('day', current_date() - interval 1 day), null) as max_date,
@@ -105,22 +93,87 @@ base as
         on coalesce(order_data.transaction_date, '') = coalesce(ads_spends_data.ads_spends_date, '')
         and coalesce(order_data.transaction_month, '') = coalesce(ads_spends_data.ads_spends_month, '')
         and order_data.transaction_quarter = ads_spends_data.ads_spends_quarter
+),
+
+precalculation as 
+(
+    select 
+        *,
+        date between current_date() - interval 7 day and current_date() - interval 1 day as last_n_days,
+        '7 days' as forecast_base
+    from base
+    union all
+    select 
+        *,
+        date between current_date() - interval 3 day and current_date() - interval 1 day as last_n_days,
+        '3 days' as forecast_base
+    from base
+    union all
+    select 
+        *,
+        current_month as last_n_days,
+        'month' as forecast_base
+    from base
+),
+
+forecasting as 
+(
+    select 
+        date_updated,
+        'forecasted day' as mode,
+        current_month,
+        max_date,
+        dts.dates,
+        precalculation.month,
+        precalculation.quarter,
+        avg(gmv_initial) as gmv_avg_distributed,
+        avg(gross_profit_final) as gross_avg_distributed,
+        avg(promocode_discount) as promocode_discount_avg_distributed,
+        avg(orders) as orders_avg_distributed,
+        avg(first_orders) as first_orders_avg_distributed,
+        avg(first_gmv_initial) as first_gmv_initial_avg_distributed,
+        avg(first_gross_profit_final) as first_gross_profit_final_avg_distributed,
+        avg(first_promocode_discount) as first_promocode_discount_avg_distributed,
+        avg(gmv_initial / orders) as average_check_avg_distributed,
+        avg(first_gmv_initial / first_orders) as first_average_check_avg_distributed,
+        avg(media_revenue) as media_revenue_avg_distributed,
+        avg(service_fee) as service_fee_avg_distributed,
+        avg(commission_revenue) as commission_revenue_avg_distributed,
+    
+        avg(spend) as spend_avg_distributed,
+        avg(clicks) as clicks_avg_distributed,
+    
+        avg(spend / orders) as general_cpo_avg_distributed,
+        avg(spend / first_orders) as general_cppu_avg_distributed,
+        
+        last_n_days,
+        forecast_base
+    from precalculation
+    join 
+        (
+            select
+                explode(sequence(current_date(), date_add(current_date(), date_diff(last_day(current_date()), current_date())))) AS dates,
+                date_trunc("month", current_date()) as month
+        ) as dts
+        on dts.month = precalculation.month
+    where 1=1
+        and current_month
+        and last_n_days
+    group by 
+        date_updated,
+        current_month,
+        max_date,
+        dates,
+        precalculation.month,
+        precalculation.quarter,
+        last_n_days,
+        forecast_base
 )
 
 select 
-    *,
-    date between current_date() - interval 7 day and current_date() - interval 1 day as last_n_days,
-    '7 days' as forecast_base
-from base
+    *
+from precalculation
 union all
 select 
-    *,
-    date between current_date() - interval 3 day and current_date() - interval 1 day as last_n_days,
-    '3 days' as forecast_base
-from base
-union all
-select 
-    *,
-    current_month as last_n_days,
-    'month' as forecast_base
-from base
+    *
+from forecasting
