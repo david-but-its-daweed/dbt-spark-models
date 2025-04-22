f22a8290ac42a92a487052a635275b624c07753b
David <44006282+david-but-its-daweed@users.noreply.github.com>
2025-04-22 10:49:12 -0700

    added sesion_id (#1478)
    Co-authored-by: daweed@joom.com <daweed@joom.com>
    Co-authored-by: david-but-its-daweed <david-but-its-daweed@users.noreply.github.com>

--- models/spark/b2b_mart/ss/ss_events_by_session.sql
+++ models/spark/b2b_mart/ss/ss_events_by_session.sql
@@ -11,50 +11,46 @@
 
 
 WITH bots AS (
-    SELECT
-        device_id,
-        MAX(1) AS bot_flag
+    SELECT DISTINCT device_id
     FROM threat.bot_devices_joompro
-    WHERE is_device_marked_as_bot
-       OR is_retrospectively_detected_bot
-    GROUP BY device_id
+    WHERE is_device_marked_as_bot OR is_retrospectively_detected_bot
 ),
 
 events AS (
-    SELECT
-        DISTINCT user['userId'] AS user_id,
-        de.device.id AS device_id,
-        de.device.osType AS device_os_type,
+    SELECT DISTINCT
+        `user`.userId AS user_id,
+        device.id AS device_id,
+        device.osType AS device_os_type,
         type,
         event_ts_msk,
         TO_JSON(
             MAP_FILTER(
                 MAP(
-                    'pageUrl',                CAST(payload.pageUrl AS STRING),
-                    'page',                   CAST(payload.page AS STRING),
-                    'pageName',               CAST(payload.pageName AS STRING),
-                    'source',                 CAST(payload.source AS STRING),
-                    'product_id',             CAST(payload.productId AS STRING),
-                    'timeBeforeClick',        CAST(payload.timeBeforeClick AS STRING),
-                    'productsNumber',         CAST(payload.productsNumber AS STRING),
-                    'query',                  CAST(payload.query AS STRING),
-                    'topProductsNumber',      CAST(payload.topProductsNumber AS STRING),
-                    'hasNextPage',            CAST(payload.hasNextPage AS STRING),
-                    'searchResultsUniqId',    CAST(payload.searchResultsUniqId AS STRING),
-                    'isSearchByImage',        CAST(payload.isSearchByImage AS STRING),
-                    'index',                  CAST(payload.index AS STRING),
-                    'position',               CAST(payload.position AS STRING),
-                    'promotionId',            CAST(payload.promotionId AS STRING)
+                    'pageUrl', CAST(payload.pageUrl AS STRING),
+                    'page', CAST(payload.page AS STRING),
+                    'pageName', CAST(payload.pageName AS STRING),
+                    'source', CAST(payload.source AS STRING),
+                    'product_id', CAST(payload.productId AS STRING),
+                    'timeBeforeClick', CAST(payload.timeBeforeClick AS STRING),
+                    'productsNumber', CAST(payload.productsNumber AS STRING),
+                    'query', CAST(payload.query AS STRING),
+                    'topProductsNumber', CAST(payload.topProductsNumber AS STRING),
+                    'hasNextPage', CAST(payload.hasNextPage AS STRING),
+                    'searchResultsUniqId', CAST(payload.searchResultsUniqId AS STRING),
+                    'isSearchByImage', CAST(payload.isSearchByImage AS STRING),
+                    'index', CAST(payload.index AS STRING),
+                    'position', CAST(payload.position AS STRING),
+                    'promotionId', CAST(payload.promotionId AS STRING)
                 ),
                 (k, v) -> v IS NOT NULL
             )
         ) AS event_params
-    FROM {{ source('b2b_mart', 'device_events') }} AS de
-    LEFT JOIN bots ON de.device.id = bots.device_id
-    WHERE partition_date >= '2024-04-01'
-      AND bot_flag IS NULL
-      AND user['userId'] IS NOT NULL
-      AND type NOT IN ('deviceCreate')
+    FROM {{ source('b2b_mart', 'device_events') }}
+    WHERE
+        partition_date >= '2024-04-01'
+        AND device.id NOT IN (SELECT b.device_id FROM bots AS b)
+        AND `user`.userId IS NOT NULL
+        AND type NOT IN ('deviceCreate')
 ),
 
 events_with_gap AS (
@@ -63,8 +59,8 @@ events_with_gap AS (
         device_id,
         device_os_type,
         CASE
-            WHEN device_os_type IN ('android','ios','tizen','harmonyos' ) THEN 'mobile'
-            WHEN device_os_type IN ('ubuntu','linux','mac os','windows','chromium os') THEN 'desktop'
+            WHEN device_os_type IN ('android', 'ios', 'tizen', 'harmonyos') THEN 'mobile'
+            WHEN device_os_type IN ('ubuntu', 'linux', 'mac os', 'windows', 'chromium os') THEN 'desktop'
             ELSE 'other'
         END AS device_platform,
         type,
@@ -82,7 +78,7 @@ sessions_numbered AS (
         /* Если больше 30 мин - новая сессия */
         CASE
             WHEN prev_event_ts_msk IS NULL THEN 1
-            WHEN UNIX_TIMESTAMP(event_ts_msk) - UNIX_TIMESTAMP(prev_event_ts_msk) > 60*30 THEN 1
+            WHEN UNIX_TIMESTAMP(event_ts_msk) - UNIX_TIMESTAMP(prev_event_ts_msk) > 60 * 30 THEN 1
             ELSE 0
         END AS is_new_session_flag
     FROM events_with_gap
@@ -91,7 +87,7 @@ sessions_numbered AS (
 sessions_with_id AS (
     SELECT
         *,
-        sum(is_new_session_flag) OVER (
+        SUM(is_new_session_flag) OVER (
             PARTITION BY user_id ORDER BY event_ts_msk
         ) AS session_num
     FROM sessions_numbered
@@ -100,6 +96,7 @@ sessions_with_id AS (
 sessions_with_rownum AS (
     SELECT
         *,
+        CONCAT(user_id, 's', session_num) AS session_id,
         ROW_NUMBER() OVER (
             PARTITION BY user_id, session_num
             ORDER BY event_ts_msk ASC
@@ -114,6 +111,7 @@ sessions_with_rownum AS (
 final AS (
     SELECT
         user_id,
+        session_id,
         session_num,
         TO_TIMESTAMP(MIN(event_ts_msk)) AS session_start,
         TO_TIMESTAMP(MAX(event_ts_msk)) AS session_end,
@@ -140,12 +138,12 @@ final AS (
             )
         ) AS unique_devices_in_session
     FROM sessions_with_rownum
-    GROUP BY 1,2
+    GROUP BY 1, 2, 3
 )
 
-
-SELECT 
+SELECT
     user_id,
+    session_id,
     session_num,
     session_start,
     session_end,
