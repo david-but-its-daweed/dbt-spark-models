bb9a553f7cc8f05f615274d3d070f80e0764b226
Dmitrii <BorisovDm@users.noreply.github.com>
2025-04-24 14:31:17 +0200

    JPA-1740 return creation_date_null_check (#1487)

--- tests/spark/pulse/creation_date_null_check.sql
+++ tests/spark/pulse/creation_date_null_check.sql
@@ -1,6 +1,5 @@
 SELECT id
 FROM {{ source('joompro_analytics_mart', 'mercadolibre_products_snapshot') }}
-WHERE 1 != 1
-    -- listing_creation_date IS NULL
-    -- AND effective_ts >= DATE_SUB(CURRENT_DATE(), 7)
-    -- AND effective_ts < DATE_SUB(CURRENT_DATE(), 2)
+WHERE listing_creation_date IS NULL
+     AND effective_ts >= DATE_SUB(CURRENT_DATE(), 7)
+     AND effective_ts < DATE_SUB(CURRENT_DATE(), 2)
