3f37d11f7b2426903fc93fce74a673040eb11804
abadoyan-lab <a.badoyan@joom.com>
2025-05-12 19:04:38 +0400

    Update purchasing_and_production_report.sql (#1528)

--- models/spark/b2b_mart/dashboards/purchasing_and_production_report.sql
+++ models/spark/b2b_mart/dashboards/purchasing_and_production_report.sql
@@ -195,7 +195,7 @@ WITH procurement_orders AS (
            MIN(CASE WHEN procurement_sub_status_name = 'psiApprovedManually' THEN status_time END) AS sub_status_psi_approved_manually_ts,
            MIN(CASE WHEN procurement_sub_status_name = 'packingAndLabeling' THEN status_time END) AS sub_status_packing_and_labeling_ts,
            MIN(CASE WHEN procurement_sub_status_name = 'readyForShipment' THEN status_time END) AS sub_status_ready_for_shipment_ts,
-           MIN(CASE WHEN procurement_sub_status_name = 'shippedBy3PL' THEN status_time END) AS sub_status_shipped_by_3pl_ts,
+           MIN(CASE WHEN procurement_sub_status_name IN ('shippedBy3PL', 'shipped') THEN status_time END) AS sub_status_shipped_by_3pl_ts,
            MIN(CASE WHEN procurement_sub_status_name = 'finalPaymentRequested' THEN status_time END) AS sub_status_final_payment_requested_ts,
            MIN(CASE WHEN procurement_sub_status_name = 'finalPaymentInProgress' THEN status_time END) AS sub_status_final_payment_in_progress_ts,
            MIN(CASE WHEN procurement_sub_status_name = 'finalPaymentAcquired' THEN status_time END) AS sub_status_final_payment_acquired_ts,
