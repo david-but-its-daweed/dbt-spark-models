7dd2bfba4184f70c67724f6f39220be52aad7db3
msaf <msaf@joom.com>
2025-05-14 17:07:00 +0100

    drop obsolete run scripts

--- run.sh
+++ run.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
-set -euo pipefail
 
-source infra/functions.sh
 
-DBT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
-
-# Interoperable btw Linux and OSX version of `date -d "<param> day ago" '+%Y-%m-%d'`
-function days_ago {
-PYTHON_AGO_ARG="$1" python3 - <<END
-import os
-from datetime import datetime, timedelta
-ago = int(os.environ['PYTHON_AGO_ARG'])
-ago_date = datetime.now() - timedelta(ago)
-print(ago_date.strftime('%Y-%m-%d'))
-END
-}
-
-DEFAULT_START_DATE=$(days_ago 1)
-DEFAULT_END_DATE=$(days_ago 0)
-DBT_VARS="{'start_date_ymd':'$DEFAULT_START_DATE','end_date_ymd':'$DEFAULT_END_DATE','table_name':'gburg.test'}"
-
-dbt run --vars $DBT_VARS $@
+python3 ./run.py --changed-only $@
