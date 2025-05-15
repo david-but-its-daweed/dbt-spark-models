d381dfcef2df7443eded7bde44aae4fbf4c72d34
abadoyan-lab <a.badoyan@joom.com>
2025-05-15 17:32:54 +0400

    [PRO-10445[ Create sourcing_report.sql (#1544)

---
+++ models/spark/b2b_mart/dashboards/sourcing_report_issues.sql
@@ -0,0 +1,183 @@
+{{ config(
+    schema='b2b_mart',
+    materialized='table',
+    file_format='parquet',
+    meta = {
+      'model_owner' : '@abadoyan',
+      'bigquery_load': 'true'
+    }
+) }}
+
+
+WITH fact_deals AS (
+    SELECT deal_id,
+           MAX(deal_type) AS deal_type,
+           MAX(sourcing_deal_type) AS sourcing_deal_type,
+           MAX(CASE WHEN self_service THEN 1 ELSE 0 END) AS self_service
+    FROM {{ ref('fact_deals') }}
+    WHERE next_effective_ts_msk IS NULL
+    GROUP BY 1
+),
+fact_issues AS (
+    SELECT *
+    FROM {{ ref('fact_issues') }}
+    WHERE next_effective_ts_msk IS NULL
+      AND coalesce(assignee_role, 'null') IN ('null', 'SourcingManager', 'MerchantSupport')
+),
+users AS (
+    SELECT user_id,
+           coalesce(nullIf(current_grade, 'unknown'), grade) AS current_grade
+    FROM {{ ref('fact_customers') }}
+),
+grade_history AS (
+    SELECT user_id,
+           grade,
+           grade_from_ts,
+           grade_to_ts
+    FROM {{ ref('fact_customers_grade_history') }}
+),
+sourcing_customer_reqeusts AS (
+    SELECT fcr.*,
+           fd.self_service,
+           coalesce(u.current_grade, 'unknown') AS current_grade
+    FROM (
+        SELECT customer_request_id,
+               deal_id,
+               user_id,
+               category_name,
+               status AS customer_request_status,
+               planned_offer_cost,
+               planned_offer_currency,
+               creation_source,
+               fake_door,
+               manual,
+               standart_deal,
+               rfq_deal,
+               sample,
+               merchant_price_currency,
+               merchant_price_per_item / 1000000 AS merchant_price_per_item,
+               merchant_price_total_amount / 1000000 AS merchant_price_total_amount,
+               created_time AS customer_request_created_time,
+               updated_time AS customer_request_updated_time
+        FROM {{ ref('fact_customer_requests') }}
+        WHERE country != 'RU'
+          AND DATE(created_time) >= '2024-04-01'
+          AND next_effective_ts_msk IS NULL
+    ) AS fcr
+    LEFT JOIN fact_deals AS fd ON fcr.deal_id = fd.deal_id
+    LEFT JOIN users AS u ON fcr.user_id = u.user_id
+),
+teams AS (
+    SELECT t1.issue_id,
+           t1.count_unique_teams,
+           t3.assignee_history,
+           ARRAY_AGG(
+                STRUCT(
+                    t2.team_ts,
+                    t2.team
+                )
+                ORDER BY t2.team_ts
+            ) AS team_history
+    FROM (
+        SELECT issue_id,
+               COUNT(DISTINCT team) AS count_unique_teams
+        FROM {{ ref('fact_issues_assignee_history') }}
+        GROUP BY 1
+    ) AS t1
+    LEFT JOIN (
+        SELECT DISTINCT issue_id,
+               team_ts,
+               team
+        FROM {{ ref('fact_issues_assignee_history') }}
+        WHERE team_ts IS NOT NULL
+    ) AS t2 ON t1.issue_id = t2.issue_id
+    LEFT JOIN (
+        SELECT issue_id,
+               ARRAY_AGG(
+                   STRUCT(
+                       assignee_id,
+                       assignee_email,
+                       assignee_role,
+                       assignee_ts
+                   )
+                   ORDER BY assignee_ts
+               ) AS assignee_history
+        FROM {{ ref('fact_issues_assignee_history') }}
+        WHERE assignee_id IS NOT NULL
+        GROUP BY 1
+    ) AS t3 ON t1.issue_id = t3.issue_id
+    GROUP BY 1,2,3
+),
+issues_ AS (
+    SELECT fcr.customer_request_id,
+           fcr.deal_id,
+           fcr.user_id,
+           fcr.category_name,
+           fcr.customer_request_status,
+           fcr.planned_offer_cost,
+           fcr.planned_offer_currency,
+           fcr.creation_source,
+           fcr.fake_door,
+           fcr.manual,
+           fcr.standart_deal,
+           fcr.rfq_deal,
+           fcr.sample,
+           fcr.customer_request_created_time,
+           fcr.customer_request_updated_time,
+           fi.issue_id,
+           fi.type AS issue_type,
+           fi.status AS issue_status,
+           fi.reject_reason AS issue_reject_reason,
+           fi.created_time AS issue_created_time,
+           fi.start_time AS issue_start_time,
+           fi.status_time AS issue_status_time,
+           CASE WHEN fi.status IN ('Completed', 'Failed') THEN fi.status_time END AS issue_ended_time,
+           fi.issue_friendly_id,
+           fi.parent_issue_id,
+           fi.reporter_email,
+           fi.reporter_role,
+           fi.first_assignee_email,
+           fi.first_assignee_role,
+           fi.first_time_assigned,
+           fi.assignee_email AS last_assignee_email,
+           fi.assignee_role AS last_assignee_role,
+           fi.last_time_assigned,
+           fi.times_assigned,
+           t.assignee_history,
+           t.team_history[SAFE_ORDINAL(1)].team AS first_team,
+           t.team_history[SAFE_ORDINAL(1)].team_ts AS first_team_ts,
+           t.team_history[SAFE_ORDINAL(ARRAY_LENGTH(team_history))].team AS last_team,
+           t.team_history[SAFE_ORDINAL(ARRAY_LENGTH(team_history))].team_ts AS last_team_ts,
+           ARRAY_LENGTH(t.team_history) - 1 AS count_changes_team,
+           t.team_history
+    FROM sourcing_customer_reqeusts AS fcr
+    /* Задачи сорсеров (в админке) */
+    LEFT JOIN fact_issues AS fi ON fcr.customer_request_id = fi.entity_id
+    /* Информация по команде по задаче */
+    LEFT JOIN teams AS t ON fi.issue_id = t.issue_id
+),
+issues AS (
+    SELECT issues_.*,
+           fd.self_service,
+           fd.deal_type,
+           fd.sourcing_deal_type,
+           CASE WHEN issue_ended_time IS NOT NULL THEN 1 ELSE 0 END AS is_issue_ended,
+           CASE WHEN issue_ended_time IS NOT NULL THEN DATE_DIFF(coalesce(last_time_assigned, issue_ended_time), issue_created_time, second) END AS in_queue_second,
+           CASE WHEN issue_ended_time IS NOT NULL THEN DATE_DIFF(issue_ended_time, last_time_assigned, second) END AS processing_second,
+           CASE WHEN issue_ended_time IS NOT NULL THEN DATE_DIFF(issue_ended_time, issue_created_time, second) END AS from_create_to_end_second,
+           CASE WHEN issue_status = 'Completed' AND issue_type = 'FindOffer' THEN
+               CASE
+                   WHEN sourcing_deal_type = 'vip' THEN 2
+                   ELSE 1
+               END
+               ELSE 0
+           END AS task_score,
+           coalesce(gh.grade, 'unknown') AS grade_by_issue_created_time
+    FROM issues_
+    LEFT JOIN fact_deals AS fd ON issues_.deal_id = fd.deal_id
+    LEFT JOIN grade_history AS gh ON issues_.user_id = gh.user_id
+                                 AND issues_.issue_created_time BETWEEN gh.grade_from_ts AND coalesce(gh.grade_to_ts, current_timestamp + INTERVAL 3 hour)
+)
+
+SELECT *
+FROM issues
---
+++ models/spark/b2b_mart/dashboards/sourcing_report_offers.sql
@@ -0,0 +1,54 @@
+{{ config(
+    schema='b2b_mart',
+    materialized='table',
+    file_format='parquet',
+    meta = {
+      'model_owner' : '@abadoyan',
+      'bigquery_load': 'true'
+    }
+) }}
+
+WITH deals AS (
+    SELECT DISTINCT deal_id
+    FROM {{ ref('sourcing_report_issues') }}
+),
+     moderator AS (
+    SELECT co.offer_id,
+           du.email AS offer_created_email
+    FROM {{ ref('fact_customer_offers') }} AS co
+    LEFT JOIN {{ ref('dim_user_admin') }} AS du ON co.moderator_id = du.admin_id
+    WHERE co.next_effective_ts_msk IS NULL
+)
+
+  
+SELECT dp.deal_id,
+       dp.deal_friendly_id,
+       dp.user_id,
+       dp.country,
+       dp.customer_request_id,
+       dp.offer_id,
+       fop.offer_status,
+       dp.offer_product_id,
+       dp.product_id,
+       dp.order_id,
+       fo.created_ts_msk AS order_created_time,
+       dp.order_friendly_id,
+       dp.merchant_order_id,
+       dp.merchant_order_friendly_id,
+       dp.merchant_id,
+       dp.order_product_id,
+       dp.product_friendly_id,
+       mod.offer_created_email,
+       dp.owner_email, --AS deal_owner_email,
+       dp.owner_role, --AS deal_owner_role,
+       CASE
+           WHEN fop.offer_status = 'rejected' AND dp.order_id IS NOT NULL THEN 'Оффер изначально был в заказе, но его убрали'
+           WHEN fop.offer_status = 'approved' AND fop.offer_created_time > fo.created_ts_msk THEN 'Оффер добавили после создания заказа'
+       END AS offer_status_manual,
+       MAX(CASE WHEN dp.customer_request_id IS NOT NULL THEN 1 ELSE 0 END) OVER (PARTITION BY dp.deal_id) AS is_deal_has_customer_request,
+       MAX(CASE WHEN dp.order_id IS NOT NULL AND dp.customer_request_id IS NULL THEN 1 ELSE 0 END) OVER (PARTITION BY dp.deal_Id) AS is_deal_with_order_without_customer_request
+FROM {{ ref('dim_deal_products') }} AS dp
+JOIN deals AS d ON dp.deal_id = d.deal_id
+LEFT JOIN {{ ref('fact_offer_product') }} AS fop ON fop.next_effective_ts_msk IS NULL AND dp.offer_id = fop.offer_id
+LEFT JOIN {{ ref('fact_order') }} AS fo ON fo.next_effective_ts_msk IS NULL AND dp.order_id = fo.order_id
+LEFT JOIN moderator AS mod ON dp.offer_id = mod.offer_id
