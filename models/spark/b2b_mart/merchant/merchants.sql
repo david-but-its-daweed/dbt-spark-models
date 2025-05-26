0434c6085d3666ed17af76481b92285a80a3e7e7
David <44006282+david-but-its-daweed@users.noreply.github.com>
2025-05-26 09:14:05 -0700

    New merchant table (#1594)
    Co-authored-by: daweed@joom.com <daweed@joom.com>
    Co-authored-by: david-but-its-daweed <david-but-its-daweed@users.noreply.github.com>

---
+++ models/spark/b2b_mart/merchant/merchants.sql
@@ -0,0 +1,33 @@
+{{ config(
+    schema='b2b_mart',
+    materialized='table',
+    file_format='parquet',
+    meta = {
+      'model_owner' : '@daweed',
+      'bigquery_load': 'true',
+    }
+) }}
+
+SELECT
+    m._id AS merchant_id,
+    m.name AS merchant_name,
+    m.companyname AS company_name,
+    m.email,
+
+    CASE
+        WHEN kyc.status = 0 THEN 'Action required'
+        WHEN kyc.status = 10 THEN 'On review'
+        WHEN kyc.status = 20 THEN 'Passed'
+        WHEN kyc.status = 30 THEN 'Failed'
+        ELSE 'No status'
+    END AS kyc_status,
+
+    m.enabled AS is_enabled,
+    (DATE(FROM_UNIXTIME(m.createdTimeMs)) >= '2025-03-01' AND map.type = '2') AS is_internal,
+    DATE(FROM_UNIXTIME(m.createdTimeMs / 1000)) AS created_date
+FROM
+    mongo.b2b_core_merchants_daily_snapshot AS m
+LEFT JOIN
+    mongo.b2b_core_merchant_appendixes_daily_snapshot AS map ON m._id = map._id
+LEFT JOIN
+    mongo.b2b_core_merchant_kyc_profiles_daily_snapshot AS kyc ON m._id = kyc._id
