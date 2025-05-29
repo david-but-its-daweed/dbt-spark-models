67178abbe242da235b60e4c4edaeea6c1309e085
abadoyan-lab <a.badoyan@joom.com>
2025-05-29 20:54:22 +0400

    Update ss_events_banner_view.sql (#1630)

--- models/spark/b2b_mart/ss/ss_events_banner_view.sql
+++ models/spark/b2b_mart/ss/ss_events_banner_view.sql
@@ -10,6 +10,18 @@
 ) }}
 
 
+WITH bots AS (
+    SELECT
+        device_id,
+        MAX(1) AS bot_flag
+    FROM threat.bot_devices_joompro
+    WHERE
+        is_device_marked_as_bot
+        OR is_retrospectively_detected_bot
+    GROUP BY 1
+)
+
+    
 SELECT
     de.type,
     de.event_id,
@@ -28,7 +40,9 @@ SELECT
         nullIf(REGEXP_EXTRACT(de.payload.bannerDestinationUrl, r'search/([^/?]+)'), '')
     ) AS promotion_id
 FROM {{ source('b2b_mart', 'device_events') }} AS de
+LEFT JOIN bots ON de.device.id = bots.device_id
 WHERE
     de.partition_date >= '2025-05-22'
     AND de.type = 'bannerView'
     AND de.payload.bannerDestinationUrl IS NOT NULL
+    AND bots.bot_flag IS NULL
