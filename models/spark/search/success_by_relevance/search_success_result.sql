b4de15b0d6395fa190c9c7d4d1dc75de9268092b
Ilya <itangaev@joom.com>
2025-05-30 17:54:45 +0100

    Search success updates (#1647)
    Co-authored-by: Ilya Tangaev <itangaev@itangaev-osx.freets>

--- models/spark/search/success_by_relevance/search_success_result.sql
+++ models/spark/search/success_by_relevance/search_success_result.sql
@@ -17,17 +17,23 @@ WITH markup AS (
         query,
         relevance AS text_relevance
     FROM {{ source('search', 'text_search_success_filter_marked_up') }}
+),
+
+join AS (
+    SELECT
+        a.*,
+        a.event_date AS partition_date,  -- дата покупки
+        b.text_relevance,
+        COALESCE(b.text_relevance, CAST(a.category_relevance AS INT)) AS relevance,
+        DATEDIFF(CURRENT_DATE(), CAST(a.search_date AS DATE)) <= 6 AS is_last_7_days
+    FROM {{ ref('search_success_prepare_extracts') }} AS a
+    LEFT JOIN markup AS b
+        ON a.product_id = b.product_id
+        AND a.query = b.query
+        AND a.search_date = b.search_date
+        AND a.event_date = b.partition_date
 )
-SELECT
-    a.*,
-    a.event_date AS partition_date,  -- дата покупки
-    b.text_relevance,
-    COALESCE(b.text_relevance, CAST(a.category_relevance AS INT)) AS relevance,
-    DATEDIFF(CURRENT_DATE(), CAST(a.search_date AS DATE)) <= 6 AS is_last_7_days
-FROM {{ ref('search_success_prepare_extracts') }} AS a
-LEFT JOIN markup AS b
-    ON a.product_id = b.product_id
-    AND a.query = b.query
-    AND a.search_date = b.search_date
-    AND a.event_date = b.partition_date
-WHERE a.event_date >= "2025-04-01"
+
+SELECT *
+FROM join
+DISTRIBUTE BY partition_date
