ee37409aff4cebcc2777518772575550274218d6
David <44006282+david-but-its-daweed@users.noreply.github.com>
2025-06-11 10:53:55 -0700

    New table defining country code for each user session (#1688)
    Co-authored-by: daweed@joom.com <daweed@joom.com>
    Co-authored-by: david-but-its-daweed <david-but-its-daweed@users.noreply.github.com>

---
+++ models/spark/b2b_mart/ss/ss_session_country.sql
@@ -0,0 +1,42 @@
+{{ config(
+    schema='b2b_mart',
+    materialized='view',
+    meta = {
+      'model_owner' : '@daweed',
+      'team': 'general_analytics',
+      'bigquery_load': 'true',
+      'priority_weight': '150'
+    }
+) }}
+
+WITH session_landings AS (
+    SELECT
+        user_id,
+        session_id,
+        events.event_landing AS landing,
+        COUNT(*) AS cnt
+    FROM
+        b2b_mart.ss_events_by_session
+        LATERAL VIEW EXPLODE(events_in_session) AS events
+    WHERE
+        events.event_landing IS NOT NULL
+    GROUP BY
+        1, 2, 3
+),
+
+final AS (
+    SELECT
+        user_id,
+        session_id,
+        landing,
+        ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY cnt DESC) AS rn
+    FROM session_landings
+    WHERE landing IS NOT NULL
+),
+
+SELECT
+    user_id,
+    session_id,
+    landing AS country_code
+FROM final
+WHERE rn = 1
