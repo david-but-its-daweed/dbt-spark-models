937ef483f9867b67ae65f550b518d0302de2743c
anna zaychik <67829640+anyazay@users.noreply.github.com>
2025-06-20 15:25:11 +0100

    Update conversion_funnel_new.sql (#1732)

--- models/spark/onfy/conversion_funnel_new.sql
+++ models/spark/onfy/conversion_funnel_new.sql
@@ -1,7 +1,8 @@
 {{ config(
     schema='onfy',
-    materialized='table',
-    file_format='parquet',
+    file_format='delta',
+    materialized='incremental',
+    incremental_strategy='insert_overwrite',
     partition_by=['session_start_date'],
     meta = {
       'model_owner' : '@annzaychik',
