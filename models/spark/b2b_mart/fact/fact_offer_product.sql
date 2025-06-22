228a321d35bcfe030d9091359fbadb37e8c5a0dd
abadoyan-lab <a.badoyan@joom.com>
2025-06-22 05:51:53 +0400

    Update fact_offer_product.sql (#1735)

--- models/spark/b2b_mart/fact/fact_offer_product.sql
+++ models/spark/b2b_mart/fact/fact_offer_product.sql
@@ -9,31 +9,33 @@
 ) }}
 
 
-
-select 
-        op.offer_product_id,
-        op.product_id,
-        op.offer_id,
-        op.trademark,
-        op.hs_code,
-        op.manufacturer_id,
-        op.name AS product_name,
-        op.type as product_type,
-        op.disabled,
-        op.created_time_msk as offer_product_created_time,
-        co.customer_request_id,
-        co.deal_id,
-        co.merchant_id,
-        co.offer_type,
-        co.order_id,
-        co.status as offer_status,
-        co.user_id,
-        co.created_time as offer_created_time,
-        TIMESTAMP(dbt_valid_from) as effective_ts_msk,
-        TIMESTAMP(dbt_valid_to) as next_effective_ts_msk
-    from {{ ref('scd2_offer_products_snapshot') }} op
-    left join (
-      select distinct *
-      from {{ ref('fact_customer_offers') }}
-      where next_effective_ts_msk is null
-      ) co on op.offer_id = co.offer_id
+SELECT
+    op.offer_product_id,
+    op.product_id,
+    op.offer_id,
+    op.trademark,
+    op.hs_code,
+    op.manufacturer_id,
+    op.name AS product_name,
+    op.type AS product_type,
+    op.disabled,
+    op.is_certification_required,
+    op.is_agency_registration_required,
+    op.agency_name,
+    op.created_time_msk AS offer_product_created_time,
+    co.customer_request_id,
+    co.deal_id,
+    co.merchant_id,
+    co.offer_type,
+    co.order_id,
+    co.status AS offer_status,
+    co.user_id,
+    co.created_time AS offer_created_time,
+    TIMESTAMP(dbt_valid_from) AS effective_ts_msk,
+    TIMESTAMP(dbt_valid_to) AS next_effective_ts_msk
+FROM {{ ref('scd2_offer_products_snapshot') }} AS op
+LEFT JOIN (
+    SELECT DISTINCT *
+    FROM {{ ref('fact_customer_offers') }}
+    WHERE next_effective_ts_msk IS NULL
+) AS co ON op.offer_id = co.offer_id
