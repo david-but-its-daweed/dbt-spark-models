6f23ac50c18722781d05f50a0ed765b5fd6601c1
abadoyan-lab <a.badoyan@joom.com>
2025-06-25 15:09:47 +0400

    [PRO-11158] Update and rename purchasing_and_production_report_boxes.sql to procu… (#1754)
    …rement_orders_boxes.sql

--- models/spark/b2b_mart/dashboards/purchasing_and_production_report/procurement_orders_boxes.sql
+++ models/spark/b2b_mart/dashboards/purchasing_and_production_report/procurement_orders_boxes.sql
@@ -41,10 +41,13 @@ WITH request AS (
 merchant AS (
     SELECT
         procurement_order_id,
+        COUNT(variant_id) AS count_variants,
         SUM(price_per_item * qty) AS total_price,
         SUM(price_per_item * original_qty) / SUM(original_qty) AS weighted_avg_price_per_item,
         SUM(original_qty) AS original_qty,
         SUM(qty) AS qty,
+        SUM(item_weight_netto) AS item_weight_netto,
+        SUM(number_of_boxes) AS number_of_boxes,
         SUM(number_of_boxes * box_weight) AS brutto_kg,
         SUM(box_length * box_width * box_height * number_of_boxes / 1000000) AS volume
     FROM (
@@ -56,6 +59,7 @@ merchant AS (
             v_value.box.box.w AS box_width,
             v_value.box.box.h AS box_height,
             v_value.box.box.weight AS box_weight,
+            v_value.sWeight AS item_weight_netto,
             v_value.qty AS qty,
             v_value.originalQty AS original_qty,
             v_value.box.itemQuantityPerBox AS qty_per_box,
@@ -64,12 +68,15 @@ merchant AS (
         FROM {{ ref('procurement_orders') }}
         LATERAL VIEW explode(variants) AS v_key, v_value
     ) AS m
+    /* Оставляем только подтвержденные варианты */
+    WHERE original_qty > 0
     GROUP BY procurement_order_id
 ),
 
 warehouse_product AS (
     SELECT
         procurement_order_id,
+        SUM(number_of_boxes) AS number_of_boxes,
         SUM(number_of_boxes * box_weight) AS box_brutto_kg,
         SUM(box_length * box_width * box_height * number_of_boxes / 1000000) AS box_volume
     FROM (
@@ -92,6 +99,7 @@ warehouse_product AS (
 warehouse_pickup AS (
     SELECT
         procurement_order_id,
+        SUM(number_of_boxes) AS number_of_boxes,
         SUM(number_of_boxes * pickup_box_weight) AS pickup_box_brutto_kg,
         SUM(pickup_box_length * pickup_box_width * pickup_box_height * number_of_boxes / 1000000) AS pickup_box_volume
     FROM (
@@ -113,6 +121,7 @@ warehouse_pickup AS (
 warehouse AS (
     SELECT
         COALESCE(w.procurement_order_id, wp.procurement_order_id) AS procurement_order_id,
+        COALESCE(w.number_of_boxes, wp.number_of_boxes) AS number_of_boxes,
         COALESCE(w.box_brutto_kg, wp.pickup_box_brutto_kg) AS pickup_box_brutto_kg,
         COALESCE(w.box_volume, wp.pickup_box_volume) AS pickup_box_volume
     FROM warehouse_product AS w
@@ -131,59 +140,68 @@ currency_rate AS (
     GROUP BY effective_date, currency_code
 )
 
+
 SELECT
     r.procurement_order_id,
 
     -- SS data
-    ss.qty AS request_qty_raw,
-    ss.weighted_avg_merchant_price_per_item AS weighted_avg_merchant_price_per_item_raw,
-    ss.brutto_kg AS request_weight_raw,
-    ss.volume AS request_volume_raw,
+    ss.qty AS request_qty,
+    ss.weighted_avg_merchant_price_per_item AS weighted_avg_merchant_price_per_item,
+    ss.brutto_kg AS request_weight,
+    ss.volume AS request_volume,
 
     -- Merchant data
-    m.total_price AS total_price_raw,
-    m.total_price * cr.rate AS total_price_usd_raw,
-    m.weighted_avg_price_per_item AS weighted_avg_price_per_item_raw,
-    m.original_qty AS original_qty_raw,
-    m.qty AS final_qty_raw,
-    m.brutto_kg AS merchant_weight_raw,
-    m.brutto_kg / IF(m.original_qty = 0, NULL, m.original_qty) AS merchant_weight_per_piece_raw,
-    m.volume AS merchant_volume_raw,
-    m.volume / IF(m.original_qty = 0, NULL, m.original_qty) AS merchant_volume_per_piece_raw,
+    m.count_variants,
+    m.total_price AS total_price,
+    m.total_price * cr.rate AS total_price_usd,
+    m.weighted_avg_price_per_item AS weighted_avg_price_per_item,
+    m.original_qty AS original_qty,
+    m.qty AS final_qty,
+    m.item_weight_netto,
+    m.number_of_boxes AS merchant_number_of_boxes,
+    m.brutto_kg AS merchant_weight,
+    m.brutto_kg / IF(m.original_qty = 0, NULL, m.original_qty) AS merchant_weight_per_piece,
+    m.volume AS merchant_volume,
+    m.volume / IF(m.original_qty = 0, NULL, m.original_qty) AS merchant_volume_per_piece,
 
     -- Warehouse data
-    w.pickup_box_brutto_kg AS warehouse_weight_raw,
-    w.pickup_box_brutto_kg / IF(m.qty = 0, NULL, m.qty) AS warehouse_weight_per_piece_raw,
-    w.pickup_box_volume AS warehouse_volume_raw,
-    w.pickup_box_volume / IF(m.qty = 0, NULL, m.qty) AS warehouse_volume_per_piece_raw,
+    w.number_of_boxes AS warehouse_number_of_boxes,
+    w.pickup_box_brutto_kg AS warehouse_weight,
+    w.pickup_box_brutto_kg / IF(m.qty = 0, NULL, m.qty) AS warehouse_weight_per_piece,
+    w.pickup_box_volume AS warehouse_volume,
+    w.pickup_box_volume / IF(m.qty = 0, NULL, m.qty) AS warehouse_volume_per_piece,
 
     -- Comparison 1
     CASE
-        WHEN (w.pickup_box_brutto_kg IS NOT NULL AND m.brutto_kg IS NOT NULL)
-          OR (w.pickup_box_volume IS NOT NULL AND m.volume IS NOT NULL)
-        THEN 1 ELSE 0
+        WHEN ((w.pickup_box_brutto_kg IS NOT NULL AND m.brutto_kg IS NOT NULL)
+          OR (w.pickup_box_volume IS NOT NULL AND m.volume IS NOT NULL))
+         AND is_for_purchasing_and_production_report = 1
+        THEN 1
+        ELSE 0
     END AS is_for_merchant_comparison,
 
     (w.pickup_box_brutto_kg / IF(m.qty = 0, NULL, m.qty) - m.brutto_kg / IF(m.original_qty = 0, NULL, m.original_qty))
-        / (m.brutto_kg / IF(m.original_qty = 0, NULL, m.original_qty)) AS weight_diff_per_piece_pct_raw,
+        / (m.brutto_kg / IF(m.original_qty = 0, NULL, m.original_qty)) AS weight_diff_per_piece_pct,
 
-    (w.pickup_box_brutto_kg - m.brutto_kg) / m.brutto_kg AS weight_diff_pct_raw,
-    (w.pickup_box_volume - m.volume) / m.volume AS volume_diff_pct_raw,
+    (w.pickup_box_brutto_kg - m.brutto_kg) / m.brutto_kg AS weight_diff_pct,
+    (w.pickup_box_volume - m.volume) / m.volume AS volume_diff_pct,
 
     (w.pickup_box_volume / IF(m.qty = 0, NULL, m.qty) - m.volume / IF(m.original_qty = 0, NULL, m.original_qty))
-        / (m.volume / IF(m.original_qty = 0, NULL, m.original_qty)) AS volume_diff_per_piece_pct_raw,
+        / (m.volume / IF(m.original_qty = 0, NULL, m.original_qty)) AS volume_diff_per_piece_pct,
 
     -- Comparison 2
     CASE
-        WHEN (m.weighted_avg_price_per_item IS NOT NULL AND ss.weighted_avg_merchant_price_per_item IS NOT NULL)
+        WHEN ((m.weighted_avg_price_per_item IS NOT NULL AND ss.weighted_avg_merchant_price_per_item IS NOT NULL)
           OR (m.brutto_kg IS NOT NULL AND ss.brutto_kg IS NOT NULL)
-          OR (m.volume IS NOT NULL AND ss.volume IS NOT NULL)
-        THEN 1 ELSE 0
+          OR (m.volume IS NOT NULL AND ss.volume IS NOT NULL))
+         AND is_for_purchasing_and_production_report = 1
+        THEN 1
+        ELSE 0
     END AS is_for_confirmed_comparison,
 
-    (m.weighted_avg_price_per_item - ss.weighted_avg_merchant_price_per_item) / ss.weighted_avg_merchant_price_per_item AS merchant_price_diff_pct_raw,
-    (m.brutto_kg - ss.brutto_kg) / ss.brutto_kg AS request_weight_diff_pct_raw,
-    (m.volume - ss.volume) / ss.volume AS request_volume_diff_pct_raw
+    (m.weighted_avg_price_per_item - ss.weighted_avg_merchant_price_per_item) / ss.weighted_avg_merchant_price_per_item AS merchant_price_diff_pct,
+    (m.brutto_kg - ss.brutto_kg) / ss.brutto_kg AS request_weight_diff_pct,
+    (m.volume - ss.volume) / ss.volume AS request_volume_diff_pct
 
 FROM {{ ref('procurement_orders') }} AS r
 LEFT JOIN request AS ss ON r.customer_request_id = ss.customer_request_id
