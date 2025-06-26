8a5ea49852f101c497b27f3a77e4b7830b7836f5
abadoyan-lab <a.badoyan@joom.com>
2025-06-26 13:03:07 +0400

    [PRO-11913] Add device_platform to ss_events_customer_journey.sql (#1760)

--- models/spark/b2b_mart/ss/ss_events_customer_journey.sql
+++ models/spark/b2b_mart/ss/ss_events_customer_journey.sql
@@ -14,8 +14,9 @@
 
 
 WITH bots AS (
-    SELECT device_id,
-           MAX(1) AS bot_flag
+    SELECT
+        device_id,
+        MAX(1) AS bot_flag
     FROM threat.bot_devices_joompro
     WHERE is_device_marked_as_bot
        OR is_retrospectively_detected_bot
@@ -23,19 +24,25 @@ WITH bots AS (
 )
 
 
-SELECT user['userId'] AS user_id,
-       de.device.id AS device_id,
-       type,
-       event_ts_msk,
-       CAST(event_ts_msk AS DATE) AS event_msk_date,
-       payload.pageUrl,
-       payload.pageName,
-       payload.source,
-       payload.isRegistrationCompleted,
-       payload.productId AS product_id,
-       payload.position,
-       payload.popupType,
-       payload.section
+SELECT
+    user['userId'] AS user_id,
+    de.device.id AS device_id,
+    CASE
+        WHEN device.osType IN ('android', 'ios', 'tizen', 'harmonyos') THEN 'mobile'
+        WHEN device.osType IN ('ubuntu', 'linux', 'mac os', 'windows', 'chromium os') THEN 'desktop'
+        ELSE 'other'
+    END AS device_platform,
+    type,
+    event_ts_msk,
+    CAST(event_ts_msk AS DATE) AS event_msk_date,
+    payload.pageUrl,
+    payload.pageName,
+    payload.source,
+    payload.isRegistrationCompleted,
+    payload.productId AS product_id,
+    payload.position,
+    payload.popupType,
+    payload.section
 FROM {{ source('b2b_mart', 'device_events') }} AS de
 LEFT JOIN bots ON de.device.id = bots.device_id
 WHERE partition_date >= '2024-04-01'
