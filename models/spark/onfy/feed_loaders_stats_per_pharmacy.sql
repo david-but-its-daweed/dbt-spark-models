7c248dfcd3791c1fc82218605035ac368e9d6254
Elena Bukueva <80640194+HelBuk@users.noreply.github.com>
2025-07-02 16:31:25 +0200

    PhAn_1029 feed loaders stats (#1785)
    Co-authored-by: helbuk@joom.com <helbuk@joom.com>
    Co-authored-by: HelBuk <HelBuk@users.noreply.github.com>

---
+++ models/spark/onfy/feed_loaders_stats_per_pharmacy.sql
@@ -0,0 +1,31 @@
+{{ config(
+    schema='onfy',
+    materialized='table',
+    file_format='delta',
+    meta = {
+      'model_owner' : '@helbuk',
+      'team': 'onfy',
+      'bigquery_load': 'true',
+      'alerts_channel': '#onfy-etl-monitoring',
+      'bigquery_overwrite': 'true'
+    }
+) }}
+
+
+SELECT
+    date,
+    channel,
+    cheapest_store_name,
+    chosen_store_id,
+    COUNT(*) AS number_of_feeds,
+    COUNT(DISTINCT product_id) AS number_of_products,
+    AVG(discount_percent) AS mean_discount_percent,
+    PERCENTILE(discount_percent, 0.5) AS median_discount_percent,
+    PERCENTILE(discount_percent, 0.25) AS discount_percent_percent_percentile_25,
+    PERCENTILE(discount_percent, 0.75) AS median_discount_percent_percentile_75
+FROM pharmacy.feed_loaders_state
+GROUP BY
+    date,
+    channel,
+    cheapest_store_name,
+    chosen_store_id
