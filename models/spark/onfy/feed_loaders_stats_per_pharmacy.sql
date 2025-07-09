e445e5f9e9adb7a7bebcddc0d5fda8af48f71b8c
Elena Bukueva <80640194+HelBuk@users.noreply.github.com>
2025-07-09 15:43:24 +0200

    PhAn_1029 Corrected discount computation (#1824)
    Co-authored-by: helbuk@joom.com <helbuk@joom.com>
    Co-authored-by: HelBuk <HelBuk@users.noreply.github.com>

--- models/spark/onfy/feed_loaders_stats_per_pharmacy.sql
+++ models/spark/onfy/feed_loaders_stats_per_pharmacy.sql
@@ -19,10 +19,10 @@ SELECT
     chosen_store_id,
     COUNT(*) AS number_of_feeds,
     COUNT(DISTINCT product_id) AS number_of_products,
-    AVG(discount_percent) AS mean_discount_percent,
-    PERCENTILE(discount_percent, 0.5) AS median_discount_percent,
-    PERCENTILE(discount_percent, 0.25) AS discount_percent_percent_percentile_25,
-    PERCENTILE(discount_percent, 0.75) AS median_discount_percent_percentile_75
+    ROUND(AVG(100.0 * (price_eur - discounted_price_eur) / price_eur), 1) AS mean_discount_percent,
+    PERCENTILE(100.0 * (price_eur - discounted_price_eur) / price_eur, 0.5) AS median_discount_percent,
+    PERCENTILE(100.0 * (price_eur - discounted_price_eur) / price_eur, 0.25) AS discount_percent_percent_percentile_25,
+    PERCENTILE(100.0 * (price_eur - discounted_price_eur) / price_eur, 0.75) AS median_discount_percent_percentile_75
 FROM pharmacy.feed_loaders_state
 GROUP BY
     date,
