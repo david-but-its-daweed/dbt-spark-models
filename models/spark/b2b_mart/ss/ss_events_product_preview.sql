74acffa5ce43c9099461ed104ec671ea94fe6d18
mkirusha <kirill_melnikov@joom.com>
2025-07-10 14:54:51 +0100

    Create ss_events_product_preview.sql (#1831)

---
+++ models/spark/b2b_mart/ss/ss_events_product_preview.sql
@@ -0,0 +1,51 @@
+{{ config(
+    schema='b2b_mart',
+    materialized='view',
+    partition_by={
+         "field": "event_msk_date"
+    },
+    meta = {
+      'model_owner' : '@kirill_melnikov',
+      'team': 'general_analytics',
+      'bigquery_load': 'true',
+      'priority_weight': '150'
+    }
+) }}
+
+WITH bots AS (
+    SELECT
+        device_id,
+        MAX(1) AS bot_flag
+    FROM threat.bot_devices_joompro
+    WHERE is_device_marked_as_bot
+       OR is_retrospectively_detected_bot
+    GROUP BY 1
+)
+
+SELECT
+    user['userId'] AS user_id,
+    de.device.id AS device_id,
+    CASE
+        WHEN device.osType IN ('android', 'ios', 'tizen', 'harmonyos') THEN 'mobile'
+        WHEN device.osType IN ('ubuntu', 'linux', 'mac os', 'windows', 'chromium os') THEN 'desktop'
+        ELSE 'other'
+    END AS device_platform,
+    type,
+    event_ts_msk,
+    CAST(event_ts_msk AS DATE) AS event_msk_date,
+    payload.index, 
+    payload.pageUrl,
+    payload.pageName,
+    payload.source,
+    payload.productId AS product_id,
+    payload.position,
+    payload.promotionId,
+    payload.badges,
+    payload.minQuantity,
+    payload.productPriceRange, 
+    payload.searchResultsUniqId
+FROM  {{ source('b2b_mart', 'device_events') }} AS de
+LEFT JOIN bots ON de.device.id = bots.device_id
+WHERE partition_date >= '2024-04-01'
+  AND bot_flag IS NULL
+  AND type IN ('productPreview')
