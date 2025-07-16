7fc1493feddb64b525cf7b149ed55102405146f1
anna zaychik <67829640+anyazay@users.noreply.github.com>
2025-07-16 22:12:30 +0100

    Update orders_info.sql (#1873)

--- models/spark/onfy/orders_info.sql
+++ models/spark/onfy/orders_info.sql
@@ -1,7 +1,7 @@
 {{ config(
     schema='onfy',
     materialized='table',
-    file_format='parquet',
+    file_format='delta',
     partition_by=['partition_date'],
     meta = {
       'model_owner' : '@annzaychik',
