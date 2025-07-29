dd06b0cf83419d2c6321f66fe7115555c06c4aec
abadoyan-lab <a.badoyan@joom.com>
2025-07-29 13:59:23 +0400

    [PRO-12085] Create ai_chat_bot_message_sent.sql (#1910)
    Co-authored-by: abadoyan-lab <abadoyan-lab@users.noreply.github.com>

---
+++ models/spark/b2b_mart/dashboards/ai_chat_bot/ai_chat_bot_message_sent.sql
@@ -0,0 +1,31 @@
+{{ config(
+    schema='b2b_mart',
+    file_format='delta',
+    materialized='incremental',
+    incremental_strategy='insert_overwrite',
+    meta = {
+      'model_owner' : '@abadoyan',
+      'bigquery_load': 'true'
+    }
+) }}
+
+
+SELECT
+    event_id,
+    event_ts_msk,
+    type,
+    payload.userId AS user_id,
+    payload.contactId AS contact_id,
+    payload.messageText AS message,
+    payload.messageType AS message_type,
+    payload.senderType AS sender_type,
+    payload.sessionId AS session_id,
+    payload.status,
+    payload.dealId AS deal_id,
+    payload.stopReason AS stop_reason,
+    payload.attachments,
+    MILLIS_TO_TS_MSK(payload.timestamp) AS message_sent_ts_msk
+FROM {{ source('b2b_mart', 'operational_events') }}
+WHERE
+    partition_date >= '2025-07-28'
+    AND type = 'joomproChatMessageSent'
---
+++ models/spark/b2b_mart/dashboards/ai_chat_bot/ai_chat_bot_stopped.sql
@@ -0,0 +1,27 @@
+{{ config(
+    schema='b2b_mart',
+    file_format='delta',
+    materialized='incremental',
+    incremental_strategy='insert_overwrite',
+    meta = {
+      'model_owner' : '@abadoyan',
+      'bigquery_load': 'true'
+    }
+) }}
+
+
+SELECT
+    event_id,
+    event_ts_msk,
+    type,
+    payload.userId AS user_id,
+    payload.contactId AS contact_id,
+    payload.lastBotMessage AS last_bot_message,
+    payload.previousMessage AS previous_message,
+    payload.previousUserMessage AS previous_user_message,
+    payload.userMessage AS user_message,
+    payload.stopReason AS stop_reason,
+    MILLIS_TO_TS_MSK(payload.timestamp) AS bot_stopped_ts_msk
+FROM {{ source('b2b_mart', 'operational_events') }}
+WHERE partition_date >= '2025-07-28'
+  AND type = 'joomproChatbotStopped'
