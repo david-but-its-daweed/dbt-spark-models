a9f6fa80cb1c1b74494d1dbda450cf2dc5652898
Vladislav Markov <90322948+v232323@users.noreply.github.com>
2025-08-18 12:54:05 +0100

    [AN-4271] fix to deal with sql fluff (#1989)
    Co-authored-by: v232323 <v232323@users.noreply.github.com>

--- models/spark/category_management/merchant_charges.sql
+++ models/spark/category_management/merchant_charges.sql
@@ -14,54 +14,70 @@
   )
 }}
 
+WITH data_1 AS (
+    SELECT
+        DATE(inv.ct) AS partition_date,
+        `inv`.`_id` AS invoice_id,
+        `inv`.`ct` AS created_at,
+        `inv`.`to`.`id` AS inv_to_id,
+        `inv`.`fr`.`id` AS inv_fr_id,
+        inv.t,
+        `inv`.`s`.`tp`.`amount` AS inv_s_tp_amount,
+        `inv`.`s`.`tp`.`ccy` AS inv_s_tp_ccy,
+        ELEMENT_AT(inv.sh, 1) AS inv_sh_0, --достаем нулевой элемент аналогично inv.sh[0]
+        `inv`.`p`.`b` AS inv_p_b,
+        `inv`.`p`.`e` AS inv_p_e,
+        inv.ct
+    FROM {{ source('mongo','finance_invoices_v3_daily_snapshot') }} AS inv --mongo.finance_invoices_v3_daily_snapshot AS inv
+    WHERE
+        1 = 1
+        AND (
+            inv.t = 58 -- joomSIAJoomMerchantOrderCancellationCharge, iprojJoomMerchantOrderCancellationCharge
+            OR
+            inv.t = 2 --  // счет Joom мерчантам по рефандам
+        )
+        AND DATE(inv.ct) >= DATE('2025-01-01')
+)
 SELECT
-    DATE(inv.ct) AS partition_date,
-    inv._id AS invoice_id,
-    inv.ct AS created_at,
+    inv.partition_date,
+    inv.invoice_id,
+    inv.created_at,
     CASE
-        WHEN inv.t = 2 THEN `inv.to.id`
-        WHEN inv.t = 58 THEN `inv.fr.id`
+        WHEN inv.t = 2 THEN inv.inv_to_id
+        WHEN inv.t = 58 THEN inv.inv_fr_id
     END AS merchant_id_corrected,
     CASE
-        WHEN inv.t = 2 THEN `inv.fr.id`
-        WHEN inv.t = 58 THEN `inv.to.id`
+        WHEN inv.t = 2 THEN inv.inv_fr_id
+        WHEN inv.t = 58 THEN inv.inv_to_id
     END AS beneficiary_corrected,
     CASE
-        WHEN inv.t = 2 THEN inv.s.tp.amount / 1000000.0 * (-1)
-        WHEN inv.t = 58 THEN inv.s.tp.amount / 1000000.0
+        WHEN inv.t = 2 THEN inv.inv_s_tp_amount / 1000000.0 * (-1)
+        WHEN inv.t = 58 THEN inv.inv_s_tp_amount / 1000000.0
     END AS invoice_amount_corrected,
-    inv.s.tp.ccy AS invoice_ccy,
+    inv.inv_s_tp_ccy AS invoice_ccy,
     inv.t AS inv_type,
     CASE
         WHEN inv.t = 2 THEN 'quality refund'
         WHEN inv.t = 58 THEN 'merchant cancellation'
     END AS inv_type_text,
     CASE
-        WHEN `inv.sh[0].s` = 1 THEN 'Created'
-        WHEN `inv.sh[0].s` = 2 THEN 'Unnumbered'
-        WHEN `inv.sh[0].s` = 3 THEN 'Ready'
-        WHEN `inv.sh[0].s` = 4 THEN 'PayoutASsigned'
-        WHEN `inv.sh[0].s` = 5 THEN 'Paid'
-        WHEN `inv.sh[0].s` = 6 THEN 'PayoutNotExpected'
+        WHEN inv.inv_sh_0.s = 1 THEN 'Created'
+        WHEN inv.inv_sh_0.s = 2 THEN 'Unnumbered'
+        WHEN inv.inv_sh_0.s = 3 THEN 'Ready'
+        WHEN inv.inv_sh_0.s = 4 THEN 'PayoutASsigned'
+        WHEN inv.inv_sh_0.s = 5 THEN 'Paid'
+        WHEN inv.inv_sh_0.s = 6 THEN 'PayoutNotExpected'
     END AS invoice_status,
-    TO_DATE(inv.p.b, 'yyyyMMdd') AS period_begin_dt,
-    TO_DATE(inv.p.e, 'yyyyMMdd') AS period_end_dt,
+    TO_DATE(inv.inv_p_b, 'yyyyMMdd') AS period_begin_dt,
+    TO_DATE(inv.inv_p_e, 'yyyyMMdd') AS period_end_dt,
     CASE
-        WHEN inv.t = 2 THEN inv.s.tp.amount / 1000000.0 * (-1)
-        WHEN inv.t = 58 THEN inv.s.tp.amount / 1000000.0
+        WHEN inv.t = 2 THEN inv.inv_s_tp_amount / 1000000.0 * (-1)
+        WHEN inv.t = 58 THEN inv.inv_s_tp_amount / 1000000.0
     END * cr.rate AS invoice_amount_usd
-FROM {{ source('mongo','finance_invoices_v3_daily_snapshot') }} AS inv --mongo.finance_invoices_v3_daily_snapshot AS inv
+FROM data_1 AS inv
 LEFT JOIN {{ ref('dim_pair_currency_rate') }} AS cr --models.dim_pair_currency_rate  AS cr
     ON
         1 = 1
-        AND inv.s.tp.ccy = cr.currency_code
+        AND inv.inv_s_tp_ccy = cr.currency_code
         AND DATE(inv.ct) = cr.effective_date
         AND cr.currency_code_to = 'USD'
-WHERE
-    1 = 1
-    AND (
-        inv.t = 58 -- joomSIAJoomMerchantOrderCancellationCharge, iprojJoomMerchantOrderCancellationCharge
-        OR
-        inv.t = 2 --  // счет Joom мерчантам по рефандам
-    )
-    AND DATE(inv.ct) >= DATE('2025-01-01')
