a457308208ab0af53faca5ba48e707c1793920d4
Maria Vasiukova <34578801+MVasyukova@users.noreply.github.com>
2025-08-27 16:34:00 +0100

    on_schema_change added (#2044)
    Co-authored-by: vasiukova_mn@joom.com <vasiukova_mn@joom.com>
    Co-authored-by: MVasyukova <MVasyukova@users.noreply.github.com>

--- models/spark/category_management/assortment_imporovement/assortment_coverage_metric_bq.sql
+++ models/spark/category_management/assortment_imporovement/assortment_coverage_metric_bq.sql
@@ -1,6 +1,7 @@
 {{
   config(
     file_format='delta',
+    on_schema_change='sync_all_columns',
     materialized='incremental',
     incremental_strategy='insert_overwrite',
     schema='category_management',
